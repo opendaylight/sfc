b501e086604c4f04a570558c59d548155e640f6f
malauko <malauko@cisco.com>
2014-08-18 17:36:03 +0200

    Added apply logback.xml configuration to Logback service on sfc-provider initialization Added '.hg' to .gitignore
    configuration: logger for sfc-provider to sfc.log file and send messages to syslog server
    
    Change-Id: Ibdcfc59bf89f53e486aca0645cd03205a67b78aa
    Signed-off-by: malauko <malauko@cisco.com>

--- sfc-provider/src/main/java/org/opendaylight/controller/config/yang/config/sfc_provider/impl/SfcProviderModule.java
+++ sfc-provider/src/main/java/org/opendaylight/controller/config/yang/config/sfc_provider/impl/SfcProviderModule.java
@@ -8,22 +8,29 @@
 
 package org.opendaylight.controller.config.yang.config.sfc_provider.impl;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
-import org.opendaylight.sfc.provider.*;
+import org.opendaylight.sfc.provider.OpendaylightSfc;
+import org.opendaylight.sfc.provider.SfcProviderRpc;
+import org.opendaylight.sfc.provider.SfcProviderSfEntryDataListener;
+import org.opendaylight.sfc.provider.SfcProviderSfcEntryDataListener;
+import org.opendaylight.sfc.provider.SfcProviderSffDataListener;
+import org.opendaylight.sfc.provider.SfcProviderSfpDataListener;
+import org.opendaylight.sfc.provider.SfcProviderSfpEntryDataListener;
+import org.opendaylight.sfc.provider.SfcProviderSnDataListener;
 import org.opendaylight.sfc.provider.bootstrap.SfcProviderBootstrapRestAPI;
+import org.opendaylight.sfc.provider.logback.SfcProviderLogbackLoader;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.ServiceFunctionService;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sfc.rev140701.ServiceFunctionChainService;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sn.rev140701.ServiceNodeService;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 /**
  * This class is called from the MD-SAL infra in order to bootstrap
@@ -60,6 +67,9 @@ public class SfcProviderModule extends org.opendaylight.controller.config.yang.c
      */
     @Override
     public java.lang.AutoCloseable createInstance() {
+
+        SfcProviderLogbackLoader.loadSfcLogbackConfiguration();
+
         final OpendaylightSfc opendaylightSfc = new OpendaylightSfc();
 
 

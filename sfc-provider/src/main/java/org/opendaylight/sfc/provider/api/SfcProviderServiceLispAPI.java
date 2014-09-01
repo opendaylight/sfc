df48447007931c711fa9b3343b11d10e8c38ab00
David Goldberg <david.goldberg@contextream.com>
2014-09-01 15:42:49 +0300

    added lisp updater for service functions and service function forwarders with lisp address
    Signed-off-by: David Goldberg <david.goldberg@contextream.com>

---
+++ sfc-provider/src/main/java/org/opendaylight/sfc/provider/api/SfcProviderServiceLispAPI.java
@@ -0,0 +1,70 @@
+package org.opendaylight.sfc.provider.api;
+
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.sfc.provider.lisp.LispUpdater;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.ServiceFunctions;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.functions.ServiceFunction;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.ServiceFunctionForwarders;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.service.function.forwarders.ServiceFunctionForwarder;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SfcProviderServiceLispAPI extends SfcProviderAbstractAPI {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SfcProviderServiceLispAPI.class);
+    private static LispUpdater lispUpdater = new LispUpdater();
+
+    SfcProviderServiceLispAPI(Object[] params, Class[] paramsTypes, String m) {
+        super(params, paramsTypes, m);
+    }
+
+    public static SfcProviderServiceLispAPI getUpdateServiceFunction(Object[] params, Class[] paramsTypes) {
+        return new SfcProviderServiceLispAPI(params, paramsTypes, "updateServiceFunction");
+    }
+
+    public static SfcProviderServiceLispAPI getUpdateServiceFunctionForwarder(Object[] params, Class[] paramsTypes) {
+        return new SfcProviderServiceLispAPI(params, paramsTypes, "updateServiceFunctionForwarder");
+    }
+
+    protected static boolean updateServiceFunction(ServiceFunction sf) {
+        boolean ret = false;
+        LOG.debug("\n####### Start: {}", Thread.currentThread().getStackTrace()[1]);
+        if (odlSfc.getDataProvider() != null) {
+            sf = lispUpdater.updateLispData(sf);
+
+            InstanceIdentifier<ServiceFunction> sfEntryIID = InstanceIdentifier.builder(ServiceFunctions.class)
+                    .child(ServiceFunction.class, sf.getKey()).toInstance();
+
+            WriteTransaction writeTx = odlSfc.getDataProvider().newWriteOnlyTransaction();
+            writeTx.put(LogicalDatastoreType.CONFIGURATION, sfEntryIID, sf, true);
+            writeTx.commit();
+
+            ret = true;
+        }
+        LOG.debug("\n########## Stop: {}", Thread.currentThread().getStackTrace()[1]);
+        return ret;
+    }
+
+    protected static boolean updateServiceFunctionForwarder(ServiceFunctionForwarder sff) {
+        boolean ret = false;
+        LOG.debug("\n####### Start: {}", Thread.currentThread().getStackTrace()[1]);
+        if (odlSfc.getDataProvider() != null) {
+
+            sff = lispUpdater.updateLispData(sff);
+
+            InstanceIdentifier<ServiceFunctionForwarder> sffEntryIID = InstanceIdentifier.builder(ServiceFunctionForwarders.class)
+                    .child(ServiceFunctionForwarder.class, sff.getKey()).toInstance();
+
+            WriteTransaction writeTx = odlSfc.getDataProvider().newWriteOnlyTransaction();
+            writeTx.merge(LogicalDatastoreType.CONFIGURATION, sffEntryIID, sff, true);
+            writeTx.commit();
+
+            ret = true;
+        }
+        LOG.debug("\n########## Stop: {}", Thread.currentThread().getStackTrace()[1]);
+        return ret;
+    }
+
+}

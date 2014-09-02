cb28a38e589ee411695628ae138aeba3a3153229
Shuva Jyoti Kar <shuva.jyoti.kar@ericsson.com>
2014-09-02 18:48:52 +0530

    Incorporating all of Sam+Reinaldo'sComments+Codecleanup
    Change-Id: I529978e21fe1828dc0e974f37d8e46b44b7b4922
    Signed-off-by: Shuva Jyoti Kar <shuva.jyoti.kar@ericsson.com>

---
+++ openflow-nsh-unaware-no-encap/src/main/java/org/opendaylight/ofsfc/provider/OpenFlowSfcRenderer.java
@@ -0,0 +1,64 @@
+package org.opendaylight.ofsfc.provider;
+
+import com.google.common.util.concurrent.Futures;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncReadWriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.sal.common.util.Rpcs;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.ServiceFunctions;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.functions.ServiceFunction;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sfc.rev140701.ServiceFunctionChains;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sfc.rev140701.service.function.chain.grouping.ServiceFunctionChain;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.ServiceFunctionForwarders;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sfp.rev140701.ServiceFunctionPaths;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sfp.rev140701.service.function.paths.ServiceFunctionPath;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sft.rev140701.ServiceFunctionTypes;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sn.rev140701.ServiceNodes;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.common.RpcError;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class OpenFlowSfcRenderer implements AutoCloseable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OpenFlowSfcRenderer.class);
+
+    protected ExecutorService executor;
+    protected DataBroker dataProvider;
+    private static OpenFlowSfcRenderer opendaylightSfcObj;
+
+    private Future<RpcResult<Void>> currentTask;
+
+    public OpenFlowSfcRenderer() {
+        executor = Executors.newFixedThreadPool(1);
+        opendaylightSfcObj = this;
+    }
+
+    public void setDataProvider(DataBroker salDataProvider) {
+        dataProvider = salDataProvider;
+    }
+
+    public DataBroker getDataProvider() {
+        return dataProvider;
+    }
+
+    public static OpenFlowSfcRenderer getOpendaylightSfcObj() {
+        return OpenFlowSfcRenderer.opendaylightSfcObj;
+    }
+
+    /**
+     * Implemented from the AutoCloseable interface.
+     */
+    @Override
+    public void close() throws ExecutionException, InterruptedException {
+        // When we close this service we need to shutdown our executor!
+    }
+}

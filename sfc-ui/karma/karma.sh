b7b2535b063ad34c340f0b0063239527356d1de0
Andrej Kincel <akincel@cisco.com>
2014-09-25 12:20:54 +0200

    Unit tests support for sfc-ui
    karma-unit.tpl.js - config for karma test runner
    
    package.json - config for npm package manager (contains packages required
    to execute tests)
    
    README.md - added step by step guide for karma installation and test execution
    
    Change-Id: I19807a8a8404f7dfa1ffbef191ff0ec7970376a6
    Signed-off-by: Andrej Kincel <akincel@cisco.com>

---
+++ sfc-ui/karma/karma.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+platform='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   platform='linux'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   platform='osx'
+fi
+
+if [[ $platform == 'linux' ]]; then
+   fullpath=`readlink -f $0`
+elif [[ $platform == 'osx' ]]; then
+   TARGET_FILE=$0
+   cd `dirname "$TARGET_FILE"`
+   TARGET_FILE=`basename $TARGET_FILE`
+
+   # Iterate down a (possible) chain of symlinks
+   while [ -L "$TARGET_FILE" ]
+   do
+       TARGET_FILE=`readlink "$TARGET_FILE"`
+       cd `dirname "$TARGET_FILE"`
+       TARGET_FILE=`basename "$TARGET_FILE"`
+   done
+
+   # Compute the canonicalized name by finding the physical path
+   # for the directory we're in and appending the target file.
+   PHYS_DIR=`pwd -P`
+   RESULT=$PHYS_DIR/$TARGET_FILE
+   fullpath=$RESULT
+fi
+
+basedir=`dirname "${fullpath}"`
+
+$basedir/../node_modules/karma-cli/bin/karma start $basedir/karma-unit.tpl.js

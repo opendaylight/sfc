d1849b2fa3328a52a41b3f57b275cb674cd92b00
Thanh Ha <thanh.ha@linuxfoundation.org>
2014-11-10 23:17:01 -0500

    Revert "Double commit to stable/helium"
    For Helium SR1 need to revert commit def723c826b69485e1192fa89c30dbc800145989.
    
    Change-Id: I3f1ad5d51b4840d73a1761db2f7ec0debc9ad3b1
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- sfcofl2/src/main/java/org/opendaylight/ofsfc/provider/OpenflowAclDataListener.java
+++ sfcofl2/src/main/java/org/opendaylight/ofsfc/provider/OpenflowAclDataListener.java
@@ -8,7 +8,6 @@
  */
 package org.opendaylight.ofsfc.provider;
 
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -18,10 +17,6 @@ import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
 import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
 import org.opendaylight.ofsfc.provider.utils.SfcInstanceIdentifierUtils;
 import org.opendaylight.ofsfc.provider.utils.SfcOfL2APIUtil;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.acl.rev140520.AccessLists;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.acl.rev140520.access.lists.AccessList;
-import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.acl.rev140701.access.lists.access.list.access.list.entries.actions.SfcAction;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.acl.rev140520.access.lists.access.list.access.list.entries.Actions;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.acl.rev140701.Actions1;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.acl.rev140701.access.lists.access.list.access.list.entries.actions.SfcAction;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.acl.rev140701.access.lists.access.list.access.list.entries.actions.sfc.action.AclServiceFunctionPath;
@@ -41,9 +36,6 @@ public class OpenflowAclDataListener extends OpenflowAbstractDataListener {
     private static final Logger LOG = LoggerFactory.getLogger(OpenflowAclDataListener.class);
     private static final OpenflowSfcRenderer odlSfc = OpenflowSfcRenderer.getOpendaylightSfcObj();
 
-    private static final short DEFAULT_MASK = 32;
-    public static final String SUBNET_MASK = "/";
-
     public OpenflowAclDataListener(DataBroker dataBroker) {
         setDataBroker(dataBroker);
         setIID(SfcInstanceIdentifierUtils.createServiceFunctionAclsPath());
@@ -60,9 +52,9 @@ public class OpenflowAclDataListener extends OpenflowAbstractDataListener {
         dataCreatedConfigurationObject = change.getCreatedData();
 
         for (Map.Entry<InstanceIdentifier<?>, DataObject> entry : dataCreatedConfigurationObject.entrySet()) {
-            if (entry.getValue() instanceof AccessLists) {
-                AccessLists createdAccessLists = (AccessLists) entry.getValue();
-                configureAclFlows(createdAccessLists, true);
+            if (entry.getValue() instanceof AccessListEntries) {
+                AccessListEntries createdAccessListEntries = (AccessListEntries) entry.getValue();
+                configureAclFlows(createdAccessListEntries, true);
             }
         }
 
@@ -71,9 +63,9 @@ public class OpenflowAclDataListener extends OpenflowAbstractDataListener {
         Set<InstanceIdentifier<?>> dataRemovedConfigurationIID = change.getRemovedPaths();
         for (InstanceIdentifier instanceIdentifier : dataRemovedConfigurationIID) {
             DataObject dataObject = dataOriginalConfigurationObject.get(instanceIdentifier);
-            if (dataObject instanceof AccessLists) {
-                AccessLists removedAccessLists = (AccessLists) dataObject;
-                configureAclFlows(removedAccessLists, false);
+            if (dataObject instanceof AccessListEntries) {
+                AccessListEntries removedAccessListEntries = (AccessListEntries) dataObject;
+                configureAclFlows(removedAccessListEntries, false);
             }
         }
 
@@ -83,94 +75,49 @@ public class OpenflowAclDataListener extends OpenflowAbstractDataListener {
         dataUpdatedConfigurationObject = change.getUpdatedData();
 
         for (Map.Entry<InstanceIdentifier<?>, DataObject> entry : dataUpdatedConfigurationObject.entrySet()) {
-            if ((entry.getValue() instanceof AccessLists && (!(dataCreatedConfigurationObject.containsKey(entry
+            if ((entry.getValue() instanceof AccessListEntries && (!(dataCreatedConfigurationObject.containsKey(entry
                     .getKey()))))) {
-                AccessLists updatedAccessLists = (AccessLists) entry.getValue();
+                AccessListEntries updatedAccessListEntries = (AccessListEntries) entry.getValue();
 
-                configureAclFlows(updatedAccessLists, true);
+                configureAclFlows(updatedAccessListEntries, true);
 
             }
         }
 
     }
 
-    private void configureAclFlows(AccessLists createdAccessLists, boolean isAddFlow) {
+    private void configureAclFlows(AccessListEntries createdAccessListEntries, boolean isAddFlow) {
+
+        AclServiceFunctionPath sfcAction = (AclServiceFunctionPath) (SfcAction) createdAccessListEntries.getActions();
+
+        String sfpName = sfcAction.getServiceFunctionPath();
+        ServiceFunctionPath servicefunctionPath = SfcOfL2APIUtil.readServiceFunctionPath(getDataBroker(), sfpName);
+
+        Long pathId = servicefunctionPath.getPathId();
+
         Matches matches;
         AceIp aceIp;
         AceIpv4 aceIpv4;
-        Short srcPort = 0;
-        Short dstPort = 0;
-        Short srcMask = 0;
-        Short dstMask = 0;
-        String srcIpAddress = null;
-        String dstIpAddress = null;
-        byte protocol = (byte) 0;
 
-        OpenflowSfcFlowProgrammer flowProgrammer = OpenflowSfcFlowProgrammer.getInstance();
+        matches = createdAccessListEntries.getMatches();
+        aceIp = (AceIp) matches.getAceType();
+        aceIpv4 = (AceIpv4) aceIp.getAceIpVersion();
 
-        Iterator<AccessList> aclIter = createdAccessLists.getAccessList().iterator();
-        while (aclIter.hasNext()) {
-            AccessList acl = aclIter.next();
+        String srcIpAddress = aceIpv4.getSourceIpv4Address().getValue();
+        String dstIpAddress = aceIpv4.getDestinationIpv4Address().getValue();
 
-            Iterator<AccessListEntries> aclEntryIter = acl.getAccessListEntries().iterator();
+        Short srcPort = aceIp.getSourcePortRange().getLowerPort().getValue().shortValue();
+        Short dstPort = aceIp.getDestinationPortRange().getLowerPort().getValue().shortValue();
 
-            while (aclEntryIter.hasNext()) {
+        byte protocol = aceIp.getIpProtocol().byteValue();
 
-                AccessListEntries createdAccessListEntries = aclEntryIter.next();
-                SfcAction sfcAction = createdAccessListEntries.getActions().getAugmentation(Actions1.class)
-                        .getSfcAction();
-                String aclServicePathName = ((AclServiceFunctionPath) sfcAction).getServiceFunctionPath();
+        List<ServicePathHop> servicePathHopList = servicefunctionPath.getServicePathHop();
 
-                ServiceFunctionPath servicefunctionPath = SfcOfL2APIUtil.readServiceFunctionPath(getDataBroker(),
-                        aclServicePathName);
-
-                Long pathId = servicefunctionPath.getPathId();
-
-                matches = createdAccessListEntries.getMatches();
-                aceIp = (AceIp) matches.getAceType();
-                aceIpv4 = (AceIpv4) aceIp.getAceIpVersion();
-
-                if (aceIpv4.getSourceIpv4Address() != null) {
-                    srcIpAddress = aceIpv4.getSourceIpv4Address().getValue();
-                    if (srcIpAddress.contains(SUBNET_MASK)) {
-                        String[] parts = srcIpAddress.split(SUBNET_MASK);
-                        srcIpAddress = parts[0];
-                        srcMask = Short.parseShort(parts[1]);
-                    } else {
-                        srcMask = DEFAULT_MASK;
-                    }
-                }
-                if (aceIpv4.getDestinationIpv4Address() != null) {
-                    dstIpAddress = aceIpv4.getDestinationIpv4Address().getValue();
-                    if (dstIpAddress.contains(SUBNET_MASK)) {
-                        String[] parts = dstIpAddress.split(SUBNET_MASK);
-                        dstIpAddress = parts[0];
-                        dstMask = Short.parseShort(parts[1]);
-                    } else {
-                        dstMask = DEFAULT_MASK;
-                    }
-                }
-
-                if (aceIp != null) {
-                    if (aceIp.getSourcePortRange() != null) {
-                        srcPort = aceIp.getSourcePortRange().getLowerPort().getValue().shortValue();
-                    }
-                    if (aceIp.getDestinationPortRange() != null) {
-                        dstPort = aceIp.getDestinationPortRange().getLowerPort().getValue().shortValue();
-                    }
-                    if (aceIp.getIpProtocol() != null) {
-                        protocol = aceIp.getIpProtocol().byteValue();
-                    }
-                }
-                List<ServicePathHop> servicePathHopList = servicefunctionPath.getServicePathHop();
-
-                for (ServicePathHop servicePathHop : servicePathHopList) {
-
-                    flowProgrammer.setNodeInfo(servicePathHop.getServiceFunctionForwarder());
-                    flowProgrammer.configureClassificationFlow(srcIpAddress, srcMask, dstIpAddress, dstMask, srcPort,
-                            dstPort, protocol, pathId, isAddFlow);
-                }
-            }
+        for (ServicePathHop servicePathHop : servicePathHopList) {
+            OpenflowSfcFlowProgrammer.getInstance().setNodeInfo(servicePathHop.getServiceFunctionForwarder());
+            OpenflowSfcFlowProgrammer.getInstance().configureClassificationFlow(srcIpAddress, (short) 32, dstIpAddress,
+                    (short) 32, srcPort, dstPort, protocol, pathId, isAddFlow);
         }
     }
+
 }

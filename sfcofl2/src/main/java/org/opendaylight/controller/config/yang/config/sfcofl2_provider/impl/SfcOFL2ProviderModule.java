4af39978503cde08f8890d2881bb73764eddc9ab
ebrjohn <brady.allen.johnson@ericsson.com>
2015-01-26 12:51:31 +0100

    Start SfcOFL2ProviderModule when the SFCOFL2 feature installs.
    From the previous commit to bootstrap SFCOFL2 for the local karaf distro,
    it wasnt instantiating the SFCOFL2 plugin.
    
    Change-Id: Iad49bbad39f0add41bb5179c83691052ca474728
    Signed-off-by: ebrjohn <brady.allen.johnson@ericsson.com>

--- sfcofl2/src/main/java/org/opendaylight/controller/config/yang/config/sfcofl2_provider/impl/SfcOFL2ProviderModule.java
+++ sfcofl2/src/main/java/org/opendaylight/controller/config/yang/config/sfcofl2_provider/impl/SfcOFL2ProviderModule.java
@@ -12,18 +12,22 @@ import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
 import org.opendaylight.ofsfc.provider.OpenflowSfcRenderer;
 import org.opendaylight.ofsfc.provider.OpenflowSfcFlowProgrammer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class SfcOFL2ProviderModule extends
-        org.opendaylight.controller.config.yang.config.sfcofl2_provider.impl.AbstractSfcOFL2ProviderModule {
+public class SfcOFL2ProviderModule extends org.opendaylight.controller.config.yang.config.sfcofl2_provider.impl.AbstractSfcOFL2ProviderModule {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SfcOFL2ProviderModule.class);
+
     public SfcOFL2ProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier,
-            org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+                                 org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
         super(identifier, dependencyResolver);
     }
 
     public SfcOFL2ProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier,
-            org.opendaylight.controller.config.api.DependencyResolver dependencyResolver,
-            org.opendaylight.controller.config.yang.config.sfcofl2_provider.impl.SfcOFL2ProviderModule oldModule,
-            java.lang.AutoCloseable oldInstance) {
+                                 org.opendaylight.controller.config.api.DependencyResolver dependencyResolver,
+                                 org.opendaylight.controller.config.yang.config.sfcofl2_provider.impl.SfcOFL2ProviderModule oldModule,
+                                 java.lang.AutoCloseable oldInstance) {
         super(identifier, dependencyResolver, oldModule, oldInstance);
     }
 
@@ -39,11 +43,15 @@ public class SfcOFL2ProviderModule extends
         final OpenflowSfcRenderer openflowSfcRenderer = new OpenflowSfcRenderer(dataBroker, rpcProvider);
         OpenflowSfcFlowProgrammer.createFlowProgrammer();
 
-        return new AutoCloseable() {
+        java.lang.AutoCloseable ret = new java.lang.AutoCloseable() {
             @Override
             public void close() throws Exception {
                 openflowSfcRenderer.close();
             }
         };
+
+        LOG.info("SFC OFL2 provider (instance {}) initialized.", ret);
+
+        return ret;
     }
 }

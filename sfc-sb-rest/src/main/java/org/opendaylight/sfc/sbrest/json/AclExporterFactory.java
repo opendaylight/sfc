a40474b3f553d63669276c346cdf0d517f1e5210
Andrej Kincel <akincel@cisco.com>
2015-02-12 08:35:46 +0100

    SB REST - JSON is generated without null values
    + JSON generated by Exporters is not containing pairs of key:null
    anymore. This makes the work of target SB devices (e.g. sfc_agent) easier.
    In other words, JSON now contains only valid key:value pairs.
    
    + Fixed exportJsonNameOnly to return an JSON array as top level (the same
    behaviour as exportJson). In fact exportJsonNameOnly is not necessary
    anymore.
    
    + Added test for AcLExporter
    
    Change-Id: I0e78dc10cee50ae3bd526fe8b462934097c0002d
    Signed-off-by: Andrej Kincel <akincel@cisco.com>

--- sfc-sb-rest/src/main/java/org/opendaylight/sfc/sbrest/json/AclExporterFactory.java
+++ sfc-sb-rest/src/main/java/org/opendaylight/sfc/sbrest/json/AclExporterFactory.java
@@ -1,7 +1,16 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package org.opendaylight.sfc.sbrest.json;
 
 import java.util.List;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.acl.rev140701.Actions1;
@@ -30,43 +39,83 @@ public class AclExporterFactory implements ExporterFactory {
     }
 }
 
-class AclExporter implements Exporter {
+class AclExporter extends AbstractExporter implements Exporter {
+
+    public static final String _ACCESS_LIST = "access-list";
+    public static final String _ACL_NAME = "acl-name";
+    public static final String _ACCESS_LIST_ENTRIES = "access-list-entries";
+    public static final String _RULE_NAME = "rule-name";
+    public static final String _MATCHES = "matches";
+    public static final String _ACTIONS = "actions";
+    public static final String _ACE_OPER_DATA = "ace-oper-data";
+    public static final String _DEFAULT_ACTIONS = "default-actions";
+    public static final String _DSCP = "dscp";
+    public static final String _IP_PROTOCOL = "ip-protocol";
+    public static final String _SOURCE_PORT_RANGE = "source-port-range";
+    public static final String _DESTINATION_PORT_RANGE = "destination-port-range";
+    public static final String _DESTINATION_MAC_ADDRESS = "destination-mac-address";
+    public static final String _DESTINATION_MAC_ADDRESS_MASK = "destination-mac-address-mask";
+    public static final String _SOURCE_MAC_ADDRESS = "source-mac-address";
+    public static final String _SOURCE_MAC_ADDRESS_MASK = "source-mac-address-mask";
+    public static final String _LOWER_PORT = "lower-port";
+    public static final String _UPPER_PORT = "upper-port";
+    public static final String _DESTINATION_IPV4_ADDRESS = "destination-ipv4-address";
+    public static final String _SOURCE_IPV4_ADDRESS = "source-ipv4-address";
+    public static final String _DESTINATION_IPV6_ADDRESS = "destination-ipv6-address";
+    public static final String _SOURCE_IPV6_ADDRESS = "source-ipv6-address";
+    public static final String _FLOW_LABEL = "flow-label";
+    public static final String _PERMIT = "permit";
+    public static final String _DENY = "deny";
+    public static final String _SERVICE_FUNCTION_ACL_RENDERED_SERVICE_PATH = "service-function-acl:rendered-service-path";
+    public static final String _MATCH_COUNTER = "match-counter";
+
+    public static final String ACE_IP = "AceIp";
+    public static final String ACE_ETH = "AceEth";
+    public static final String ACE_IPV4 = "AceIpv4";
+    public static final String ACE_IPV6 = "AceIpv6";
+    public static final String DENY = "deny";
+    public static final String PERMIT = "permit";
+    public static final String ACL_RENDERED_SERVICE_PATH = "AclRenderedServicePath";
 
     private static final Logger LOG = LoggerFactory.getLogger(AclExporter.class);
 
     @Override
     public String exportJson(DataObject dataObject) {
-
-        String ret;
+        String ret = null;
         if (dataObject instanceof AccessList) {
             AccessList acl = (AccessList) dataObject;
 
             ArrayNode aclArray = mapper.createArrayNode();
 
             ObjectNode aclNode = mapper.createObjectNode();
-            aclNode.put("acl-name", acl.getAclName());
+            aclNode.put(_ACL_NAME, acl.getAclName());
 
             ArrayNode aceArrayNode = mapper.createArrayNode();
             List<AccessListEntries> aceList = acl.getAccessListEntries();
             if (aceList != null) {
                 for (AccessListEntries ace : aceList) {
                     ObjectNode aceNode = mapper.createObjectNode();
-                    aceNode.put("rule-name", ace.getRuleName());
-                    aceNode.put("matches", this.getMatchesObjectNode(ace.getMatches()));
-                    aceNode.put("actions", this.getActionsObjectNode(ace.getActions()));
-                    aceNode.put("ace-oper-data", this.getAceOperDataObjectNode(ace.getAceOperData()));
+                    aceNode.put(_RULE_NAME, ace.getRuleName());
+                    aceNode.put(_MATCHES, this.getMatchesObjectNode(ace.getMatches()));
+                    aceNode.put(_ACTIONS, this.getActionsObjectNode(ace.getActions()));
+                    aceNode.put(_ACE_OPER_DATA, this.getAceOperDataObjectNode(ace.getAceOperData()));
 
                     aceArrayNode.add(aceNode);
                 }
-                aclNode.putArray("access-list-entries").addAll(aceArrayNode);
+                aclNode.putArray(_ACCESS_LIST_ENTRIES).addAll(aceArrayNode);
             }
 
-            aclNode.put("default-actions", this.getDefaultActionsObjectNode(acl.getDefaultActions()));
+            aclNode.put(_DEFAULT_ACTIONS, this.getDefaultActionsObjectNode(acl.getDefaultActions()));
 
             aclArray.add(aclNode);
-            ret = "{ \"access-list\" : " + aclArray.toString() + " }";
-            LOG.debug("Created Access List JSON: {}", ret);
-
+            try {
+                Object aclObject = mapper.treeToValue(aclArray, Object.class);
+                ret = mapper.writeValueAsString(aclObject);
+                ret = "{\"" + _ACCESS_LIST + "\":" + ret + "}";
+                LOG.debug("Created Access List JSON: {}", ret);
+            } catch (JsonProcessingException e) {
+                LOG.error("Error during creation of JSON for Access List {}", acl.getAclName());
+            }
         } else {
             throw new IllegalArgumentException("Argument is not an instance of Access List");
         }
@@ -82,10 +131,11 @@ class AclExporter implements Exporter {
             AccessList acl = (AccessList) dataObject;
 
             ObjectNode aclNode = mapper.createObjectNode();
+            aclNode.put(_ACL_NAME, acl.getAclName());
+            ArrayNode aclArray = mapper.createArrayNode();
+            aclArray.add(aclNode);
 
-            aclNode.put("acl-name", acl.getAclName());
-
-            ret = "{ \"access-list\" : " + aclNode.toString() + " }";
+            ret = "{\"" + _ACCESS_LIST + "\":" + aclArray.toString() + "}";
             LOG.debug("Created Access List JSON: {}", ret);
 
         } else {
@@ -106,29 +156,29 @@ class AclExporter implements Exporter {
             String aceType = matches.getAceType().getImplementedInterface().getSimpleName();
 
             switch (aceType) {
-                case "AceIp":
+                case ACE_IP:
                     AceIp aceIp = (AceIp) matches.getAceType();
                     if (aceIp.getDscp() != null) {
-                        matchesNode.put("dscp", aceIp.getDscp().getValue());
+                        matchesNode.put(_DSCP, aceIp.getDscp().getValue());
                     }
-                    matchesNode.put("ip-protocol", aceIp.getIpProtocol());
-                    matchesNode.put("source-port-range", this.getSourcePortRangeObjectNode(aceIp));
-                    matchesNode.put("destination-port-range", this.getDestinationPortRangeObjectNode(aceIp));
+                    matchesNode.put(_IP_PROTOCOL, aceIp.getIpProtocol());
+                    matchesNode.put(_SOURCE_PORT_RANGE, this.getSourcePortRangeObjectNode(aceIp));
+                    matchesNode.put(_DESTINATION_PORT_RANGE, this.getDestinationPortRangeObjectNode(aceIp));
                     matchesNode = this.getAceIpVersionObjectNode(aceIp, matchesNode);
                     break;
-                case "AceEth":
+                case ACE_ETH:
                     AceEth aceEth = (AceEth) matches.getAceType();
                     if (aceEth.getDestinationMacAddress() != null) {
-                        matchesNode.put("destination-mac-address", aceEth.getDestinationMacAddress().getValue());
+                        matchesNode.put(_DESTINATION_MAC_ADDRESS, aceEth.getDestinationMacAddress().getValue());
                     }
                     if (aceEth.getDestinationMacAddressMask() != null) {
-                        matchesNode.put("destination-mac-address-mask", aceEth.getDestinationMacAddressMask().getValue());
+                        matchesNode.put(_DESTINATION_MAC_ADDRESS_MASK, aceEth.getDestinationMacAddressMask().getValue());
                     }
                     if (aceEth.getSourceMacAddress() != null) {
-                        matchesNode.put("source-mac-address", aceEth.getSourceMacAddress().getValue());
+                        matchesNode.put(_SOURCE_MAC_ADDRESS, aceEth.getSourceMacAddress().getValue());
                     }
                     if (aceEth.getSourceMacAddressMask() != null) {
-                        matchesNode.put("source-mac-address-mask", aceEth.getSourceMacAddressMask().getValue());
+                        matchesNode.put(_SOURCE_MAC_ADDRESS_MASK, aceEth.getSourceMacAddressMask().getValue());
                     }
                     break;
             }
@@ -142,15 +192,16 @@ class AclExporter implements Exporter {
             return null;
         }
 
-        ObjectNode sourcePortRangeNode = mapper.createObjectNode();
+        ObjectNode sourcePortRangeNode = null;
 
         SourcePortRange sourcePortRange = aceIp.getSourcePortRange();
         if (sourcePortRange != null) {
+            sourcePortRangeNode = mapper.createObjectNode();
             if (sourcePortRange.getLowerPort() != null) {
-                sourcePortRangeNode.put("lower-port", sourcePortRange.getLowerPort().getValue());
+                sourcePortRangeNode.put(_LOWER_PORT, sourcePortRange.getLowerPort().getValue());
             }
             if (sourcePortRange.getUpperPort() != null) {
-                sourcePortRangeNode.put("upper-port", sourcePortRange.getUpperPort().getValue());
+                sourcePortRangeNode.put(_UPPER_PORT, sourcePortRange.getUpperPort().getValue());
             }
         }
 
@@ -162,15 +213,16 @@ class AclExporter implements Exporter {
             return null;
         }
 
-        ObjectNode destinationPortRangeNode = mapper.createObjectNode();
+        ObjectNode destinationPortRangeNode = null;
 
         DestinationPortRange destinationPortRange = aceIp.getDestinationPortRange();
         if (destinationPortRange != null) {
+            destinationPortRangeNode = mapper.createObjectNode();
             if (destinationPortRange.getLowerPort() != null) {
-                destinationPortRangeNode.put("lower-port", destinationPortRange.getLowerPort().getValue());
+                destinationPortRangeNode.put(_LOWER_PORT, destinationPortRange.getLowerPort().getValue());
             }
             if (destinationPortRange.getUpperPort() != null) {
-                destinationPortRangeNode.put("upper-port", destinationPortRange.getUpperPort().getValue());
+                destinationPortRangeNode.put(_UPPER_PORT, destinationPortRange.getUpperPort().getValue());
             }
         }
 
@@ -186,25 +238,25 @@ class AclExporter implements Exporter {
             String aceIpVersion = aceIp.getAceIpVersion().getImplementedInterface().getSimpleName();
 
             switch (aceIpVersion) {
-                case "AceIpv4":
+                case ACE_IPV4:
                     AceIpv4 aceIpv4 = (AceIpv4) aceIp.getAceIpVersion();
                     if (aceIpv4.getDestinationIpv4Address() != null) {
-                        matchesNode.put("destination-ipv4-address", aceIpv4.getDestinationIpv4Address().getValue());
+                        matchesNode.put(_DESTINATION_IPV4_ADDRESS, aceIpv4.getDestinationIpv4Address().getValue());
                     }
                     if (aceIpv4.getSourceIpv4Address() != null) {
-                        matchesNode.put("source-ipv4-address", aceIpv4.getSourceIpv4Address().getValue());
+                        matchesNode.put(_SOURCE_IPV4_ADDRESS, aceIpv4.getSourceIpv4Address().getValue());
                     }
                     break;
-                case "AceIpv6":
+                case ACE_IPV6:
                     AceIpv6 aceIpv6 = (AceIpv6) aceIp.getAceIpVersion();
                     if (aceIpv6.getDestinationIpv6Address() != null) {
-                        matchesNode.put("destination-ipv6-address", aceIpv6.getDestinationIpv6Address().getValue());
+                        matchesNode.put(_DESTINATION_IPV6_ADDRESS, aceIpv6.getDestinationIpv6Address().getValue());
                     }
                     if (aceIpv6.getSourceIpv6Address() != null) {
-                        matchesNode.put("source-ipv6-address", aceIpv6.getSourceIpv6Address().getValue());
+                        matchesNode.put(_SOURCE_IPV6_ADDRESS, aceIpv6.getSourceIpv6Address().getValue());
                     }
                     if (aceIpv6.getFlowLabel() != null) {
-                        matchesNode.put("flow-label", aceIpv6.getFlowLabel().getValue());
+                        matchesNode.put(_FLOW_LABEL, aceIpv6.getFlowLabel().getValue());
                     }
                     break;
             }
@@ -224,14 +276,14 @@ class AclExporter implements Exporter {
             String actionType = actions.getPacketHandling().getImplementedInterface().getSimpleName();
 
             switch (actionType) {
-                case "deny":
-                    actionsNode.put("deny", "");
+                case DENY:
+                    actionsNode.put(_DENY, "");
                     break;
-                case "permit":
-                    actionsNode.put("permit", "");
+                case PERMIT:
+                    actionsNode.put(_PERMIT, "");
                     break;
                 default:
-                    actionsNode.put("deny", "");
+                    actionsNode.put(_DENY, "");
                     break;
             }
         }
@@ -244,13 +296,17 @@ class AclExporter implements Exporter {
                 String sfcActionType = sfcAction.getImplementedInterface().getSimpleName();
 
                 switch (sfcActionType) {
-                    case "AclRenderedServicePath":
+                    case ACL_RENDERED_SERVICE_PATH:
                         AclRenderedServicePath aclRenderedServicePath = (AclRenderedServicePath) sfcAction;
-                        actionsNode.put("service-function-acl:rendered-service-path",
+                        actionsNode.put(_SERVICE_FUNCTION_ACL_RENDERED_SERVICE_PATH,
                                 aclRenderedServicePath.getRenderedServicePath());
                         break;
                 }
             }
+        }
+
+        if (actionsNode.size() == 0) {
+            return null;
         }
 
         return actionsNode;
@@ -261,10 +317,11 @@ class AclExporter implements Exporter {
             return null;
         }
 
-        ObjectNode aceOperDataNode = mapper.createObjectNode();
+        ObjectNode aceOperDataNode = null;
 
         if (aceOperData.getMatchCounter() != null) {
-            aceOperDataNode.put("match-counter", aceOperData.getMatchCounter().getValue().longValue());
+            aceOperDataNode = mapper.createObjectNode();
+            aceOperDataNode.put(_MATCH_COUNTER, aceOperData.getMatchCounter().getValue().longValue());
         }
 
         return aceOperDataNode;
@@ -277,7 +334,7 @@ class AclExporter implements Exporter {
 
         ObjectNode defaultActionsNode = mapper.createObjectNode();
 
-        defaultActionsNode.put("deny", defaultActions.isDeny());
+        defaultActionsNode.put(_DENY, defaultActions.isDeny());
 
         return defaultActionsNode;
     }

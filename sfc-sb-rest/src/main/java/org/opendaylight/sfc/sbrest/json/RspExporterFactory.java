bd1b0dfed3592129563e92540ffe36c1cd5bd247
Andrej Kincel <akincel@cisco.com>
2015-02-12 08:35:46 +0100

    SB REST - JSON is generated without null values
    + JSON generated by Exporters is not containing pairs of key:null
    anymore. This makes the work of target SB devices (e.g. sfc_agent) easier.
    In other words, JSON now contains only valid key:value pairs.
    
    + Fixed exportJsonNameOnly to return an JSON array as top level (the same
    behaviour as exportJson). In fact exportJsonNameOnly is not necessary
    anymore.
    
    + Added test for AcLExporter
    + Added test for RspExporter + fixed HOP service_index key
    
    Change-Id: I0e78dc10cee50ae3bd526fe8b462934097c0002d
    Signed-off-by: Andrej Kincel <akincel@cisco.com>

--- sfc-sb-rest/src/main/java/org/opendaylight/sfc/sbrest/json/RspExporterFactory.java
+++ sfc-sb-rest/src/main/java/org/opendaylight/sfc/sbrest/json/RspExporterFactory.java
@@ -1,10 +1,13 @@
 package org.opendaylight.sfc.sbrest.json;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.rsp.rev140701.rendered.service.paths.RenderedServicePath;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.rsp.rev140701.rendered.service.paths.rendered.service.path.RenderedServicePathHop;
 import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -15,41 +18,64 @@ public class RspExporterFactory implements ExporterFactory {
     }
 }
 
-class RspExporter implements Exporter {
+class RspExporter extends AbstractExporter implements Exporter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RspExporter.class);
+
+    public static final String _RENDERED_SERVICE_PATH = "rendered-service-path";
+    public static final String _NAME = "name";
+    public static final String _CONTEXT_METADATA = "context-metadata";
+    public static final String _PATH_ID = "path-id";
+    public static final String _PARENT_SERVICE_FUNCTION_PATH = "parent-service-function-path";
+    public static final String _SERVICE_CHAIN_NAME = "service-chain-name";
+    public static final String _STARTING_INDEX = "starting-index";
+    public static final String _VARIABLE_METADATA = "variable-metadata";
+    public static final String _HOP_NUMBER = "hop-number";
+    public static final String _SERVICE_FUNCTION_FORWARDER = "service-function-forwarder";
+    public static final String _SERVICE_FUNCTION_NAME = "service-function-name";
+    public static final String _SERVICE_INDEX = "service_index";
+    public static final String _RENDERED_SERVICE_PATH_HOP = "rendered-service-path-hop";
 
     @Override
     public String exportJson(DataObject dataObject) {
 
-        String ret;
+        String ret = null;
         if (dataObject instanceof RenderedServicePath) {
             RenderedServicePath rsp = (RenderedServicePath) dataObject;
             ArrayNode rspArray = mapper.createArrayNode();
 
             ObjectNode node = mapper.createObjectNode();
-            node.put("context-metadata", rsp.getContextMetadata());
-            node.put("name", rsp.getName());
-            node.put("path-id", rsp.getPathId());
-            node.put("parent-service-function-path", rsp.getParentServiceFunctionPath());
-            node.put("service-chain-name", rsp.getServiceChainName());
-            node.put("starting-index", rsp.getStartingIndex());
-            node.put("variable-metadata", rsp.getVariableMetadata());
+            node.put(_CONTEXT_METADATA, rsp.getContextMetadata());
+            node.put(_NAME, rsp.getName());
+            node.put(_PATH_ID, rsp.getPathId());
+            node.put(_PARENT_SERVICE_FUNCTION_PATH, rsp.getParentServiceFunctionPath());
+            node.put(_SERVICE_CHAIN_NAME, rsp.getServiceChainName());
+            node.put(_STARTING_INDEX, rsp.getStartingIndex());
+            node.put(_VARIABLE_METADATA, rsp.getVariableMetadata());
 
             List<RenderedServicePathHop> hopList = rsp.getRenderedServicePathHop();
             if (hopList != null) {
                 ArrayNode hopArray = mapper.createArrayNode();
                 for (RenderedServicePathHop e : hopList) {
                     ObjectNode o = mapper.createObjectNode();
-                    o.put("hop-number", e.getHopNumber());
-                    o.put("service-function-forwarder", e.getServiceFunctionForwarder());
-                    o.put("service-function-name", e.getServiceFunctionName());
-                    o.put("service-index", e.getServiceIndex());
+                    o.put(_HOP_NUMBER, e.getHopNumber());
+                    o.put(_SERVICE_FUNCTION_FORWARDER, e.getServiceFunctionForwarder());
+                    o.put(_SERVICE_FUNCTION_NAME, e.getServiceFunctionName());
+                    o.put(_SERVICE_INDEX, e.getServiceIndex());
                     hopArray.add(o);
                 }
-                node.putArray("rendered-service-path-hop").addAll(hopArray);
+                node.putArray(_RENDERED_SERVICE_PATH_HOP).addAll(hopArray);
             }
 
             rspArray.add(node);
-            ret = "{ \"rendered-service-path\" : " + rspArray.toString() + " }";
+            try {
+                Object rspObject = mapper.treeToValue(rspArray, Object.class);
+                ret = mapper.writeValueAsString(rspObject);
+                ret = "{\"" + _RENDERED_SERVICE_PATH + "\":" + ret + "}";
+                LOG.debug("Created Rendered Service Path JSON: {}", ret);
+            } catch (JsonProcessingException e) {
+                LOG.error("Error during creation of JSON for Rendered Service Path {}", rsp.getName());
+            }
 
         } else {
             throw new IllegalArgumentException("Argument is not an instance of RenderedServicePath");
@@ -67,7 +93,9 @@ class RspExporter implements Exporter {
 
             ObjectNode node = mapper.createObjectNode();
             node.put("name", obj.getName());
-            ret = "{ \"rendered-service-path\" : " + node.toString() + " }";
+            ArrayNode rspArray = mapper.createArrayNode();
+            rspArray.add(node);
+            ret = "{\"" + _RENDERED_SERVICE_PATH + "\":" + rspArray.toString() + "}";
         } else {
             throw new IllegalArgumentException("Argument is not an instance of RenderedServicePath");
         }

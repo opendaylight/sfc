8ee6674bebe4d494fd168a830e66bc691047dbe0
Andrej Kincel <akincel@cisco.com>
2015-02-12 08:35:46 +0100

    SB REST - Exporters refactor & unit test (do not merge yet)
    + JSON generated by Exporters is not containing pairs of key:null
    anymore. This makes the work of target SB devices (e.g. sfc_agent) easier.
    In other words, JSON now contains only valid key:value pairs.
    
    + Fixed exportJsonNameOnly to return an JSON array as top level (the same
    behaviour as exportJson). In fact exportJsonNameOnly is not necessary
    anymore.
    
    + Major refactor of Utils, SfExporter and SffExporter
    
    + Added test for AcLExporter
    + Added test for RspExporter
    + Added test fpr SfExporter
    
    + Fixed service_index key in sfc_agent.py
    
    Change-Id: I0e78dc10cee50ae3bd526fe8b462934097c0002d
    Signed-off-by: Andrej Kincel <akincel@cisco.com>

--- sfc-sb-rest/src/main/java/org/opendaylight/sfc/sbrest/json/Exporter.java
+++ sfc-sb-rest/src/main/java/org/opendaylight/sfc/sbrest/json/Exporter.java
@@ -1,11 +1,8 @@
 package org.opendaylight.sfc.sbrest.json;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 
 public interface Exporter {
-    ObjectMapper mapper = new ObjectMapper();
-
     String exportJson(DataObject dataObject);
     String exportJsonNameOnly(DataObject dataObject);
 }
--- sfc-sb-rest/src/main/java/org/opendaylight/sfc/sbrest/json/SfExporterFactory.java
+++ sfc-sb-rest/src/main/java/org/opendaylight/sfc/sbrest/json/SfExporterFactory.java
@@ -1,10 +1,21 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package org.opendaylight.sfc.sbrest.json;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.function.entry.SfDataPlaneLocator;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.functions.ServiceFunction;
 import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SfExporterFactory implements ExporterFactory {
     @Override
@@ -13,39 +24,60 @@ public class SfExporterFactory implements ExporterFactory {
     }
 }
 
-class SfExporter implements Exporter {
+class SfExporter extends AbstractExporter implements Exporter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SfExporter.class);
+
+    public static final String _SERVICE_FUNCTION = "service-function";
+    public static final String _NAME = "name";
+    public static final String _TYPE = "type";
+    public static final String _REST_URI = "rest-uri";
+    public static final String _IP_MGMT_ADDRESS = "ip-mgmt-address";
+    public static final String _REQUEST_RECLASSIFICATION = "request_reclassification";
+    public static final String _NSH_AWARE = "nsh-aware";
+    public static final String _SF_DATA_PLANE_LOCATOR = "sf-data-plane-locator";
+    public static final String _SERVICE_FUNCTION_FORWARDER = "service-function-forwarder";
+
+    public static final String SERVICE_FUNCTION_TYPE_PREFIX = "service-function-type:";
 
     @Override
     public String exportJson(DataObject dataObject) {
 
-        String ret;
+        String ret = null;
         if (dataObject instanceof ServiceFunction) {
             ServiceFunction sf = (ServiceFunction) dataObject;
 
             ArrayNode sfArray = mapper.createArrayNode();
-
-            ObjectNode node = mapper.createObjectNode();
-            node.put("name", sf.getName());
-            node.put("ip-mgmt-address", Util.convertIpAddress(sf.getIpMgmtAddress()));
-            if (sf.getRestUri() != null)
-                node.put("rest-uri", sf.getRestUri().getValue());
-            if (sf.getType() != null)
-                node.put("type", "service-function-type:" + sf.getType().getSimpleName().toLowerCase());
-            node.put("nsh-aware", sf.isNshAware());
-            node.put("request-reclassification", sf.isRequestReclassification());
+            ObjectNode sfNode = mapper.createObjectNode();
+            sfNode.put(_NAME, sf.getName());
+            sfNode.put(_IP_MGMT_ADDRESS, Util.convertIpAddress(sf.getIpMgmtAddress()));
+            if (sf.getRestUri() != null) {
+                sfNode.put(_REST_URI, sf.getRestUri().getValue());
+            }
+            if (sf.getType() != null) {
+                sfNode.put(_TYPE, SERVICE_FUNCTION_TYPE_PREFIX + sf.getType().getSimpleName().toLowerCase());
+            }
+            sfNode.put(_NSH_AWARE, sf.isNshAware());
+            sfNode.put(_REQUEST_RECLASSIFICATION, sf.isRequestReclassification());
 
             if (sf.getSfDataPlaneLocator() != null) {
                 ArrayNode locatorArray = mapper.createArrayNode();
-                for (SfDataPlaneLocator e : sf.getSfDataPlaneLocator()) {
-                    ObjectNode o = Util.ObjectNodeFromSfDataPlaneLocator(e);
-                    locatorArray.add(o);
+                for (SfDataPlaneLocator sfDataPlaneLocator : sf.getSfDataPlaneLocator()) {
+                    ObjectNode sfLocatorNode = this.getObjectNodeFromSfDataPlaneLocator(sfDataPlaneLocator);
+                    locatorArray.add(sfLocatorNode);
                 }
-                node.putArray("sf-data-plane-locator").addAll(locatorArray);
+                sfNode.putArray(_SF_DATA_PLANE_LOCATOR).addAll(locatorArray);
             }
 
-            sfArray.add(node);
-
-            ret = "{ \"service-function\" : " + sfArray.toString() + " }";
+            sfArray.add(sfNode);
+            try {
+                Object sfObject = mapper.treeToValue(sfArray, Object.class);
+                ret = mapper.writeValueAsString(sfObject);
+                ret = "{\"" + _SERVICE_FUNCTION + "\":" + ret + "}";
+                LOG.debug("Created Service Function JSON: {}", ret);
+            } catch (JsonProcessingException e) {
+                LOG.error("Error during creation of JSON for Service Function {}", sf.getName());
+            }
 
         } else {
             throw new IllegalArgumentException("Argument is not an instance of ServiceFunction");
@@ -62,12 +94,27 @@ class SfExporter implements Exporter {
             ServiceFunction obj = (ServiceFunction) dataObject;
 
             ObjectNode node = mapper.createObjectNode();
-            node.put("name", obj.getName());
-            ret = "{ \"service-function\" : " + node.toString() + " }";
+            node.put(_NAME, obj.getName());
+            ArrayNode sfArray = mapper.createArrayNode();
+            sfArray.add(node);
+            ret = "{\"" + _SERVICE_FUNCTION + "\":" + sfArray.toString() + "}";
         } else {
             throw new IllegalArgumentException("Argument is not an instance of ServiceFunction");
         }
 
         return ret;
+    }
+
+    private ObjectNode getObjectNodeFromSfDataPlaneLocator(SfDataPlaneLocator locator) {
+        ObjectNode sfLocatorNode = Util.getDataPlaneLocatorObjectNode(locator);
+
+        if (sfLocatorNode == null) {
+            sfLocatorNode = mapper.createObjectNode();
+        }
+
+        sfLocatorNode.put(_NAME, locator.getName());
+        sfLocatorNode.put(_SERVICE_FUNCTION_FORWARDER, locator.getServiceFunctionForwarder());
+
+        return sfLocatorNode;
     }
 }

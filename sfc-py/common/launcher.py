8e41e3888e908a1f7edcbb332e2d0c489f6beb14
Dusan Madar <dumadar@cisco.com>
2015-02-16 11:50:43 +0100

    SFC-PY - start refactor
    Add 'common' package, refactor sf/sff_thread.py, basic sfc_agent.py refactor.
    
    Change-Id: I7e5e5abe28e5f531f49bf4708e8d483c41e21ca6
    Signed-off-by: Dusan Madar <dumadar@cisco.com>

---
+++ sfc-py/common/launcher.py
@@ -0,0 +1,130 @@
+__author__ = "Jim Guichard, Reinaldo Penno"
+__copyright__ = "Copyright(c) 2014, Cisco Systems, Inc."
+__version__ = "0.2"
+__email__ = "jguichar@cisco.com, rapenno@gmail.com"
+__status__ = "alpha"
+
+#
+# Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License v1.0 which accompanies this distribution,
+# and is available at http://www.eclipse.org/legal/epl-v10.html
+
+"""
+TODO: edit this
+"""
+
+
+import socket
+import logging
+import asyncio
+
+from common.services import UDP, CUDP, find_service
+
+logger = logging.getLogger(__name__)
+
+
+def start_server(loop, addr, udpserver, message):
+    """
+    TODO: add docstring
+
+    :param loop:
+    :type loop: `:class:asyncio.unix_events._UnixSelectorEventLoop`
+    :param addr: IP address and port
+    :type addr: tuple (str, int)
+    :param udpserver: UDP server instance
+    :type udpserver: `:class:ControlUdpServer`
+    :param message: message which will displayed upon server start
+    :type message: str
+
+    :return `:class:asyncio.selector_events._SelectorDatagramTransport`
+
+    """
+    #t = asyncio.Task(loop.create_datagram_endpoint(
+    #    lambda: udpserver, local_addr=addr))
+    listen = loop.create_datagram_endpoint(lambda: udpserver, local_addr=addr)
+    transport = loop.run_until_complete(listen)[0]
+    logger.info(message, addr)
+
+    return transport
+
+
+def start_service(service_name, service_ip, service_port, service_type,
+                  service_control_port, service_thread, service_start_message):
+    """
+    Start a service.
+
+    The Python agent uses this function as the thread start whenever it wants
+    to create a service.
+
+    :param service_name: SF name
+    :type service_name: str
+    :param service_ip: SF IP address
+    :type service_ip: str
+    :param service_port: SF port
+    :type service_port: int
+    :param service_type: SF type
+    :type service_type: str
+    :param service_control_port: SF control port
+    :type service_control_port: int
+    :param service_thread:
+    :type service_thread: `:class:threading.Thread`
+    :param service_start_message: what will be logged upon a service start
+    :type service_start_message: str
+
+    """
+    # TODO: is this even necessary?
+    udpserver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    logging.basicConfig(level=logging.INFO)
+
+    service_class = find_service(service_type)
+    logger.info('Starting %s service ...', service_type.upper())
+
+    service = service_class(loop)
+    udpserver_transport = start_server(loop,
+                                       (service_ip, service_port),
+                                       service,
+                                       service_start_message)
+
+    udpserver_socket = udpserver_transport.get_extra_info('socket')
+    service_thread[service_name]['socket'] = udpserver_socket
+
+    control_udp_server = find_service(CUDP)(loop)
+    start_server(loop,
+                 (service_ip, service_control_port),
+                 control_udp_server,
+                 'Listening for Control messages on port: ')
+
+    loop.run_forever()
+    udpserver_socket.close()
+    loop.close()
+
+
+def start_sf(sf_name, sf_ip, sf_port, sf_type, sf_control_port, sf_thread):
+    """
+    Start a Service Function
+    """
+    logger.info('Starting Service Function')
+    start_service(sf_name, sf_ip, sf_port, sf_type, sf_control_port,
+                  sf_thread, 'Starting new Service Function ...')
+
+
+def start_sff(sff_name, sff_ip, sff_port, sff_control_port, sff_thread):
+    """
+    Start a Service Function Forwarder
+    """
+    logger.info('Starting Service Function Forwarder')
+    start_service(sff_name, sff_ip, sff_port, UDP, sff_control_port,
+                  sff_thread, "Listening for NSH packets on port: ")
+
+# This does not work in MacOS when SFF/SF are different python
+# applications on the same machine
+# def start_server(loop, addr, service, myip):
+#     t = asyncio.Task(loop.create_datagram_endpoint(
+#         service, local_addr=(myip, 57444), remote_addr=addr))
+#     loop.run_until_complete(t)
+#     print('Listening for packet on:', addr)
---
+++ sfc-py/common/services.py
@@ -0,0 +1,287 @@
+__author__ = "Jim Guichard, Reinaldo Penno"
+__copyright__ = "Copyright(c) 2014, Cisco Systems, Inc."
+__version__ = "0.2"
+__email__ = "jguichar@cisco.com, rapenno@gmail.com"
+__status__ = "alpha"
+
+#
+# Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License v1.0 which accompanies this distribution,
+# and is available at http://www.eclipse.org/legal/epl-v10.html
+
+"""All supported services, spawned in a thread by sfc_agent.py"""
+
+import logging
+import asyncio
+import binascii
+
+import nsh.decode as nsh_decode
+from common.sff_globals import agent_globals
+from nsh.service_index import process_service_index
+
+logger = logging.getLogger(__name__)
+
+#: Decode vxlan-gpe, base NSH header and NSH context headers
+server_vxlan_values = nsh_decode.VXLANGPE()
+server_ctx_values = nsh_decode.CONTEXTHEADER()
+server_base_values = nsh_decode.BASEHEADER()
+
+#: Global flags used for indication of current packet processing status
+# Packet needs more processing within this SFF
+PACKET_CHAIN = 0b00000000
+# Packet was sent to another SFF or service function
+PACKET_CONSUMED = 0b00000001
+# Packet will be dropped
+PACKET_ERROR = 0b00000010
+# Referenced service function is invalid
+SERVICE_HOP_INVALID = 0xDEADBEEF
+
+#: Services names
+FWL = 'firewall'
+NAT = 'nat'  # TODO: should this be `napt44` or just `nat`?
+DPI = 'dpi'
+QOS = 'qos'
+IDS = 'ids'
+UDP = 'udp'
+CUDP = 'cudp'
+
+
+def find_service(service_type):
+    """Service dispatcher - get service class based on its type
+
+    :param service_type: service type
+    :type service_type: str
+
+    :return `:class:Baseservice`
+
+    """
+    if service_type == FWL:
+        return MyFwService
+    elif service_type == NAT:
+        return MyNatService
+    elif service_type == DPI:
+        return MyDpiService
+    elif service_type == UDP:
+        return MyUdpServer
+    elif service_type == CUDP:
+        return ControlUdpServer
+    elif service_type == QOS or service_type == IDS:
+        # return a generic service for currently unimplemented services
+        return MyService
+    else:
+        raise ValueError('Service "%s" not supported' % service_type)
+
+
+class BasicService(object):
+    def __init__(self, loop):
+        """
+        Service Blueprint Class
+        TODO: add arguments description
+        """
+        self.loop = loop
+        self.transport = None
+
+        # MUST be set by EACH descendant class
+        self.service_type = None
+
+    def _decode_headers(self, data):
+        """
+        Procedure for decoding packet headers.
+
+        Decode the incoming packet for debug purposes and to strip out various
+        header values.
+
+        """
+        # decode vxlan-gpe header
+        nsh_decode.decode_vxlan(data, server_vxlan_values)
+        # decode NSH base header
+        nsh_decode.decode_baseheader(data, server_base_values)
+        # decode NSH context headers
+        nsh_decode.decode_contextheader(data, server_ctx_values)
+
+    def _process_incoming_packet(self, data):
+        """
+        TODO: add docstring, params descirptions
+        """
+        logger.info('Processing received packet')
+
+        self._decode_headers(data)
+
+        # TODO: should't this be converted before applying _decode_headers()
+        # like in MyUdpServer?
+        rw_data = bytearray(data)
+        rw_data = process_service_index(rw_data, server_base_values)[0]
+
+        return rw_data
+
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def datagram_received(self, data, addr):
+        """
+        TODO: add docstring
+
+        :param data:
+        :type data: bytes
+        :param addr: IP address and port to which data are passed
+        :type addr: tuple (str, int)
+
+        """
+        logger.info('%s service received packet from SFF:', self.service_type)
+        logger.info(addr, binascii.hexlify(data))
+        logger.info('Sending packets to %s', addr)
+
+        rw_data = self._process_incoming_packet(data)
+        self.transport.sendto(rw_data, addr)
+
+    def connection_refused(self, exc):
+        logger.error('Connection refused: %s', exc)
+
+    def connection_lost(self, exc):
+        logger.warning('Closing transport', exc)
+        loop = asyncio.get_event_loop()
+        loop.stop()
+
+
+class MyService(BasicService):
+    def __init__(self, loop):
+        super(MyService, self).__init__(loop)
+
+        self.service_type = 'generic'
+
+
+class MyFwService(BasicService):
+    def __init__(self, loop):
+        super(MyFwService, self).__init__(loop)
+
+        self.service_type = FWL
+
+
+class MyNatService(BasicService):
+    def __init__(self, loop):
+        super(MyNatService, self).__init__(loop)
+
+        self.service_type = NAT
+
+
+class MyDpiService(BasicService):
+    def __init__(self, loop):
+        super(MyDpiService, self).__init__(loop)
+
+        self.service_type = DPI
+
+
+class ControlUdpServer(BasicService):
+    def __init__(self, loop):
+        """
+        This control server class listen on a socket for commands from the main
+        process. For example, if a SFF is deleted the main program can send a
+        command to this data plane thread to exit.
+        """
+        super(ControlUdpServer, self).__init__(loop)
+
+        self.service_type = 'Control UDP Server'
+
+    def datagram_received(self, data, addr):
+        logger.info('%s received a packet from: %s', self.service_type, addr)
+        self.loop.call_soon_threadsafe(self.loop.stop)
+        #data = data.decode('utf-8')
+        #print(data_plane_path)
+        #sfp_topo = json.loads(data)
+        #print(sfp_topo)
+        #print(sfp_topo['3']['3'])
+
+    def connection_lost(self, exc):
+        logger.error('stop: %s', exc)
+
+
+class MyUdpServer(BasicService):
+    def __init__(self, loop):
+        """
+        This is the main UDP server. It receives VXLAN GPE packets, calls
+        packet processing function and finally sends them on their way
+        """
+        super(MyUdpServer, self).__init__(loop)
+
+        self.service_type = 'UDP Server'
+
+    def _lookup_next_sf(self, service_path, service_index):
+        """
+        TODO:
+        """
+        next_hop = SERVICE_HOP_INVALID
+
+        # First we determine the list of SFs in the received packet based on
+        # SPI value extracted from packet
+        try:
+            local_data_plane_path = agent_globals.get_data_plane_path()
+            next_hop = local_data_plane_path[service_path][service_index]
+        except KeyError:
+            msg = ('Could not determine next service hop. SP: %d, SI: %d' %
+                   (service_path, service_index))
+
+            logger.error(msg)
+
+        return next_hop
+
+    def _process_incoming_packet(self, data, addr):
+        """
+        TODO:
+        """
+        logger.info("Processing packet from: %s", addr)
+
+        address = ()
+
+        # Copy payload into bytearray so it can be changed
+        rw_data = bytearray(data)
+        self._decode_headers(data)
+
+        # Lookup what to do with the packet based on Service Path Identifier
+        # (SPI)
+
+        if server_base_values.service_index:
+            logger.info('Looking up next service hop ...')
+
+            next_hop = self._lookup_next_sf(server_base_values.service_path,
+                                            server_base_values.service_index)
+            if next_hop != SERVICE_HOP_INVALID:
+                address = next_hop['ip'], next_hop['port']
+            else:
+                # bye, bye packet
+                logger.info("End of path")
+                logger.info("Packet dump: %s", binascii.hexlify(rw_data))
+                logger.info('service index end up as: %d',
+                            server_base_values.service_index)
+
+                # Remove all SFC headers, leave only original packet
+                rw_data = nsh_decode.PAYLOAD_START_INDEX
+        else:
+            logger.error("Loop Detected")
+            logger.error("Packet dump: %s", binascii.hexlify(rw_data))
+
+            rw_data.__init__()
+            data = ""
+
+        logger.info('Finished processing packet from: %s', addr)
+        return rw_data, address
+
+    def datagram_received(self, data, addr):
+        """
+        TODO:
+        """
+        logger.info('Received a packet from: %s', addr)
+
+        rw_data, address = self._process_incoming_packet(data, addr)
+        if address:
+            logger.info("Sending packets to: %s", address)
+            # send the packet to the next SFF based on address
+            self.transport.sendto(rw_data, address)
+            logger.info("listening for NSH packets ...")
+
+    def connection_lost(self, exc):
+        logger.error('stop', exc)
+
+    def error_received(self, exc):
+        logger.error('Error received:', exc)

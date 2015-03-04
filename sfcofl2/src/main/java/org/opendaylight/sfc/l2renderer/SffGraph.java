d486b8936f03171fc3b4299dc592c97f83ced541
Brady Johnson <brady.allen.johnson@ericsson.com>
2015-03-04 14:27:01 +0100

    Populate the Rsp Hop SFF Ingress DPL
    Change-Id: I0ccd2d808ce1fb9a15f94f088ca4fafa1d8bf46a
    Signed-off-by: Brady Johnson <brady.allen.johnson@ericsson.com>

--- sfcofl2/src/main/java/org/opendaylight/sfc/l2renderer/SffGraph.java
+++ sfcofl2/src/main/java/org/opendaylight/sfc/l2renderer/SffGraph.java
@@ -1,8 +1,10 @@
 package org.opendaylight.sfc.l2renderer;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +12,8 @@ import org.slf4j.LoggerFactory;
 /**
  * This class stores a simple Connection Graph of how the
  * SFFs (Service Function Forwarders) are interconnected.
+ * Additionally, it stores SFF ingress and egress DPLs (Data Plane Locators)
+ * for a particular PathId
  *
  * @author Brady Johnson brady.allen.johnson@ericsson.com
  *
@@ -39,17 +43,44 @@ public class SffGraph {
         }
     }
 
+    /**
+     * Internal class to hold the ingress and egress DPLs for an SFF for a particular pathId
+     */
+    public class SffDataPlaneLocators {
+        private String sffName;
+        private long pathId;
+        private String ingressDplName;
+        private String egressDplName;
+
+        public SffDataPlaneLocators(String sffName, long pathId, String ingressDplName, String egressDplName) {
+            this.sffName = sffName;
+            this.pathId = pathId;
+            this.ingressDplName = ingressDplName;
+            this.egressDplName = egressDplName;
+        }
+        public String getSffName()        { return sffName;        }
+        public long getPathId()           { return pathId;         }
+        public String getIngressDplName() { return ingressDplName; }
+        public String getEgressDplName()  { return egressDplName;  }
+    }
+
     private List<SffGraphEntry> graphEntries;
+    private Map<Long, Map<String, SffDataPlaneLocators>> pathIdTosffDataPlaneLocators;
 
     public SffGraph() {
         graphEntries = new ArrayList<SffGraphEntry>();
+        pathIdTosffDataPlaneLocators = new HashMap<Long, Map<String, SffDataPlaneLocators>>();
     }
 
-    public void addEntry(final String srcSff, final String dstSff, long pathId) {
-        addEntry(srcSff, dstSff, null, pathId);
+    //
+    // Graph methods
+    //
+
+    public void addGraphEntry(final String srcSff, final String dstSff, long pathId) {
+        addGraphEntry(srcSff, dstSff, null, pathId);
     }
 
-    public void addEntry(final String srcSff, final String dstSff, final String sf, long pathId) {
+    public void addGraphEntry(final String srcSff, final String dstSff, final String sf, long pathId) {
         SffGraphEntry entry = new SffGraphEntry(srcSff, dstSff, sf, pathId);
         graphEntries.add(entry);
 
@@ -59,5 +90,58 @@ public class SffGraph {
 
     public Iterator<SffGraphEntry> iterator() {
         return graphEntries.iterator();
+    }
+
+    //
+    // SFF DPL methods
+    //
+
+    public void addSffDpls(final String sffName, final long pathId, final String ingressDpl, final String egressDpl) {
+        SffDataPlaneLocators sffDpl = new SffDataPlaneLocators(sffName, pathId, ingressDpl, egressDpl);
+
+        Map<String, SffDataPlaneLocators> sffToDpls = pathIdTosffDataPlaneLocators.get(pathId);
+
+        LOG.info("SffGraphEntry addSffDpls sff [{}] path [{}] ingressDpl [{}] egressDpl [{}]",
+                sffName, pathId, ingressDpl, egressDpl);
+
+        if(sffToDpls == null) {
+            sffToDpls = new HashMap<String, SffDataPlaneLocators>();
+            pathIdTosffDataPlaneLocators.put(pathId, sffToDpls);
+        }
+
+        sffToDpls.put(sffName, sffDpl);
+    }
+
+    private SffDataPlaneLocators getSffDpl(final String sffName, final long pathId) {
+        Map<String, SffDataPlaneLocators> sffToDpls = pathIdTosffDataPlaneLocators.get(pathId);
+        if(sffToDpls == null) {
+            LOG.info("SffGraph getSffDpl cant find sffToDpls list for sff [{}] path [{}]", sffName, pathId);
+
+            return null;
+        }
+
+        return sffToDpls.get(sffName);
+    }
+
+    public String getSffIngressDpl(final String sffName, final long pathId) {
+        SffDataPlaneLocators sffDpl = getSffDpl(sffName, pathId);
+
+        if(sffDpl == null) {
+            LOG.info("SffGraph getSffIngressDpl cant find sffDpl for sff [{}] path [{}]", sffName, pathId);
+            return null;
+        }
+
+        return sffDpl.getIngressDplName();
+    }
+
+    public String getSffEgressDpl(final String sffName, final long pathId) {
+        SffDataPlaneLocators sffDpl = getSffDpl(sffName, pathId);
+
+        if(sffDpl == null) {
+            LOG.info("SffGraph getSffEgressDpl cant find sffDpl for sff [{}] path [{}]", sffName, pathId);
+            return null;
+        }
+
+        return sffDpl.getEgressDplName();
     }
 }

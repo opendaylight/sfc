172052fb6dc928e4d4a434d60242679d018db2cd
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-03-05 19:54:43 -0500

    Fix pep8 in odl2ovs_cli.py
    Change-Id: I2d20b0578e43d4fefc1fc141640e7ac470b429d5
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- sfc-py/unused/odl2ovs_cli.py
+++ sfc-py/unused/odl2ovs_cli.py
@@ -1,10 +1,3 @@
-__author__ = "Paul Quinn, Reinaldo Penno"
-__copyright__ = "Copyright(c) 2014, Cisco Systems, Inc."
-__version__ = "0.2"
-__email__ = "paulq@cisco.com, rapenno@gmail.com"
-__status__ = "alpha"
-
-
 #
 # Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
 #
@@ -14,15 +7,25 @@ __status__ = "alpha"
 
 import subprocess
 
+
+__author__ = "Paul Quinn, Reinaldo Penno"
+__copyright__ = "Copyright(c) 2014, Cisco Systems, Inc."
+__version__ = "0.2"
+__email__ = "paulq@cisco.com, rapenno@gmail.com"
+__status__ = "alpha"
+
+
 def cli_local(sfdpinfo, vxlan, key):
     sfi = 0
     if len(sfdpinfo) > 1:
         for sf in sfdpinfo:
-            part1 = 'ovs-vsctl add-port br-int ' + 'vxlan' + str(vxlan) + ' -- set interface vxlan' + str(vxlan) + ' options:dst_port=6633 type=vxlan options:remote_ip=' + sfdpinfo[sfi]['locator']
-            part2 = ' options:key=' + str(key) + ' options:nsp=' + str(sfdpinfo[sfi]['pid']) + ' options:nsi=' + str(sfdpinfo[sfi]['index'])
+            part1 = 'ovs-vsctl add-port br-int ' + 'vxlan' + str(vxlan) + ' -- set interface vxlan' + str(vxlan) + \
+                    ' options:dst_port=6633 type=vxlan options:remote_ip=' + sfdpinfo[sfi]['locator']
+            part2 = ' options:key=' + str(key) + ' options:nsp=' + str(sfdpinfo[sfi]['pid']) + ' options:nsi=' + \
+                    str(sfdpinfo[sfi]['index'])
             cli = part1 + part2
             print cli
-            #subprocess.call([cli], shell=True)
+            # subprocess.call([cli], shell=True)
             vxlan += 1
             sfi += 1
     else:
@@ -34,8 +37,10 @@ def ovs_cli_local(sfdpinfo, vxlan, key):
     sfi = 0
     if len(sfdpinfo) > 1:
         for sf in sfdpinfo:
-            part1 = 'ovs-vsctl add-port br-int ' + 'vxlan' + str(vxlan) + '-- set interface vxlan' + str(vxlan) + ' options:dst_port=6633 type=vxlan options:remote_ip=' + sfdpinfo[sfi]['locator']
-            part2 = ' options:key=' + str(key) + ' options:nsp=' + str(sfdpinfo[sfi]['pid']) + ' options:nsi=' + str(sfdpinfo[sfi]['index'])
+            part1 = 'ovs-vsctl add-port br-int ' + 'vxlan' + str(vxlan) + '-- set interface vxlan' + str(vxlan) + \
+                    ' options:dst_port=6633 type=vxlan options:remote_ip=' + sfdpinfo[sfi]['locator']
+            part2 = ' options:key=' + str(key) + ' options:nsp=' + str(sfdpinfo[sfi]['pid']) + ' options:nsi=' + \
+                    str(sfdpinfo[sfi]['index'])
             cli = part1 + part2
             subprocess.call([cli], shell=True)
             vxlan += 1
@@ -47,17 +52,20 @@ def ovs_cli_local(sfdpinfo, vxlan, key):
 
 
 def cli_nextsff(nextsffloc, nextsff, key, vxlan, pid):
-    part1 = 'ovs-vsctl add-port br-tun ' + 'vxlan' + str(vxlan) + ' -- set interface vxlan' + str(vxlan) + ' options:dst_port=6633 type=vxlan options:remote_ip=' + nextsffloc
-    part2 = ' options:key=' + str(key) + ' options:nsp=' + str(pid) + ' options:nsi=' + str(nextsff['sff-index'])
+    part1 = 'ovs-vsctl add-port br-tun ' + 'vxlan' + str(vxlan) + ' -- set interface vxlan' + str(vxlan) + \
+            ' options:dst_port=6633 type=vxlan options:remote_ip=' + nextsffloc
+    part2 = ' options:key=' + str(key) + ' options:nsp=' + str(pid) + ' options:nsi=' + \
+            str(nextsff['sff-index'])
     cli = part1 + part2
     print cli
-    #subprocess.call([cli], shell=True)
+    # subprocess.call([cli], shell=True)
     vxlan += 1
     return
 
 
 def ovs_cli_nextsff(nextsffloc, nextsff, key, vxlan, path):
-    part1 = 'ovs-vsctl add-port br-tun ' + 'vxlan' + str(vxlan) + ' -- set interface vxlan' + str(vxlan) + ' options:dst_port=6633 type=vxlan options:remote_ip=' + nextsffloc
+    part1 = 'ovs-vsctl add-port br-tun ' + 'vxlan' + str(vxlan) + ' -- set interface vxlan' + str(vxlan) + \
+            ' options:dst_port=6633 type=vxlan options:remote_ip=' + nextsffloc
     part2 = ' options:key=' + str(key) + ' options:nsp=' + str(path) + ' options:nsi=' + str(nextsff['sff-index'])
     cli = part1 + part2
 

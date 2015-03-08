e66c68f026a9106423d71d3ad791e420cfe0bd6b
shlomi <shlomi.alfasi@contextream.com>
2015-03-08 21:03:39 +0200

    adding SFG support
    Signed-off-by: shlomi <shlomi.alfasi@contextream.com>

---
+++ sfc-provider/src/main/java/org/opendaylight/sfc/provider/SfcProviderSfgEntryDataListener.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.sfc.provider;
+
+
+import static org.opendaylight.sfc.provider.SfcProviderDebug.printTraceStart;
+import static org.opendaylight.sfc.provider.SfcProviderDebug.printTraceStop;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sfg.rev160202.service.function.groups.ServiceFunctionGroup;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * This class gets called whenever there is a change to
+ * a Service Function list entry, i.e.,
+ * added/deleted/modified.
+ *
+ * <p
+ * @author Reinaldo Penno (rapenno@gmail.com)
+ * @version 0.1
+ * @since       2014-06-30
+ */
+public class SfcProviderSfgEntryDataListener implements DataChangeListener  {
+    private static final Logger LOG = LoggerFactory.getLogger(SfcProviderSfgEntryDataListener.class);
+    private final OpendaylightSfc odlSfc = OpendaylightSfc.getOpendaylightSfcObj();
+
+
+    /**
+     * This method is called whenever there is change in a SF. Before doing any changes
+     * it takes a global lock in order to ensure it is the only writer.
+     * @param change
+     */
+    @Override
+    public void onDataChanged(
+            final AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> change ) {
+
+        printTraceStart(LOG);
+
+        odlSfc.getLock();
+
+        // SF ORIGINAL
+        Map<InstanceIdentifier<?>, DataObject> dataOriginalDataObject = change.getOriginalData();
+
+        for (Map.Entry<InstanceIdentifier<?>, DataObject> entry : dataOriginalDataObject.entrySet()) {
+            if( entry.getValue() instanceof  ServiceFunctionGroup) {
+//                ServiceFunction originalServiceFunction = (ServiceFunction) entry.getValue();
+//                LOG.debug("\n########## getOriginalConfigurationData {}  {}",
+//                        originalServiceFunction.getType(), originalServiceFunction.getName());
+//                SfcProviderServiceFunctionAPI.putServiceFunctionDescriptionExecutor(originalServiceFunction);
+//                SfcProviderServiceFunctionAPI.putServiceFunctionMonitorExecutor(originalServiceFunction);
+            }
+        }
+
+        // SFG CREATION
+        Map<InstanceIdentifier<?>, DataObject> dataCreatedObject = change.getCreatedData();
+
+        for (Map.Entry<InstanceIdentifier<?>, DataObject> entry : dataCreatedObject.entrySet()) {
+            if( entry.getValue() instanceof ServiceFunctionGroup) {
+//                ServiceFunction createdServiceFunction = (ServiceFunction) entry.getValue();
+//                SfcProviderServiceFunctionAPI.putServiceFunctionDescriptionExecutor(createdServiceFunction);
+//                SfcProviderServiceFunctionAPI.putServiceFunctionMonitorExecutor(createdServiceFunction);
+//                if (!SfcProviderServiceTypeAPI.createServiceFunctionTypeEntryExecutor(createdServiceFunction)) {
+//                    LOG.error("Failed to create service function type: {}", createdServiceFunction.getType());
+//                }
+            }
+        }
+
+        // SFG DELETION
+        Set<InstanceIdentifier<?>> dataRemovedConfigurationIID = change.getRemovedPaths();
+        for (InstanceIdentifier instanceIdentifier : dataRemovedConfigurationIID) {
+            DataObject dataObject = dataOriginalDataObject.get(instanceIdentifier);
+            if( dataObject instanceof ServiceFunctionGroup) {
+//                ServiceFunction originalServiceFunction = (ServiceFunction) dataObject;
+//
+//                if (!SfcProviderServiceTypeAPI.deleteServiceFunctionTypeEntryExecutor(originalServiceFunction)) {
+//                    LOG.error("Failed to delete Service Function Type for SF: {}", originalServiceFunction.getName());
+//                }
+//
+//                /* Before removing RSPs used by this Service Function, we need to remove all
+//                 * references in the SFF/SF operational trees
+//                 */
+//                String sfName = originalServiceFunction.getName();
+//                List<String> rspList = SfcProviderServiceFunctionAPI.
+//                        readServiceFunctionStateAsStringListExecutor(sfName);
+//                if ((rspList != null) && (!rspList.isEmpty())) {
+//                    if (SfcProviderServiceFunctionAPI.deleteServiceFunctionStateExecutor(sfName)) {
+//                    } else {
+//                        LOG.error("{}: Failed to delete SF {} operational state",
+//                                Thread.currentThread().getStackTrace()[1], sfName);
+//                    }
+//                    SfcProviderServiceForwarderAPI
+//                            .deletePathFromServiceForwarderStateExecutor(rspList);
+//
+//                    SfcProviderRenderedPathAPI.deleteRenderedServicePathsExecutor(rspList);
+//                }
+            }
+        }
+
+
+        // SFG UPDATE
+        Map<InstanceIdentifier<?>, DataObject> dataUpdatedConfigurationObject
+                = change.getUpdatedData();
+        for (Map.Entry<InstanceIdentifier<?>, DataObject> entry : dataUpdatedConfigurationObject.entrySet()) {
+            if ((entry.getValue() instanceof ServiceFunctionGroup) && (!(dataCreatedObject.containsKey(entry.getKey())))) {
+
+
+//                DataObject dataObject = dataOriginalDataObject.get(entry.getKey());
+//                ServiceFunction originalServiceFunction = (ServiceFunction) dataObject;
+//                Object[] serviceFunctionObj = {originalServiceFunction};
+//                Class[] serviceFunctionClass = {ServiceFunction.class};
+//
+//
+//                ServiceFunction updatedServiceFunction = (ServiceFunction) entry.getValue();
+//                SfcProviderServiceFunctionAPI.putServiceFunctionDescriptionExecutor(updatedServiceFunction);
+//                SfcProviderServiceFunctionAPI.putServiceFunctionMonitorExecutor(updatedServiceFunction);
+//
+//                // We only update SF type entry if type has changed
+//                if (!updatedServiceFunction.getType().equals(originalServiceFunction.getType())) {
+//
+//                    // We remove the original SF from SF type list
+//                    Future future = odlSfc.getExecutor().submit(SfcProviderServiceTypeAPI
+//                            .getDeleteServiceFunctionFromServiceType(serviceFunctionObj, serviceFunctionClass));
+//                    try {
+//                        LOG.debug("getDeleteServiceFunctionFromServiceType returns: {}", future.get());
+//                    } catch (InterruptedException e) {
+//                        LOG.warn("failed to ...." , e);
+//                    } catch (ExecutionException e) {
+//                        LOG.warn("failed to ...." , e);
+//                    }
+//                    // We create a independent entry
+//                    serviceFunctionObj[0] = updatedServiceFunction;
+//                    serviceFunctionClass[0] = ServiceFunction.class;
+//                    odlSfc.getExecutor().submit(SfcProviderServiceTypeAPI
+//                            .getCreateServiceFunctionTypeEntry(serviceFunctionObj, serviceFunctionClass));
+//                }
+//
+//               /* Before removing RSPs used by this Service Function, we need to remove all
+//                 * references in the SFF/SF operational trees
+//                 */
+//                String sfName = originalServiceFunction.getName();
+//                List<SfServicePath> sfServicePathList = SfcProviderServiceFunctionAPI.
+//                        readServiceFunctionStateExecutor(sfName);
+//                List<String> rspList = new ArrayList<>();
+//                if ((sfServicePathList != null) && (!sfServicePathList.isEmpty())) {
+//                    if (SfcProviderServiceFunctionAPI.deleteServiceFunctionStateExecutor(sfName)) {
+//                    } else {
+//                        LOG.error("{}: Failed to delete SF {} operational state",
+//                                Thread.currentThread().getStackTrace()[1], sfName);
+//                    }
+//                    for (SfServicePath sfServicePath : sfServicePathList) {
+//                        String rspName = sfServicePath.getName();
+//                        SfcProviderServiceForwarderAPI
+//                                .deletePathFromServiceForwarderStateExecutor(rspName);
+//                        rspList.add(rspName);
+//                    }
+//                    SfcProviderRenderedPathAPI.deleteRenderedServicePathsExecutor(rspList);
+//                }
+                /* We do not update the SFF dictionary. Since the user configured it in the first place,
+                 * (s)he is also responsible for updating it.
+                 */
+            }
+        }
+        odlSfc.releaseLock();
+        printTraceStop(LOG);
+    }
+
+}
---
+++ sfc-sb-rest/src/main/java/org/opendaylight/sfc/sbrest/provider/task/SbRestSfgTask.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.sfc.sbrest.provider.task;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+
+import org.opendaylight.sfc.sbrest.json.SfExporterFactory;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sfg.rev160202.service.function.groups.ServiceFunctionGroup;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SbRestSfgTask extends SbRestAbstractTask {
+
+    private static final String SFG_REST_URI = "/config/service-function:service-function-groups/service-function-group/";
+    private static final Logger LOG = LoggerFactory.getLogger(SbRestSfgTask.class);
+
+    public SbRestSfgTask(RestOperation restOperation, ServiceFunctionGroup dataObject, ExecutorService odlExecutor) {
+        super(restOperation, odlExecutor);
+        this.exporterFactory = new SfExporterFactory();
+        if (restOperation.equals(RestOperation.DELETE)) {
+            this.jsonObject = exporterFactory.getExporter().exportJsonNameOnly(dataObject);
+        } else {
+            this.jsonObject = exporterFactory.getExporter().exportJson(dataObject);
+        }
+        setRestUriList(dataObject);
+    }
+
+    @Override
+    protected void setRestUriList(DataObject dataObject) {
+        ServiceFunctionGroup obj = (ServiceFunctionGroup) dataObject;
+
+        if (obj.getRestUri() != null) {
+            String restUri = obj.getRestUri().getValue() + SFG_REST_URI + obj.getName();
+            this.restUriList = new ArrayList<>();
+            this.restUriList.add(restUri);
+            LOG.info("SFG will be send to REST URI {}", restUri);
+        } else {
+            this.restUriList = null;
+        }
+    }
+}

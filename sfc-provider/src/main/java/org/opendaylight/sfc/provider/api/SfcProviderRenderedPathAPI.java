c1699c46097bc7525930bca6e513c9600f9befd9
shuqianx <shuqiangx.zhao@intel.com>
2015-03-14 18:35:04 -0400

    add shortest path algorithm for service function selection
    Change-Id: I053a983d16fb08d4e146fc54b80010be42c8e176
    Signed-off-by: shuqianx <shuqiangx.zhao@intel.com>

--- sfc-provider/src/main/java/org/opendaylight/sfc/provider/api/SfcProviderRenderedPathAPI.java
+++ sfc-provider/src/main/java/org/opendaylight/sfc/provider/api/SfcProviderRenderedPathAPI.java
@@ -41,6 +41,7 @@ import org.slf4j.LoggerFactory;
 import org.opendaylight.yang.gen.v1.urn.intel.params.xml.ns.yang.sfc.sfst.rev150312.Random;
 import org.opendaylight.yang.gen.v1.urn.intel.params.xml.ns.yang.sfc.sfst.rev150312.RoundRobin;
 import org.opendaylight.yang.gen.v1.urn.intel.params.xml.ns.yang.sfc.sfst.rev150312.LoadBalance;
+import org.opendaylight.yang.gen.v1.urn.intel.params.xml.ns.yang.sfc.sfst.rev150312.ShortestPath;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -108,6 +109,8 @@ public class SfcProviderRenderedPathAPI extends SfcProviderAbstractAPI {
             scheduler = new SfcServiceFunctionLoadBalanceSchedulerAPI();
         } else if (serviceFunctionSchedulerType == Random.class) {
             scheduler = new SfcServiceFunctionRandomSchedulerAPI();
+        } else if (serviceFunctionSchedulerType == ShortestPath.class) {
+            scheduler = new SfcServiceFunctionShortestPathSchedulerAPI();
         } else {
             scheduler = new SfcServiceFunctionRandomSchedulerAPI();
         }

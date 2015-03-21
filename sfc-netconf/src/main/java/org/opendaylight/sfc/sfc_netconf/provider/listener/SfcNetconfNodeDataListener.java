6c8154a25abddf789c67dfdd4cfdde09f3ebd720
Reinaldo Penno <rapenno@gmail.com>
2015-03-21 11:18:41 -0500

    SFC Netconf Implementation@IETF-92
    - First patch, basic listener support
    
    Change-Id: Icc656e8cc4b5c873a4148d0c48e2ae0d1b540dda
    Signed-off-by: Reinaldo Penno <rapenno@gmail.com>

---
+++ sfc-netconf/src/main/java/org/opendaylight/sfc/sfc_netconf/provider/listener/SfcNetconfNodeDataListener.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+/**
+ * DataChangeListener attached to the OVSDB southbound operational datastore
+ *
+ * <p>
+ * @author Reinaldo Penno (rapenno@gmail.com)
+ * @version 0.1
+ * @since       2015-02-13
+ */
+
+package org.opendaylight.sfc.sfc_netconf.provider.listener;
+
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.sfc.provider.OpendaylightSfc;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+import static org.opendaylight.sfc.provider.SfcProviderDebug.printTraceStart;
+import static org.opendaylight.sfc.provider.SfcProviderDebug.printTraceStop;
+
+public class SfcNetconfNodeDataListener extends SfcNetconfAbstractDataListener {
+    private static final Logger LOG = LoggerFactory.getLogger(SfcNetconfNodeDataListener.class);
+
+
+    public SfcNetconfNodeDataListener(OpendaylightSfc opendaylightSfc) {
+        setOpendaylightSfc(opendaylightSfc);
+        setDataBroker(opendaylightSfc.getDataProvider());
+        setInstanceIdentifier(null);
+        setDataStoreType(LogicalDatastoreType.OPERATIONAL);
+        registerAsDataChangeListener();
+    }
+
+
+    @Override
+    public void onDataChanged(
+            final AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> change) {
+
+        printTraceStart(LOG);
+
+       Map<InstanceIdentifier<?>, DataObject> dataOriginalDataObject = change.getOriginalData();
+
+/*        for (Map.Entry<InstanceIdentifier<?>, DataObject> entry : dataOriginalDataObject.entrySet()) {
+            if (entry.getValue() instanceof OvsdbNodeAugmentation) {
+                OvsdbNodeAugmentation originalOvsdbNodeAugmentation = (OvsdbNodeAugmentation) entry.getValue();
+                LOG.debug("\nOriginal Rendered Service Path: {}", originalOvsdbNodeAugmentation.toString());
+            }
+        }*/
+
+        // Node CREATION
+        Map<InstanceIdentifier<?>, DataObject> dataCreatedObject = change.getCreatedData();
+
+        for (Map.Entry<InstanceIdentifier<?>, DataObject> entry : dataCreatedObject.entrySet()) {
+            if (entry.getValue() instanceof Node) {
+                Node node = (Node) entry.getValue();
+                String nodeName = node.getNodeId().getValue();
+                LOG.debug("\nCreated OvsdbNodeAugmentation: {}", node.toString());
+
+            }
+        }
+
+/*       // NODE UPDATE
+        Map<InstanceIdentifier<?>, DataObject> dataUpdatedObject = change.getUpdatedData();
+        for (Map.Entry<InstanceIdentifier<?>, DataObject> entry : dataUpdatedObject.entrySet()) {
+            if ((entry.getValue() instanceof OvsdbNodeAugmentation)
+                    && (!dataCreatedObject.containsKey(entry.getKey()))) {
+                OvsdbNodeAugmentation updatedOvsdbNodeAugmentation = (OvsdbNodeAugmentation) entry.getValue();
+                LOG.debug("\nModified OvsdbNodeAugmentation : {}", updatedOvsdbNodeAugmentation.toString());
+
+            }
+        }
+
+
+        // NODE DELETION
+        Set<InstanceIdentifier<?>> dataRemovedConfigurationIID = change.getRemovedPaths();
+        for (InstanceIdentifier instanceIdentifier : dataRemovedConfigurationIID) {
+            DataObject dataObject = dataOriginalDataObject.get(instanceIdentifier);
+            if (dataObject instanceof OvsdbNodeAugmentation) {
+
+                OvsdbNodeAugmentation deletedOvsdbNodeAugmentation = (OvsdbNodeAugmentation) dataObject;
+                LOG.debug("\nDeleted OvsdbNodeAugmentation: {}", deletedOvsdbNodeAugmentation.toString());
+
+            }
+        }*/
+        printTraceStop(LOG);
+    }
+
+
+}

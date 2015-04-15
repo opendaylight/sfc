e232cfb9f86357df7a771b6b0cf7014f91530077
Yi Yang <yi.y.yang@intel.com>
2015-04-15 13:42:19 +0000

    Add shortest path algorithm for service function selection
    Change-Id: Ia7acb96bd8bc60b740ada2036986ac4de76f22be
    Signed-off-by: Shuqiang Zhao <shuqiangx.zhao@intel.com>
    Signed-off-by: Yi Yang <yi.y.yang@intel.com>

---
+++ sfc-provider/src/main/java/org/opendaylight/sfc/provider/topology/SfcProviderTopologyNode.java
@@ -0,0 +1,75 @@
+/*
+* Copyright (c) 2014 Intel Corporation. All rights reserved.
+*
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License v1.0 which accompanies this distribution,
+* and is available at http://www.eclipse.org/legal/epl-v10.html
+*/
+package org.opendaylight.sfc.provider.topology;
+
+/**
+ * This class represents a node/vertex in topology graph
+ * that composes of SFs (Service Functions) and SFFs (Service Function Forwarders)
+ * , a node may be SF or SFF, please refer to class SfcProviderGraph for
+ * topology graph
+ *
+ * @author Shuqiang Zhao <shuqiangx.zhao@intel.com>
+ * @author Yi Yang <yi.y.yang@intel.com>
+ *
+ */
+public class SfcProviderTopologyNode implements Comparable<SfcProviderTopologyNode> {
+    private String name;
+    private int load;
+    private SfcProviderTopologyNode parent;
+    private int color;
+    private int dist;
+
+    public SfcProviderTopologyNode(String name)
+    {
+        this.name = name;
+        this.load = 0;
+        this.parent = null;
+        this.color = 0;
+        this.dist = 0;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public SfcProviderTopologyNode getParent() {
+        return this.parent;
+    }
+
+    public void setParent(SfcProviderTopologyNode parent) {
+        this.parent = parent;
+    }
+
+    public int getColor() {
+        return this.color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public int getDist() {
+        return this.dist;
+    }
+
+    public void setDist(int dist) {
+        this.dist = dist;
+    }
+
+    public int compareTo(SfcProviderTopologyNode node) {
+        return this.name.compareTo(node.getName());
+    }
+
+    public boolean equals(SfcProviderTopologyNode node) {
+        boolean flag = false;
+        if (this.name == node.getName()) {
+            flag = true;
+        }
+        return flag;
+    }
+}

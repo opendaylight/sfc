d8c91d34d5e1a934accb91e62cd6c09beca5e0ff
Johnson Li <johnson.li@intel.com>
2015-04-27 11:16:51 +0800

    Adding Connected SFF Dictionary into SFF
    Signed-off-by: Johnson Li <johnson.li@intel.com>

--- sfc-ui/src/main/resources/pages/src/app/sfc/serviceforwarder/serviceforwarder.services.js
+++ sfc-ui/src/main/resources/pages/src/app/sfc/serviceforwarder/serviceforwarder.services.js
@@ -30,6 +30,22 @@ define(['app/sfc/sfc.module'], function (sfc) {
       $scope.data['service-function-dictionary'].splice(index, 1);
     };
 
+    svc.addForwarder = function ($scope) {
+      if (angular.isUndefined($scope.data['connected-sff-dictionary'])) {
+        $scope.data['connected-sff-dictionary'] = [];
+      }
+      $scope.data['connected-sff-dictionary'].push(
+        {
+          "nonExistent": false,
+          "sff-sff-data-plane-locator": {},
+          "sff-interfaces": []
+        });
+    };
+
+    svc.removeForwarder = function (index, $scope) {
+      $scope.data['connected-sff-dictionary'].splice(index, 1);
+    };
+
     svc.sffInterfaceToString = function (sffInterfaces) {
       var string = "";
       _.each(sffInterfaces, function (interf) {
@@ -46,7 +62,31 @@ define(['app/sfc/sfc.module'], function (sfc) {
       return array;
     };
 
+    svc.sffInterfaceInSffToString = function (sffInterfaces) {
+      var string = "";
+      _.each(sffInterfaces, function (interf) {
+        string = string.concat(interf['sff-interface'] + ", ");
+      });
+      return string.slice(0, -2);
+    };
+
+    svc.sffInterfaceInSffToStringArray = function (sffInterfaces) {
+      var array = [];
+      _.each(sffInterfaces, function (interf) {
+        array.push(interf['sff-interface']);
+      });
+      return array;
+    };
+
     svc.sffInterfaceToObjectArray = function (sffInterfaces) {
+      var objectArray = [];
+      _.each(sffInterfaces, function (interf) {
+        objectArray.push({'sff-interface': interf});
+      });
+      return objectArray;
+    };
+
+    svc.sffInterfaceInSffToObjectArray = function (sffInterfaces) {
       var objectArray = [];
       _.each(sffInterfaces, function (interf) {
         objectArray.push({'sff-interface': interf});
@@ -70,9 +110,23 @@ define(['app/sfc/sfc.module'], function (sfc) {
       return string;
     };
 
+    svc.sffForwarderDictionaryToString = function (sffForwarderDictionary) {
+      var string = "";
+      _.each(sffForwarderDictionary, function (sff) {
+        string = string.concat(sff.name + " ");
+      });
+      return string;
+    };
+
     svc.removeTemporaryPropertiesFromSf = function (sf) {
       if (angular.isDefined(sf.nonExistent)) {
         delete sf.nonExistent;
+      }
+    };
+
+    svc.removeTemporaryPropertiesFromSff = function (sff) {
+      if (angular.isDefined(sff.nonExistent)) {
+        delete sff.nonExistent;
       }
     };
 
@@ -100,12 +154,37 @@ define(['app/sfc/sfc.module'], function (sfc) {
       }
     };
 
+    /*
+    svc.addMgtIpToChoosenSff = function (choosenSff, sffModelData) {
+      choosenSff.ip-mgmt-address = sffModelData.ip-mgmt-address;
+    };
+
+    svc.sffChangeListener = function (choosenSff, $scope) {
+      if (angular.isDefined(choosenSff)) {
+        var sffModelData = _.findWhere($scope.sffs, {name: choosenSff.name});
+
+        if (angular.isDefined(sffModelData)) {
+          svc.addMgtIpToChoosenSff(choosenSff, sffModelData);
+        }
+      }
+    };
+    */
+
     svc.sfUpdate = function (sf, $scope) {
       if (angular.isDefined(sf)) {
         sf['sff-interfaces'] = svc.sffInterfaceToStringArray(sf['sff-interfaces']);
         var sfModelData = _.findWhere($scope.sfs, {name: sf.name});
 
         sf.nonExistent = angular.isDefined(sfModelData) ? false : true;
+      }
+    };
+
+    svc.sffUpdate = function (sff, $scope) {
+      if (angular.isDefined(sff)) {
+        sff['sff-interfaces'] = svc.sffInterfaceInSffToStringArray(sff['sff-interfaces']);
+        var sffModelData = _.findWhere($scope.sffs, {name: sff.name});
+
+        sff.nonExistent = angular.isDefined(sffModelData) ? false : true;
       }
     };
 

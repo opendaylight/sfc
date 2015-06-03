32711681a10e43175e252ad4c6145163196f73ee
Marcel Sestak <msestak@cisco.com>
2015-06-03 23:13:40 +0200

    script for building setup python package
    Change-Id: Id596b5b6b4ff5bcb3a444c3c07d69637d7b0d3e7
    Signed-off-by: Marcel Sestak <msestak@cisco.com>

--- sfc-py/jenkins_build_package.sh
+++ sfc-py/jenkins_build_package.sh
@@ -8,7 +8,7 @@ apt-get install libssl-dev or  sudo apt-get install libssl-dev openssl
 # download and extract Python to /tmp
 wget https://www.python.org/ftp/python/3.4.3/Python-3.4.3.tgz --directory-prefix=/tmp
 tar -xvzf /tmp/Python-3.4.3.tgz -C /tmp
-#save current dir should be root of the Sfc project
+#save current dir should be root of the sfc-py project
 
 curpath="$PWD"
 cd /tmp/Python-3.4.3
@@ -33,21 +33,20 @@ source "$sfc_py3_venv_dir/bin/activate"
 
 
 # create SFC package
-# === TODO start ===
 
 # cd to sfc/sfc-py directory
-cd $curpath/sfc-py
-pwd 
+cd $curpath
+ 
 # 3. execute "sudo python3 setup.py sdist" to build the package
 python3 setup.py sdist
-# 4. check if the package 'sfc-0.1.0.tar.gz' was created in sfc/sfc-py/sfc/dist
+# 4. check if the package 'sfc-0.1.0.tar.gz' was created in sfc/sfc-py/dist
 
-file="$curpath/sfc-py/dist/sfc-0.1.0.tar.gz"
+file="$curpath/dist/sfc-0.1.0.tar.gz"
 if [ -s "$file" ]
 then
 	echo "Sfc Package created and has size greater than zero."
       # 5. publish the package on PyPI
-      # === TODO end ===
+      # === decided to do this step manually
 else
 	echo "Sfc Package not found."
 fi
@@ -56,6 +55,3 @@ fi
 # deactivate virtualenv, remove temporary stuff
 deactivate
 rm -rf $sfc_py3_dir $sfc_py3_venv_dir
-
-#Pypi registration 
-#Marcello Puskaj7727 msestak@cisco.com   - not registered yet

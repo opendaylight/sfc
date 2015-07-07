4e1333bd4dca8cb4fbdacb0e544ff784ef08a5f5
Marcel Sestak <msestak@cisco.com>
2015-07-07 11:47:04 +0200

    uploading SFC python package to Pypi server in JJ sfc-verify-python-master
    Change-Id: Ib48df64a14fafa5667419d4d162ff8893d6e47e0
    Signed-off-by: Marcel Sestak <msestak@cisco.com>

--- sfc-py/jenkins_build_package.sh
+++ sfc-py/jenkins_build_package.sh
@@ -11,11 +11,16 @@ tar -xvzf /tmp/Python-3.4.3.tgz -C /tmp
 #save current dir should be root of the sfc-py project
 
 curpath="$PWD"
+USR="rapenno"
+PSW="SfcpY0423"
+
 cd /tmp/Python-3.4.3
 
 # set Python 3.4 install directory, Python 3.4 executable and virtualenv directory path
 sfc_random_suffix=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
 sfc_py3_dir="/tmp/python3_$sfc_random_suffix"
+sfc_twine_dir="/tmp/twine_$sfc_random_suffix"
+sfc_twine_executable="$sfc_twine_dir/twine"
 sfc_py3_executable="$sfc_py3_dir/bin/python3"
 sfc_py3_venv_dir="/tmp/sfc_venv_$sfc_random_suffix"
 
@@ -30,6 +35,9 @@ echo ++++++++++++++++ Activating virtualenv +++++++++++++++++++
 
 source "$sfc_py3_venv_dir/bin/activate"
 
+# instal twine
+pip install twine 
+which twine
 
 # create SFC package
 
@@ -38,14 +46,16 @@ cd $curpath
  
 # 3. execute "sudo python3 setup.py sdist" to build the package
 python3 setup.py sdist
-# 4. check if the package 'sfc-0.1.0.tar.gz' was created in sfc/sfc-py/dist
+# 4. check if the package 'sfc-0.1.1.tar.gz' was created in sfc/sfc-py/dist
 
-file="$curpath/dist/sfc-0.1.0.tar.gz"
+file="$curpath/dist/sfc-0.1.3.tar.gz"
 if [ -s "$file" ]
 then
 	echo "Sfc Package created and has size greater than zero."
-      # 5. publish the package on PyPI
-      # === decided to do this step manually
+    # 5. publish the package on PyPI
+    # === decided to do this step using Reinaldo Penno login
+    #$sfc_twine_executable upload -u $USR -p $PSW dist/*
+    twine upload -u $USR -p $PSW dist/*
 else
 	echo "Sfc Package not found."
 fi
@@ -53,4 +63,4 @@ fi
 
 # deactivate virtualenv, remove temporary stuff
 deactivate
-rm -rf $sfc_py3_dir $sfc_py3_venv_dir
+# rm -rf $sfc_py3_dir $sfc_py3_venv_dir $sfc_twine_dir

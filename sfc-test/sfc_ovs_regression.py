5b357ffc2f94f74780ceccde6cdb3691e50ff131
Reinaldo Penno <rapenno@gmail.com>
2015-07-11 02:30:58 -0700

    Bug 3982 - SFC Agent does not work with OVS NSH v8
    - NSH Client and Agent fixes for OVS Interop
    
    Change-Id: I123186983900bbce07d39b940e560e443300c20e
    Signed-off-by: Reinaldo Penno <rapenno@gmail.com>

--- sfc-test/sfc_ovs_regression.py
+++ sfc-test/sfc_ovs_regression.py
@@ -1,8 +1,10 @@
 __author__ = "Andrej Kincel"
+__author__ = "Reinaldo Penno"
 __copyright__ = "Copyright(c) 2015, Cisco Systems, Inc."
 __license__ = "New-style BSD"
 __version__ = "0.1"
 __email__ = "andrej.kincel@gmail.com"
+__email__ = "rapenno@gmail.com"
 __status__ = "Tested with SFC-Karaf distribution as of 04/14/2015"
 
 
@@ -13,19 +15,15 @@ import time
 
 if __name__ == "__main__":
 
-    delete_configuration()
-    put_and_check(SF_URL, SERVICE_FUNCTIONS_JSON, SERVICE_FUNCTIONS_JSON)
-    check(SFT_URL, SERVICE_FUNCTION_TYPE_JSON, "Checking Service Function Type...")
-    post_rpc(SFF_OVS_RPC_URL, SERVICE_FORWARDER_OVS_RPC_REQ_1, SERVICE_FORWARDER_OVS_RPC_RESP)
-    post_rpc(SFF_OVS_RPC_URL, SERVICE_FORWARDER_OVS_RPC_REQ_2, SERVICE_FORWARDER_OVS_RPC_RESP)
+    # delete_configuration()
+    # put_and_check(SF_URL, SERVICE_FUNCTIONS_JSON, SERVICE_FUNCTIONS_JSON)
+    # check(SFT_URL, SERVICE_FUNCTION_TYPE_JSON, "Checking Service Function Type...")
+    # put_and_check(SFF_URL, SERVICE_FUNCTION_FORWARDERS_JSON, SERVICE_FUNCTION_FORWARDERS_JSON)
+    # put_and_check(SFC_URL, SERVICE_CHAINS_JSON, SERVICE_CHAINS_JSON)
+    # put_and_check(SFP_URL, SERVICE_PATH_JSON, SERVICE_PATH_JSON)
+    post_rpc(RSP_RPC_URL, RENDERED_SERVICE_PATH_RPC_REQ, RENDERED_SERVICE_PATH_RPC_RESP)
+    check(RSP_URL, RENDERED_SERVICE_PATH_RESP_JSON, "Checking RSP...")
 
-    #put_and_check(SFF_URL, SERVICE_FUNCTION_FORWARDERS_JSON, SERVICE_FUNCTION_FORWARDERS_JSON)
-
-    put_and_check(SFC_URL, SERVICE_CHAINS_JSON, SERVICE_CHAINS_JSON)
-    put_and_check(SFP_URL, SERVICE_PATH_JSON, SERVICE_PATH_JSON)
-
-    #post_rpc(RSP_RPC_URL, RENDERED_SERVICE_PATH_RPC_REQ, RENDERED_SERVICE_PATH_RPC_RESP)
-    #check(RSP_URL, RENDERED_SERVICE_PATH_RESP_JSON, "Checking RSP...")
     #check(SFF_OPER_URL, SERVICE_FUNCTION_FORWARDERS_OPER_JSON, "Checking SFF Operational State...")
     #check(SF_OPER_URL, SERVICE_FUNCTION_OPER_JSON, "Checking SF Operational State...")
 

2bfea2c94d68148b47f5219047921d154ed394a9
Reinaldo Penno <rapenno@gmail.com>
2015-07-12 15:54:53 -0700

    Bug 3983 - NSH Inner Packet headers not constructed properly
    Change-Id: Ib96a661e2aa73d4a50abba3e16c24736118e4e87
    Signed-off-by: Reinaldo Penno <rapenno@gmail.com>

--- sfc-py/sfc/sff_client.py
+++ sfc-py/sfc/sff_client.py
@@ -28,13 +28,12 @@ from sfc.nsh.encode import *  # noqa
 
 __author__ = "Reinaldo Penno, Jim Guichard, Paul Quinn"
 __copyright__ = "Copyright(c) 2014, Cisco Systems, Inc."
-__version__ = "0.4"
+__version__ = "0.6"
 __email__ = "repenno@cisco.com, jguichar@cisco.com, paulq@cisco.com"
-__status__ = "alpha"
+__status__ = "beta"
 
-"""Service Function Forwarder (SFF) Client. This module
-   will initiate the path by building a NSH/VXLAN-GPE packet and sending to the first SFF
-   in the path"""
+"""NSH Client. This module can send many different types of NSH encap packets in order to test SFF/SFF
+   implementations"""
 
 logger = logging.getLogger(__name__)
 
@@ -53,20 +52,25 @@ SERVICEFUNCTION_INVALID = 0xDEADBEEF  # Referenced service function is invalid
 RSP_STARTING_INDEX = 255
 
 
-# Client side code: Build NSH packet encapsulated in VXLAN & NSH.
-
 class MyNshBaseClass:
+    """
+    Base class common to all encap types
+    """
     def __init__(self):
         self.server_vxlan_values = VXLANGPE()
         self.server_base_values = BASEHEADER()
         self.server_ctx_values = CONTEXTHEADER()
         self.server_ethernet_values = ETHHEADER()
         self.server_trace_values = TRACEREQHEADER()
+        self.inner_header = InnerHeader()
 
 
 class MyVxlanGpeNshIpClient(MyNshBaseClass):
-    def __init__(self, loop, encapsulate_type, encapsulate_header_values, base_header_values, ctx_header_values,
-                 remote_sff_ip, remote_sff_port, dest_ip, dest_port):
+    """
+    Class for VXLAN_GPE + NSH + IP
+    """
+    def __init__(self, loop, encapsulate_header_values, base_header_values, ctx_header_values,
+                 remote_sff_ip, remote_sff_port, inner_dest_ip, inner_dest_port, encapsulate_type='VXLAN-GPE/NSH/IPv4'):
         super().__init__()
         self.transport = None
         self.loop = loop
@@ -75,8 +79,8 @@ class MyVxlanGpeNshIpClient(MyNshBaseClass):
         self.ctx_header_values = ctx_header_values
         self.remote_sff_ip = remote_sff_ip
         self.remote_sff_port = remote_sff_port
-        self.dest_ip = dest_ip
-        self.dest_port = dest_port
+        self.inner_dest_ip = inner_dest_ip
+        self.inner_dest_port = inner_dest_port
         self.encapsulate_type = encapsulate_type
 
     def alarm_handler(self, signum=None, frame=None):
@@ -89,7 +93,8 @@ class MyVxlanGpeNshIpClient(MyNshBaseClass):
         packet = build_nsh_header(self.encapsulate_header_values,
                                   self.base_header_values,
                                   self.ctx_header_values)
-        udp_packet = build_udp_packet(self.remote_sff_ip, self.dest_ip, self.dest_port, self.remote_sff_port,
+        udp_packet = build_udp_packet(self.remote_sff_ip, self.inner_dest_ip, self.inner_dest_port,
+                                      self.remote_sff_port,
                                       "test".encode('utf-8'))
         logger.info("Sending %s packet to SFF: %s", self.encapsulate_type, (self.remote_sff_ip, self.remote_sff_port))
         logger.debug("Packet dump: %s", binascii.hexlify(packet))
@@ -127,8 +132,12 @@ class MyVxlanGpeNshIpClient(MyNshBaseClass):
 
 
 class MyVxlanGpeNshEthClient(MyNshBaseClass):
-    def __init__(self, loop, encapsulate_type, ethernet_values, encapsulate_header_values, base_header_values,
-                 ctx_header_values, remote_sff_ip, remote_sff_port, dest_ip, dest_port):
+    """
+    Class for VXLAN_GPE + NSH + Ethernet + IP
+    """
+    def __init__(self, loop, ethernet_values, encapsulate_header_values, base_header_values,
+                 ctx_header_values, remote_sff_ip, remote_sff_port, inner_dest_ip, inner_dest_port,
+                 encapsulate_type='VXLAN-GPE/NSH/Ethernet'):
         super().__init__()
         self.transport = None
         self.loop = loop
@@ -138,8 +147,8 @@ class MyVxlanGpeNshEthClient(MyNshBaseClass):
         self.ctx_header_values = ctx_header_values
         self.remote_sff_ip = remote_sff_ip
         self.remote_sff_port = remote_sff_port
-        self.dest_ip = dest_ip
-        self.dest_port = dest_port
+        self.inner_dest_ip = inner_dest_ip
+        self.inner_dest_port = inner_dest_port
         self.encapsulate_type = encapsulate_type
 
     def alarm_handler(self, signum=None, frame=None):
@@ -155,8 +164,82 @@ class MyVxlanGpeNshEthClient(MyNshBaseClass):
                                       self.ethernet_values)
         # packet = build_vxlan_header(self.encapsulate_header_values,
         # self.ethernet_values)
-        udp_inner_packet = build_udp_packet("172.20.33.195", self.dest_ip, self.dest_port, self.remote_sff_port,
-                                            "test".encode('utf-8'))
+        udp_inner_packet = build_udp_packet("172.20.33.195", self.inner_dest_ip, self.inner_dest_port,
+                                            self.remote_sff_port, "test".encode('utf-8'))
+        gpe_nsh_ethernet_packet = packet + udp_inner_packet
+        logger.debug("Ethernet dump: ", binascii.hexlify(gpe_nsh_ethernet_packet))
+        logger.info("Sending %s packet to SFF: %s", self.encapsulate_type, (self.remote_sff_ip, self.remote_sff_port))
+        # Send the packet
+        signal.signal(signal.SIGALRM, self.alarm_handler)
+        signal.alarm(2)
+        try:
+            self.transport.sendto(gpe_nsh_ethernet_packet, (self.remote_sff_ip, self.remote_sff_port))
+        except socket.error as msg:
+            print('Failed to send packet. Error Code : ' + str(msg))
+            sys.exit()
+
+    def datagram_received(self, data, addr):
+        signal.alarm(0)
+        logger.info("Received packet from SFF: %s", addr)
+        logger.debug("Packet dump: %s", binascii.hexlify(data))
+        # Decode all the headers
+        decode_vxlan(data, self.server_vxlan_values)
+        decode_baseheader(data, self.server_base_values)
+        decode_contextheader(data, self.server_ctx_values)
+        self.loop.stop()
+
+    @staticmethod
+    def connection_refused(exc):
+        logger.error('Connection refused:', exc)
+
+    def connection_lost(self, exc):
+        logger.error('closing transport', exc)
+        self.loop = asyncio.get_event_loop()
+        self.loop.stop()
+
+    @staticmethod
+    def error_received(exc):
+        logger.error('Error received:', exc)
+
+
+class MyVxlanNshEthClient(MyNshBaseClass):
+    """
+    Class for VXLAN + NSH + Ethernet + IP
+    """
+    def __init__(self, loop, ethernet_values, encapsulate_header_values, base_header_values,
+                 ctx_header_values, remote_sff_ip, remote_sff_port, inner_header,
+                 encapsulate_type='VXLAN/NSH/Ethernet'):
+        super().__init__()
+        self.transport = None
+        self.loop = loop
+        self.ethernet_values = ethernet_values
+        self.encapsulate_header_values = encapsulate_header_values
+        self.base_header_values = base_header_values
+        self.ctx_header_values = ctx_header_values
+        self.inner_header = inner_header
+        self.remote_sff_ip = remote_sff_ip
+        self.remote_sff_port = remote_sff_port
+        self.encapsulate_type = encapsulate_type
+
+    def alarm_handler(self, signum=None, frame=None):
+        logger.error("No response from %s:%d", self.remote_sff_ip, self.remote_sff_port)
+        sys.exit(1)
+
+    def connection_made(self, transport):
+        self.transport = transport
+        # Building client IP packet to send to SFF
+        packet = build_nsh_eth_header(self.encapsulate_header_values,
+                                      self.base_header_values,
+                                      self.ctx_header_values,
+                                      self.ethernet_values)
+
+        # VXLAN header is now built as part of nsh_eth heder
+        # packet = build_vxlan_header(self.encapsulate_header_values,
+        # self.ethernet_values)
+        udp_inner_packet = build_udp_packet(self.inner_header.inner_src_ip, self.inner_header.inner_dest_ip,
+                                            self.inner_header.inner_src_port,
+                                            self.inner_header.inner_dest_port, "test".encode('utf-8'))
+
         gpe_nsh_ethernet_packet = packet + udp_inner_packet
         logger.debug("Ethernet dump: ", binascii.hexlify(gpe_nsh_ethernet_packet))
         logger.info("Sending %s packet to SFF: %s", self.encapsulate_type, (self.remote_sff_ip, self.remote_sff_port))
@@ -355,19 +438,23 @@ def main(argv):
     sfp_index = 3
     trace_req = False
     num_trace_hops = 254
-    dest_ip = "127.0.0.1"
-    dest_port = 80
+    inner_src_ip = "192.168.0.1"
+    inner_dest_ip = "192.168.0.2"
+    inner_src_port = 10000
+    inner_dest_port = 20000
     encapsulate = 'gpe-nsh-ipv4'  # make vxlan-gpe encapsulation default
 
     try:
         logging.basicConfig(level=logging.INFO)
         opt, args = getopt.getopt(argv, "h",
-                                  ["help", "local-port=", "local-ip=", "dest-ip=", "dest-port=", "remote-sff-ip=",
+                                  ["help", "local-port=", "local-ip=", "inner-src-ip=", "inner-dest-ip=",
+                                   "inner-src-port=", "inner-dest-port=", "remote-sff-ip=",
                                    "remote-sff-port=", "sfp-id=", "sfp-index=", "trace-req", "num-trace-hops=",
                                    "encapsulate="])
     except getopt.GetoptError:
         print(
-            "sff_client --help | --local-port | --local-ip | --dest-ip | --dest-port | --remote-sff-ip | "
+            "sff_client --help | --local-port | --local-ip | --inner-src-ip | --inner-dest-ip | --inner-src-port | "
+            "--inner-dest-port | --remote-sff-ip | "
             "--remote-sff-port | --sfp-id | --sfp-index | --trace-req | --num-trace-hops | --encapsulate")
         sys.exit(2)
 
@@ -414,12 +501,20 @@ def main(argv):
             local_ip = arg
             continue
 
-        if opt == "--dest-port":
-            dest_port = int(arg)
+        if opt == "--inner-dest-port":
+            inner_dest_port = int(arg)
             continue
 
-        if opt == "--dest-ip":
-            dest_ip = arg
+        if opt == "--inner-dest-ip":
+            inner_dest_ip = arg
+            continue
+
+        if opt == "--inner-src-port":
+            inner_src_port = int(arg)
+            continue
+
+        if opt == "--inner-src-ip":
+            inner_src_ip = arg
             continue
 
     loop = asyncio.get_event_loop()
@@ -453,22 +548,20 @@ def main(argv):
     # sock_raw = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
     # except socket.error as msg:
     # print("Socket could not be created. Error Code : {}".format(msg))
-    #         sys.exit()
+    # sys.exit()
     #
     # udp_packet = build_udp_packet("", "100.0.3.2", 10000, 4790, "test".encode('utf-8'))
     # try:
-    #     sock_raw.sendto(udp_packet, ("100.0.3.2", 0))
-    #     #sock_raw.sendto(udp_packet, int.from_bytes(socket.inet_aton("10.0.1.1"), byteorder='big'))
+    # sock_raw.sendto(udp_packet, ("100.0.3.2", 0))
+    # #sock_raw.sendto(udp_packet, int.from_bytes(socket.inet_aton("10.0.1.1"), byteorder='big'))
     # except socket.error as msg:
-    #     print(msg)
+    # print(msg)
     # sys.exit()
 
     if trace_req:
         # MD-type 0x1, OAM set
         vxlan_header_values = VXLANGPE(int('00000100', 2), 0, 0x894F, int('111111111111111111111111', 2), 64)
-        # base_header_values = BASEHEADER(NSH_VERSION1, OAM_FLAG_AND_RESERVED, NSH_TYPE1_LEN, NSH_MD_TYPE1,
-        #                                 NSH_NEXT_PROTO_OAM, int(sfp_id), int(sfp_index))
-        base_header_values = BASEHEADER(int(sfp_id), int(sfp_index))
+        base_header_values = BASEHEADER(service_path=int(sfp_id), service_index=int(sfp_index))
         ctx_header_values = CONTEXTHEADER()
         trace_req_header_values = build_trace_req_header(OAM_TRACE_REQ_TYPE, 254,
                                                          remote_sff_ip, 55555)
@@ -479,13 +572,19 @@ def main(argv):
         start_client(loop, (str(ipaddress.IPv4Address(trace_req_header_values.ip_4)), 55555),
                      (remote_sff_ip, int(remote_sff_port)), traceclient)
     else:
+
+        # Common initializations for all encapsulations types
+        base_header_values = BASEHEADER(service_path=int(sfp_id), service_index=int(sfp_index),
+                                        proto=NSH_NEXT_PROTO_ETH)
+        ctx_values = CONTEXTHEADER()
+        inner_header = InnerHeader(inner_src_ip, inner_dest_ip, inner_src_port, inner_dest_port)
+
         if encapsulate == 'gpe-nsh-ipv4':
             vxlan_header_values = VXLANGPE()
-            # base_values = BASEHEADER(NSH_VERSION1, NSH_FLAG_ZERO, NSH_TYPE1_LEN, NSH_MD_TYPE1, NSH_NEXT_PROTO_IPV4,
-            #                          int(sfp_id), int(sfp_index))
-            base_header_values = BASEHEADER(service_path=int(sfp_id), service_index=int(sfp_index))
-            ctx_values = CONTEXTHEADER()
-            udpclient = MyVxlanGpeNshIpClient(loop, 'VXLAN-GPE/NSH/IP v4', vxlan_header_values, base_header_values,
+            #  override encap type
+            base_header_values.next_protocol = NSH_NEXT_PROTO_IPV4
+
+            udpclient = MyVxlanGpeNshIpClient(loop, vxlan_header_values, base_header_values,
                                               ctx_values, remote_sff_ip, int(remote_sff_port))
             start_client(loop, (local_ip, local_port), (remote_sff_ip, remote_sff_port), udpclient)
 
@@ -494,9 +593,6 @@ def main(argv):
             gre_header_values = GREHEADER(int('0', 2), int('000000000000', 2), int('000', 2), 0x894F,
                                           int('0000000000000000', 2), 0)
 
-            base_header_values = BASEHEADER(service_path=int(sfp_id), service_index=int(sfp_index),
-                                            proto=NSH_NEXT_PROTO_ETH)
-            ctx_values = CONTEXTHEADER()
             greclient = MyGreNshEthClient(loop, 'GRE/NSH/Ethernet', gre_header_values, base_header_values, ctx_values,
                                           remote_sff_ip, int(remote_sff_port))
 
@@ -505,37 +601,34 @@ def main(argv):
         elif encapsulate == 'gpe-nsh-ethernet':
 
             vxlan_header_values = VXLANGPE()
-            base_header_values = BASEHEADER(service_path=int(sfp_id), service_index=int(sfp_index),
-                                            proto=NSH_NEXT_PROTO_ETH)
-            ctx_values = CONTEXTHEADER()
 
             ethernet_header_values = ETHHEADER(0x3c, 0x15, 0xc2, 0xc9, 0x4f, 0xbc, 0x08, 0x00, 0x27, 0xb6, 0xb0, 0x58,
                                                0x08, 0x00)
 
-            udpclient = MyVxlanGpeNshEthClient(loop, 'VXLAN-GPE/NSH/Ethernet', ethernet_header_values,
+            udpclient = MyVxlanGpeNshEthClient(loop, ethernet_header_values,
                                                vxlan_header_values, base_header_values,
                                                ctx_values, remote_sff_ip,
-                                               int(remote_sff_port), dest_ip, dest_port)
+                                               int(remote_sff_port), inner_dest_ip, inner_dest_port)
             start_client(loop, (local_ip, local_port), (remote_sff_ip, remote_sff_port), udpclient)
 
         elif encapsulate == 'vxlan-nsh-ethernet-legacy':
 
             vxlan_header_values = VXLAN()
-            base_header_values = BASEHEADER(service_path=int(sfp_id), service_index=int(sfp_index),
-                                            proto=NSH_NEXT_PROTO_ETH)
-            ctx_values = CONTEXTHEADER()
 
+            # ethernet_header_values = ETHHEADER(0x3c, 0x15, 0xc2, 0xc9, 0x4f, 0xbc, 0x08, 0x00, 0x27, 0xb6, 0xb0, 0x58,
+            #                                    0x08, 0x00)
+            
             ethernet_header_values = ETHHEADER(0x3c, 0x15, 0xc2, 0xc9, 0x4f, 0xbc, 0x08, 0x00, 0x27, 0xb6, 0xb0, 0x58,
                                                0x08, 0x00)
 
-            udpclient = MyVxlanGpeNshEthClient(loop, 'VXLAN/NSH/Ethernet', ethernet_header_values,
-                                               vxlan_header_values, base_header_values,
-                                               ctx_values, remote_sff_ip,
-                                               int(remote_sff_port), dest_ip, dest_port)
+            udpclient = MyVxlanNshEthClient(loop, ethernet_header_values,
+                                            vxlan_header_values, base_header_values,
+                                            ctx_values, remote_sff_ip,
+                                            int(remote_sff_port), inner_header)
             start_client(loop, (local_ip, local_port), (remote_sff_ip, remote_sff_port), udpclient)
 
         else:
-            print("encapsulate must be specified, e.g. vxlan or gre")
+            logger.error("Encapsulation must be specified, e.g. gpe-nsh-ethernet")
 
 
 if __name__ == "__main__":

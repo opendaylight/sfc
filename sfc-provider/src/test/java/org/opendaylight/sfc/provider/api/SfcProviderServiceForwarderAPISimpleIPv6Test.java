771d94f67af963229a898c92358687d2f4c7930b
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-08-09 19:37:05 -0400

    Fix license header violations in sfc-provider
    Change-Id: I82f7d94495e466ba8fe28f6f4e3bcbe10d51ca25
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- sfc-provider/src/test/java/org/opendaylight/sfc/provider/api/SfcProviderServiceForwarderAPISimpleIPv6Test.java
+++ sfc-provider/src/test/java/org/opendaylight/sfc/provider/api/SfcProviderServiceForwarderAPISimpleIPv6Test.java
@@ -1,5 +1,3 @@
-package org.opendaylight.sfc.provider.api;
-
 /*
  * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
  *
@@ -7,6 +5,8 @@ package org.opendaylight.sfc.provider.api;
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+
+package org.opendaylight.sfc.provider.api;
 
 import org.junit.Before;
 import org.junit.Test;

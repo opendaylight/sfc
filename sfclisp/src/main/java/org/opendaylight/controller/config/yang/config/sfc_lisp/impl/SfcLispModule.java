3231c20a29019bd8545baafb91cce171f9c15187
Florin Coras <fcoras@cisco.com>
2015-08-20 17:11:55 +0200

    Fix sfclisp to use new lispflowmapping APIs
    Gerrits 25567 and 25567 introduce a considerable number of
    API changes. This patch updates sfclisp to leverage the new code.
    
    Change-Id: Ic36fe82a37039bc6ece36eccbc686f97db2c20d6
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- sfclisp/src/main/java/org/opendaylight/controller/config/yang/config/sfc_lisp/impl/SfcLispModule.java
+++ sfclisp/src/main/java/org/opendaylight/controller/config/yang/config/sfc_lisp/impl/SfcLispModule.java
@@ -15,7 +15,7 @@ import org.opendaylight.sfc.sfc_lisp.provider.LispUpdater;
 import org.opendaylight.sfc.sfc_lisp.provider.listener.SfcLispRspEntryDataListener;
 import org.opendaylight.sfc.sfc_lisp.provider.listener.SfcLispSfEntryDataListener;
 import org.opendaylight.sfc.sfc_lisp.provider.listener.SfcLispSffDataListener;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.mapping.database.rev150314.LfmMappingDatabaseService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.mappingservice.rev150820.MappingserviceService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -38,7 +38,7 @@ public class SfcLispModule extends org.opendaylight.controller.config.yang.confi
     @Override
     public java.lang.AutoCloseable createInstance() {
         final OpendaylightSfc odlSfc = OpendaylightSfc.getOpendaylightSfcObj();
-        LfmMappingDatabaseService lfmService = getRpcRegistryDependency().getRpcService(LfmMappingDatabaseService.class);
+        MappingserviceService lfmService = getRpcRegistryDependency().getRpcService(MappingserviceService.class);
         final LispUpdater lispUpdater = new LispUpdater(lfmService);
 
         // listeners

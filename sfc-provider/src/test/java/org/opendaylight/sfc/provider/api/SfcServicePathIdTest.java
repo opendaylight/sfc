51ba8a584eaf95e9208045e90ef4fb6c1b38fd26
Reinaldo Penno <rapenno@gmail.com>
2015-09-01 22:23:45 -0700

    Test coverage for Service Path ID
    - Hopefully 100% test coverage for Service Path ID file and algorithm
    - Refactored RSP Test to use same abstract class as SPID
    
    Change-Id: Iffe877abcea245d00d7f69e9a8f1dc2fdcbfefb7
    Signed-off-by: Reinaldo Penno <rapenno@gmail.com>

---
+++ sfc-provider/src/test/java/org/opendaylight/sfc/provider/api/SfcServicePathIdTest.java
@@ -0,0 +1,167 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+
+package org.opendaylight.sfc.provider.api;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+
+import static org.junit.Assert.*;
+
+/**
+ * SfcServicePathId Tester.
+ *
+ * @author Reinaldo Penno (rapenno@gmail.com)
+ * @since       2015-09-01
+ * @version 1.0
+ */
+
+public class SfcServicePathIdTest  extends AbstractSfcRendererServicePathAPITest {
+
+
+    LinkedHashSet<Integer> pathIdSet = new LinkedHashSet<Integer>();
+    LinkedList<Integer> pathIdList = new LinkedList<Integer>(Arrays.asList(0, 1, 63, 64, 65, 1023, 1024, 1025));
+    LinkedList<Integer> FlippedPathIdList = new LinkedList<Integer>(
+            Arrays.asList(8388608, 8388608, 8388671, 8388672, 8388673, 8389631, 8389632, 8389633));
+
+    /**
+     *
+     * Method: generatePathIdRandomIncrements()
+     *
+     */
+    @Test
+    public void testGeneratePathIdRandomIncrements() throws Exception {
+
+        int i;
+        for (i = 0; i < 20; i++) {
+            assertTrue(pathIdSet.add(SfcServicePathId.generatePathIdRandomIncrements()));
+        }
+        pathIdSet.clear();
+    }
+
+    /**
+     *
+     * Method: check_and_allocate_pathid(long pathid)
+     *
+     */
+    @Test
+    public void testCheck_and_allocate_pathidPathid() throws Exception {
+        for (Integer pathId : pathIdList) {
+            assertNotEquals(-1, SfcServicePathId.check_and_allocate_pathid(pathId));
+        }
+        for (Integer pathId : pathIdList) {
+            assertTrue(SfcServicePathId.free_pathid(pathId));
+        }
+    }
+
+    /**
+     *
+     * Method: check_and_allocate_symmetric_pathid(long pathid)
+     *
+     */
+    @Test
+    public void testCheck_and_allocate_symmetric_pathid() throws Exception {
+        for (Integer pathId : pathIdList) {
+            assertNotEquals(-1, SfcServicePathId.check_and_allocate_symmetric_pathid(pathId));
+        }
+        for (Integer pathId : FlippedPathIdList) {
+            assertFalse(SfcServicePathId.check_suitable_pathid(pathId));
+        }
+        for (Integer pathId : FlippedPathIdList) {
+            assertTrue(SfcServicePathId.free_pathid(pathId));
+        }
+    }
+
+    /**
+     *
+     * Method: check_and_allocate_pathid()
+     *
+     */
+    @Test
+    public void testCheck_and_allocate_pathid() throws Exception {
+        int i;
+        for (i = 0; i < 20; i++) {
+            int pathId;
+            pathId = SfcServicePathId.generatePathIdRandomIncrements();
+            assertNotEquals(-1, pathId);
+            assertTrue(pathIdSet.add(pathId));
+            assertTrue(SfcServicePathId.allocate_pathid(pathId));
+        }
+        for (Integer pathId : pathIdSet) {
+            assertFalse(SfcServicePathId.check_suitable_pathid(pathId));
+        }
+        for (Integer pathId : pathIdSet) {
+            assertTrue(SfcServicePathId.free_pathid(pathId));
+        }
+        pathIdSet.clear();
+    }
+
+    /**
+     *
+     * Method: allocate_pathid(long pathid)
+     *
+     */
+    @Test
+    public void testAllocate_pathid() throws Exception {
+        for (Integer pathId : pathIdList) {
+            assertTrue(SfcServicePathId.allocate_pathid(pathId));
+        }
+        for (Integer pathId : pathIdList) {
+            assertFalse(SfcServicePathId.check_suitable_pathid(pathId));
+        }
+        for (Integer pathId : pathIdList) {
+            assertTrue(SfcServicePathId.free_pathid(pathId));
+        }
+    }
+
+    /**
+     *
+     * Method: check_suitable_pathid(long pathid)
+     *
+     */
+    @Test
+    public void testCheck_suitable_pathid() throws Exception {
+        for (Integer pathId : pathIdList) {
+            assertTrue(SfcServicePathId.check_suitable_pathid(pathId));
+        }
+        for (Integer pathId : pathIdList) {
+            assertTrue(SfcServicePathId.allocate_pathid(pathId));
+        }
+        for (Integer pathId : pathIdList) {
+            assertFalse(SfcServicePathId.check_suitable_pathid(pathId));
+        }
+        for (Integer pathId : pathIdList) {
+            assertTrue(SfcServicePathId.free_pathid(pathId));
+        }
+        for (Integer pathId : pathIdList) {
+            assertTrue(SfcServicePathId.check_suitable_pathid(pathId));
+        }
+    }
+
+    /**
+     *
+     * Method: free_pathid(long pathid)
+     *
+     */
+    @Test
+    public void testFree_pathid() throws Exception {
+        for (Integer pathId : pathIdList) {
+            assertTrue(SfcServicePathId.allocate_pathid(pathId));
+        }
+        for (Integer pathId : pathIdList) {
+            assertTrue(SfcServicePathId.free_pathid(pathId));
+        }
+        for (Integer pathId : pathIdList) {
+            assertTrue(SfcServicePathId.check_suitable_pathid(pathId));
+        }
+    }
+}

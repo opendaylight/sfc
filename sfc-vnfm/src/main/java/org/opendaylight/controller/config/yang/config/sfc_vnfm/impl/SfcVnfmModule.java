ae6daae4d3922e34dc3eb3d8347ec207b8bc042a
Reinaldo Penno <rapenno@gmail.com>
2015-10-27 23:56:23 -0700

    Bug 4544 - SFC-VNFM Interface Implementation
    Change-Id: I5b2c150df649e1a1ec1b484225754209d83dea29
    Signed-off-by: Reinaldo Penno <rapenno@gmail.com>

---
+++ sfc-vnfm/src/main/java/org/opendaylight/controller/config/yang/config/sfc_vnfm/impl/SfcVnfmModule.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+
+package org.opendaylight.controller.config.yang.config.sfc_vnfm.impl;
+
+import org.opendaylight.sfc.provider.OpendaylightSfc;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutionException;
+
+public class SfcVnfmModule extends org.opendaylight.controller.config.yang.config.sfc_vnfm.impl.AbstractSfcVnfmModule {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SfcVnfmModule.class);
+
+
+    public SfcVnfmModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public SfcVnfmModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.controller.config.yang.config.sfc_vnfm.impl.SfcVnfmModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        final OpendaylightSfc opendaylightSfc = OpendaylightSfc.getOpendaylightSfcObj();
+        LOG.info("SFC OVS module initialized");
+
+        // close()
+        final class AutoCloseableSfcVnfm implements AutoCloseable {
+
+            @Override
+            public void close() {
+
+                try {
+                    opendaylightSfc.close();
+                } catch (ExecutionException | InterruptedException e) {
+                    LOG.error("\nFailed to close SfcOvsModule instance {} cleanly", this);
+                }
+                LOG.info("SfcOvsModule (instance {}) torn down", this);
+            }
+        }
+
+        return new AutoCloseableSfcVnfm();
+    }
+
+}

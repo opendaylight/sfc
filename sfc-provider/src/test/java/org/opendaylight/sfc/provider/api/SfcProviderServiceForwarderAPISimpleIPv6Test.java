7f812d7c26471d7b907b4d17e64a1c854d12ad3a
Brady Johnson <brady.allen.johnson@ericsson.com>
2015-11-05 12:32:22 +0100

    SFF SF-dict sff-sf-dpl is a name, not a DPL
    - Instead of redefining the DPL in this dictionary,
      refer to the SF DPL name.
    - Allows for much better referential integrity
    - Patch Set 6 removed duplicate SF type
    - Patch Set 7 fixes the python test failures
    - Patch Set 8 adds sf retrieval from ODL to the sfc_agent
    - Patch Set 9 and 10 rebasing
    - Patch Set 12 rebasing again
    
    Change-Id: I29ff6b0b7ce9a00c265ca9dbc62fb740e0159130
    Signed-off-by: Brady Johnson <brady.allen.johnson@ericsson.com>

--- sfc-provider/src/test/java/org/opendaylight/sfc/provider/api/SfcProviderServiceForwarderAPISimpleIPv6Test.java
+++ sfc-provider/src/test/java/org/opendaylight/sfc/provider/api/SfcProviderServiceForwarderAPISimpleIPv6Test.java
@@ -239,13 +239,12 @@ public class SfcProviderServiceForwarderAPISimpleIPv6Test extends AbstractDataSt
 
             List<ServiceFunctionDictionary> sfDictionaryList = new ArrayList<>();
             ServiceFunction serviceFunction = sfList.get(i);
-            SfDataPlaneLocator sfDPLocator = serviceFunction.getSfDataPlaneLocator().get(0);
-            SffSfDataPlaneLocatorBuilder sffSfDataPlaneLocatorBuilder = new SffSfDataPlaneLocatorBuilder(sfDPLocator);
+            SffSfDataPlaneLocatorBuilder sffSfDataPlaneLocatorBuilder = new SffSfDataPlaneLocatorBuilder();
+            sffSfDataPlaneLocatorBuilder.setSfDplName(serviceFunction.getSfDataPlaneLocator().get(0).getName());
             SffSfDataPlaneLocator sffSfDataPlaneLocator = sffSfDataPlaneLocatorBuilder.build();
             ServiceFunctionDictionaryBuilder dictionaryEntryBuilder = new ServiceFunctionDictionaryBuilder();
             dictionaryEntryBuilder.setName(serviceFunction.getName())
                 .setKey(new ServiceFunctionDictionaryKey(serviceFunction.getName()))
-                .setType(serviceFunction.getType())
                 .setSffSfDataPlaneLocator(sffSfDataPlaneLocator)
                 .setFailmode(Open.class)
                 .setSffInterfaces(null);
@@ -332,13 +331,12 @@ public class SfcProviderServiceForwarderAPISimpleIPv6Test extends AbstractDataSt
 
         ServiceFunction sf = sfList.get(0);
         SfDataPlaneLocator sfDPLocator = sf.getSfDataPlaneLocator().get(0);
-        SffSfDataPlaneLocatorBuilder sffSfDataPlaneLocatorBuilder = new SffSfDataPlaneLocatorBuilder(sfDPLocator)
-                .setTransport(SlTransportType.class);
+        SffSfDataPlaneLocatorBuilder sffSfDataPlaneLocatorBuilder = new SffSfDataPlaneLocatorBuilder();
+        sffSfDataPlaneLocatorBuilder.setSfDplName(sf.getSfDataPlaneLocator().get(0).getName());
         SffSfDataPlaneLocator sffSfDataPlaneLocator = sffSfDataPlaneLocatorBuilder.build();
         ServiceFunctionDictionaryBuilder dictionaryEntryBuilder = new ServiceFunctionDictionaryBuilder();
         dictionaryEntryBuilder.setName(sf.getName())
             .setKey(new ServiceFunctionDictionaryKey(sf.getName()))
-            .setType(sf.getType())
             .setSffSfDataPlaneLocator(sffSfDataPlaneLocator)
             .setFailmode(Open.class)
             .setSffInterfaces(null);

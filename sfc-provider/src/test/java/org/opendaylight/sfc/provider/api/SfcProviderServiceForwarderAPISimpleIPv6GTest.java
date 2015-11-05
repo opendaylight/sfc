a8cefe67c0bcabc99941fd4cbbb1de1254e39c44
Brady Johnson <brady.allen.johnson@ericsson.com>
2015-11-05 12:32:22 +0100

    SFF SF-dict sff-sf-dpl is a name, not a DPL
    - Instead of redefining the DPL in this dictionary,
      refer to the SF DPL name.
    - Allows for much better referential integrity
    - Patch Set 5 removed duplicate SF type
    
    Change-Id: I29ff6b0b7ce9a00c265ca9dbc62fb740e0159130
    Signed-off-by: Brady Johnson <brady.allen.johnson@ericsson.com>

--- sfc-provider/src/test/java/org/opendaylight/sfc/provider/api/SfcProviderServiceForwarderAPISimpleIPv6GTest.java
+++ sfc-provider/src/test/java/org/opendaylight/sfc/provider/api/SfcProviderServiceForwarderAPISimpleIPv6GTest.java
@@ -152,13 +152,12 @@ public class SfcProviderServiceForwarderAPISimpleIPv6GTest extends AbstractDataS
 
             List<ServiceFunctionDictionary> sfDictionaryList = new ArrayList<>();
             ServiceFunction serviceFunction = sfList.get(i);
-            SfDataPlaneLocator sfDPLocator = serviceFunction.getSfDataPlaneLocator().get(0);
-            SffSfDataPlaneLocatorBuilder sffSfDataPlaneLocatorBuilder = new SffSfDataPlaneLocatorBuilder(sfDPLocator);
+            SffSfDataPlaneLocatorBuilder sffSfDataPlaneLocatorBuilder = new SffSfDataPlaneLocatorBuilder();
+            sffSfDataPlaneLocatorBuilder.setSfDplName(serviceFunction.getSfDataPlaneLocator().get(0).getName());
             SffSfDataPlaneLocator sffSfDataPlaneLocator = sffSfDataPlaneLocatorBuilder.build();
             ServiceFunctionDictionaryBuilder dictionaryEntryBuilder = new ServiceFunctionDictionaryBuilder();
             dictionaryEntryBuilder.setName(serviceFunction.getName())
                 .setKey(new ServiceFunctionDictionaryKey(serviceFunction.getName()))
-                .setType(serviceFunction.getType())
                 .setSffSfDataPlaneLocator(sffSfDataPlaneLocator)
                 .setFailmode(Open.class)
                 .setSffInterfaces(null);
@@ -243,13 +242,12 @@ public class SfcProviderServiceForwarderAPISimpleIPv6GTest extends AbstractDataS
         List<ServiceFunctionDictionary> dictionary = new ArrayList<>();
 
         ServiceFunction sf = sfList.get(0);
-        SfDataPlaneLocator sfDPLocator = sf.getSfDataPlaneLocator().get(0);
-        SffSfDataPlaneLocatorBuilder sffSfDataPlaneLocatorBuilder = new SffSfDataPlaneLocatorBuilder(sfDPLocator);
+        SffSfDataPlaneLocatorBuilder sffSfDataPlaneLocatorBuilder = new SffSfDataPlaneLocatorBuilder();
+        sffSfDataPlaneLocatorBuilder.setSfDplName(sf.getSfDataPlaneLocator().get(0).getName());
         SffSfDataPlaneLocator sffSfDataPlaneLocator = sffSfDataPlaneLocatorBuilder.build();
         ServiceFunctionDictionaryBuilder dictionaryEntryBuilder = new ServiceFunctionDictionaryBuilder();
         dictionaryEntryBuilder.setName(sf.getName())
             .setKey(new ServiceFunctionDictionaryKey(sf.getName()))
-            .setType(sf.getType())
             .setSffSfDataPlaneLocator(sffSfDataPlaneLocator)
             .setFailmode(Open.class)
             .setSffInterfaces(null);

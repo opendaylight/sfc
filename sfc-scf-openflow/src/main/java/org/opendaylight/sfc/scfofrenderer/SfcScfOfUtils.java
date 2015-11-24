dd3503f5debe6933068eacb36fc07eab36c48c8b
Ruijing Guo <ruijing.guo@intel.com>
2015-11-24 19:56:58 +0000

    SFC Standalone Classifier
    1. Add unit test for SfcMatch
    2. Add unit test for SfcScfOfScfDataListener
    3. Add unit test for SfcScfOfScfProcessor
    
    Todo List:
    1. add more unit tests
    2. move SfcOvsPortUtils to sfc-ovs
    3. move SfcNshHeader to sfc-provider
    
    Change-Id: I86105872a9aa9a0763bec2a96cb7a84a18eb6034
    Signed-off-by: Ruijing Guo <ruijing.guo@intel.com>

---
+++ sfc-scf-openflow/src/main/java/org/opendaylight/sfc/scfofrenderer/SfcScfOfUtils.java
@@ -0,0 +1,220 @@
+/*
+ * Copyright (c) 2015 Intel Ltd. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.sfc.scfofrenderer;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.opendaylight.sfc.util.openflow.SfcOpenflowUtils;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.FlowBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.FlowKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.TableKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.flow.InstructionsBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.flow.Match;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.flow.MatchBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.instruction.GoToTableCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.instruction.go.to.table._case.GoToTableBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.list.Instruction;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.list.InstructionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.list.InstructionKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.OutputPortValues;
+
+public class SfcScfOfUtils {
+    private static final short TABLE_INDEX_CLASSIFIER = 0;
+    private static final short TABLE_INDEX_INGRESS_TRANSPORT = 1;
+
+    private static final int FLOW_PRIORITY_CLASSIFIER = 1000;
+    private static final int FLOW_PRIORITY_MATCH_ANY = 5;
+
+   /**
+    * Initialize classifier flow table.
+    * The function returns true if successful.
+    * The function returns false if unsuccessful.
+    *
+    * @param  nodeName flow table node name
+    * @return          initialization result
+    */
+    public static boolean initClassifierTable(String nodeName) {
+        int order = 0;
+
+        if (nodeName == null) {
+            return false;
+        }
+
+        List<Instruction> instructions = new ArrayList<Instruction>();
+        GoToTableBuilder gotoIngress = SfcOpenflowUtils.createActionGotoTable(TABLE_INDEX_INGRESS_TRANSPORT);
+
+        InstructionBuilder ib = new InstructionBuilder();
+        ib.setKey(new InstructionKey(order));
+        ib.setOrder(order++);
+        ib.setInstruction(new GoToTableCaseBuilder().setGoToTable(gotoIngress.build()).build());
+        instructions.add(ib.build());
+
+        MatchBuilder match = new MatchBuilder();
+
+        InstructionsBuilder isb = new InstructionsBuilder();
+        isb.setInstruction(instructions);
+
+        FlowBuilder fb = SfcOpenflowUtils.createFlowBuilder(TABLE_INDEX_CLASSIFIER, FLOW_PRIORITY_MATCH_ANY, "MatchAny",
+                match, isb);
+        return SfcOpenflowUtils.writeFlowToDataStore(nodeName, fb);
+    }
+
+   /**
+    * create classifier out flow.
+    * The function returns true if successful.
+    * The function returns false if unsuccessful.
+    *
+    * @param  nodeName flow table node name
+    * @param  flowKey  flow key
+    * @param  match    flow match
+    * @param  sfcNshHeader nsh header
+    * @param  outPort  flow out port
+    * @return          create flow result
+    */
+    public static boolean createClassifierOutFlow(String nodeName, String flowKey, Match match, SfcNshHeader sfcNshHeader,
+            Long outPort) {
+        int order = 0;
+
+        if ((nodeName == null) || (flowKey == null) || (sfcNshHeader == null) || (sfcNshHeader.getVxlanIpDst()==null)) {
+            return false;
+        }
+
+        String dstIp = sfcNshHeader.getVxlanIpDst().getValue();
+        Action setTunIpDst = SfcOpenflowUtils.createActionNxSetTunIpv4Dst(dstIp, order++);
+        Action setNsp = SfcOpenflowUtils.createActionNxSetNsp(sfcNshHeader.getNshNsp(), order++);
+        Action setNsi = SfcOpenflowUtils.createActionNxSetNsi(sfcNshHeader.getNshStartNsi(), order++);
+        Action setC1 = SfcOpenflowUtils.createActionNxSetNshc1(sfcNshHeader.getNshMetaC1(), order++);
+        Action setC2 = SfcOpenflowUtils.createActionNxSetNshc2(sfcNshHeader.getNshMetaC2(), order++);
+        Action setC3 = SfcOpenflowUtils.createActionNxSetNshc3(sfcNshHeader.getNshMetaC3(), order++);
+        Action setC4 = SfcOpenflowUtils.createActionNxSetNshc4(sfcNshHeader.getNshMetaC4(), order++);
+
+        Action out = null;
+        if (outPort == null) {
+            out = SfcOpenflowUtils.createActionOutPort(OutputPortValues.INPORT.toString(), order++);
+        } else {
+            out = SfcOpenflowUtils.createActionOutPort(outPort.intValue(), order++);
+        }
+
+        FlowBuilder flowb = new FlowBuilder();
+        flowb.setId(new FlowId(flowKey))
+            .setTableId(TABLE_INDEX_CLASSIFIER)
+            .setKey(new FlowKey(new FlowId(flowKey)))
+            .setPriority(Integer.valueOf(FLOW_PRIORITY_CLASSIFIER))
+            .setMatch(match)
+            .setInstructions(SfcOpenflowUtils.createInstructionsBuilder(SfcOpenflowUtils
+                .createActionsInstructionBuilder(setTunIpDst, setNsp, setNsi, setC1, setC2, setC3, setC4, out))
+                .build());
+        return SfcOpenflowUtils.writeFlowToDataStore(nodeName, flowb);
+    }
+
+   /**
+    * create classifier in flow.
+    * The function returns true if successful.
+    * The function returns false if unsuccessful.
+    *
+    * @param  nodeName flow table node name
+    * @param  flowKey  flow key
+    * @param  sfcNshHeader nsh header
+    * @param  outPort  flow out port
+    * @return          create in result
+    */
+    public static boolean createClassifierInFlow(String nodeName, String flowKey, SfcNshHeader sfcNshHeader, Long outPort) {
+        int order = 0;
+
+        if ((nodeName == null) || (flowKey == null) || (sfcNshHeader == null) || (sfcNshHeader.getVxlanIpDst()==null)) {
+            return false;
+        }
+
+        MatchBuilder mb = new MatchBuilder();
+
+        SfcOpenflowUtils.addMatchNshNsp(mb, sfcNshHeader.getNshNsp());
+        SfcOpenflowUtils.addMatchNshNsi(mb, sfcNshHeader.getNshEndNsi());
+
+        Action out = null;
+        if (outPort == null) {
+            out = SfcOpenflowUtils.createActionOutPort(OutputPortValues.INPORT.toString(), order++);
+        } else {
+            out = SfcOpenflowUtils.createActionOutPort(outPort.intValue(), order++);
+        }
+
+        FlowBuilder flowb = new FlowBuilder();
+        flowb.setId(new FlowId(flowKey))
+            .setTableId(TABLE_INDEX_CLASSIFIER)
+            .setKey(new FlowKey(new FlowId(flowKey)))
+            .setPriority(Integer.valueOf(FLOW_PRIORITY_CLASSIFIER))
+            .setMatch(mb.build())
+            .setInstructions(SfcOpenflowUtils.createInstructionsBuilder(SfcOpenflowUtils
+                .createActionsInstructionBuilder(out))
+                .build());
+        return SfcOpenflowUtils.writeFlowToDataStore(nodeName, flowb);
+    }
+
+   /**
+    * create classifier relay flow.
+    * The function returns true if successful.
+    * The function returns false if unsuccessful.
+    *
+    * @param  nodeName flow table node name
+    * @param  flowKey  flow key
+    * @param  sfcNshHeader nsh header
+    * @return          create relay result
+    */
+    public static boolean createClassifierRelayFlow(String nodeName, String flowKey, SfcNshHeader sfcNshHeader) {
+        int order = 0;
+
+        if ((nodeName == null) || (flowKey == null) || (sfcNshHeader == null) || (sfcNshHeader.getVxlanIpDst()==null)) {
+            return false;
+        }
+
+        MatchBuilder mb = new MatchBuilder();
+
+        SfcOpenflowUtils.addMatchNshNsp(mb, sfcNshHeader.getNshNsp());
+        SfcOpenflowUtils.addMatchNshNsi(mb, sfcNshHeader.getNshEndNsi());
+
+        String dstIp = sfcNshHeader.getVxlanIpDst().getValue();
+        Action setTunIpDst = SfcOpenflowUtils.createActionNxSetTunIpv4Dst(dstIp, order++);
+        Action mvNsp = SfcOpenflowUtils.createActionNxMoveNsp(order++);
+        Action mvNsi = SfcOpenflowUtils.createActionNxMoveNsi(order++);
+        Action mvC1 = SfcOpenflowUtils.createActionNxMoveNsc1(order++);
+        Action mvC2 = SfcOpenflowUtils.createActionNxMoveNsc2(order++);
+        Action out = SfcOpenflowUtils.createActionOutPort(OutputPortValues.INPORT.toString(), order++);
+
+        FlowBuilder flowb = new FlowBuilder();
+        flowb.setId(new FlowId(flowKey))
+            .setTableId(TABLE_INDEX_CLASSIFIER)
+            .setKey(new FlowKey(new FlowId(flowKey)))
+            .setPriority(Integer.valueOf(FLOW_PRIORITY_CLASSIFIER))
+            .setMatch(mb.build())
+            .setInstructions(SfcOpenflowUtils.createInstructionsBuilder(SfcOpenflowUtils
+                .createActionsInstructionBuilder(setTunIpDst, mvNsp, mvNsi, mvC1, mvC2, out))
+                .build());
+        return SfcOpenflowUtils.writeFlowToDataStore(nodeName, flowb);
+    }
+
+   /**
+    * delete classifier flow.
+    * The function returns true if successful.
+    * The function returns false if unsuccessful.
+    *
+    * @param  nodeName flow table node name
+    * @param  flowKey  flow key
+    * @return          delete result
+    */
+    public static boolean deleteClassifierFlow(String nodeName, String flowKey) {
+
+       if ((nodeName == null) || (flowKey == null)) {
+           return false;
+       }
+
+       return SfcOpenflowUtils.removeFlowFromDataStore(nodeName, new TableKey(TABLE_INDEX_CLASSIFIER),
+                                                            new FlowKey(new FlowId(flowKey.toString())));
+   }
+}
---
+++ sfc-scf-openflow/src/test/java/org/opendaylight/sfc/scfofrenderer/SfcScfMatchTest.java
@@ -0,0 +1,274 @@
+/*
+ * Copyright (c) 2015 Intel Ltd. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.sfc.scfofrenderer;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.flow.Match;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeConnectorId;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev150317.access.lists.acl.access.list.entries.ace.Matches;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev150317.access.lists.acl.access.list.entries.ace.MatchesBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev150317.access.lists.acl.access.list.entries.ace.matches.ace.type.AceIp;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev150317.access.lists.acl.access.list.entries.ace.matches.ace.type.AceIpBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev150317.access.lists.acl.access.list.entries.ace.matches.ace.type.AceEth;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev150317.access.lists.acl.access.list.entries.ace.matches.ace.type.AceEthBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev150317.access.lists.acl.access.list.entries.ace.matches.ace.type.ace.ip.ace.ip.version.AceIpv4;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev150317.access.lists.acl.access.list.entries.ace.matches.ace.type.ace.ip.ace.ip.version.AceIpv4Builder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev150317.access.lists.acl.access.list.entries.ace.matches.ace.type.ace.ip.ace.ip.version.AceIpv6;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev150317.access.lists.acl.access.list.entries.ace.matches.ace.type.ace.ip.ace.ip.version.AceIpv6Builder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev100924.MacAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Prefix;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv6Prefix;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.layer._3.match.Ipv4Match;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.layer._3.match.Ipv6Match;
+
+public class SfcScfMatchTest {
+
+    @Test
+    public void testSetPortMatch() {
+        Match match;
+        match = new SfcScfMatch()
+                    .setPortMatch(null)
+                    .build();
+        Assert.assertNull(match.getInPort());
+
+        NodeConnectorId inPort = new NodeConnectorId("1");
+        match = new SfcScfMatch()
+                    .setPortMatch(inPort)
+                    .build();
+        Assert.assertEquals(match.getInPort(), inPort);
+    }
+
+    @Test
+    public void testSetEthMatch() {
+        Match match;
+        match = new SfcScfMatch()
+                     .setAclMatch(null)
+                     .build();
+        Assert.assertNull(match.getEthernetMatch());
+
+        AceEth ace;
+        Matches matches;
+
+        ace  = new AceEthBuilder()
+                    .setSourceMacAddress(null)
+                    .setDestinationMacAddress(null)
+                    .build();
+
+        matches = new MatchesBuilder()
+                    .setAceType(ace)
+                    .build();
+
+        match = new SfcScfMatch()
+                    .setAclMatch(matches)
+                    .build();
+
+        Assert.assertNull(match.getEthernetMatch());
+
+        ace  = new AceEthBuilder()
+                    .setSourceMacAddress(null)
+                    .setDestinationMacAddress(new MacAddress("00:00:00:00:00:01"))
+                    .build();
+
+        matches = new MatchesBuilder()
+                    .setAceType(ace)
+                    .build();
+
+        match = new SfcScfMatch()
+                    .setAclMatch(matches)
+                    .build();
+
+        Assert.assertNull(match.getEthernetMatch().getEthernetSource());
+        Assert.assertEquals(match.getEthernetMatch().getEthernetDestination().getAddress(), new MacAddress("00:00:00:00:00:01"));
+
+        ace  = new AceEthBuilder()
+                    .setSourceMacAddress(new MacAddress("00:00:00:00:00:01"))
+                    .setDestinationMacAddress(null)
+                    .build();
+
+        matches = new MatchesBuilder()
+                    .setAceType(ace)
+                    .build();
+
+        match = new SfcScfMatch()
+                    .setAclMatch(matches)
+                    .build();
+
+        Assert.assertEquals(match.getEthernetMatch().getEthernetSource().getAddress(), new MacAddress("00:00:00:00:00:01"));
+        Assert.assertNull(match.getEthernetMatch().getEthernetDestination());
+
+        ace  = new AceEthBuilder()
+                    .setSourceMacAddress(new MacAddress("00:00:00:00:00:01"))
+                    .setDestinationMacAddress(new MacAddress("00:00:00:00:00:01"))
+                    .build();
+
+        matches = new MatchesBuilder()
+                    .setAceType(ace)
+                    .build();
+
+        match = new SfcScfMatch()
+                    .setAclMatch(matches)
+                    .build();
+
+        Assert.assertEquals(match.getEthernetMatch().getEthernetSource().getAddress(), new MacAddress("00:00:00:00:00:01"));
+        Assert.assertEquals(match.getEthernetMatch().getEthernetDestination().getAddress(), new MacAddress("00:00:00:00:00:01"));
+    }
+
+    @Test
+    public void testSetIpMatch() {
+        Match match;
+        match = new SfcScfMatch()
+                     .setAclMatch(null)
+                     .build();
+
+        Assert.assertNull(match.getIpMatch());
+        Assert.assertNull(match.getLayer3Match());
+        Assert.assertNull(match.getLayer4Match());
+        Assert.assertNull(match.getIcmpv4Match());
+        Assert.assertNull(match.getIcmpv6Match());
+
+        AceIp ip;
+        AceIpv4 ipv4;
+        Matches matches;
+
+        ipv4  = new AceIpv4Builder()
+                    .setSourceIpv4Network(null)
+                    .setDestinationIpv4Network(new Ipv4Prefix("1.1.1.1/24"))
+                    .build();
+
+        ip = new AceIpBuilder()
+                    .setAceIpVersion(ipv4)
+                    .build();
+
+        matches = new MatchesBuilder()
+                    .setAceType(ip)
+                    .build();
+
+        match = new SfcScfMatch()
+                    .setAclMatch(matches)
+                    .build();
+
+        Assert.assertNull(((Ipv4Match)match.getLayer3Match()).getIpv4Source());
+        Assert.assertEquals(((Ipv4Match)match.getLayer3Match()).getIpv4Destination(), new Ipv4Prefix("1.1.1.1/24"));
+
+        ipv4  = new AceIpv4Builder()
+                    .setSourceIpv4Network(new Ipv4Prefix("1.1.1.1/24"))
+                    .setDestinationIpv4Network(null)
+                    .build();
+
+        ip = new AceIpBuilder()
+                    .setAceIpVersion(ipv4)
+                    .build();
+
+        matches = new MatchesBuilder()
+                    .setAceType(ip)
+                    .build();
+
+        match = new SfcScfMatch()
+                    .setAclMatch(matches)
+                    .build();
+        Assert.assertEquals(((Ipv4Match)match.getLayer3Match()).getIpv4Source(), new Ipv4Prefix("1.1.1.1/24"));
+        Assert.assertNull(((Ipv4Match)match.getLayer3Match()).getIpv4Destination());
+
+        ipv4  = new AceIpv4Builder()
+                    .setSourceIpv4Network(new Ipv4Prefix("1.1.1.1/24"))
+                    .setDestinationIpv4Network(new Ipv4Prefix("2.2.2.2/24"))
+                    .build();
+
+        ip = new AceIpBuilder()
+                    .setAceIpVersion(ipv4)
+                    .build();
+
+        matches = new MatchesBuilder()
+                    .setAceType(ip)
+                    .build();
+
+        match = new SfcScfMatch()
+                    .setAclMatch(matches)
+                    .build();
+        Assert.assertEquals(((Ipv4Match)match.getLayer3Match()).getIpv4Source(), new Ipv4Prefix("1.1.1.1/24"));
+        Assert.assertEquals(((Ipv4Match)match.getLayer3Match()).getIpv4Destination(), new Ipv4Prefix("2.2.2.2/24"));
+    }
+
+    @Test
+    public void testSetIpv6Match() {
+        Match match;
+        match = new SfcScfMatch()
+                     .setAclMatch(null)
+                     .build();
+
+        Assert.assertNull(match.getIpMatch());
+        Assert.assertNull(match.getLayer3Match());
+        Assert.assertNull(match.getLayer4Match());
+        Assert.assertNull(match.getIcmpv6Match());
+        Assert.assertNull(match.getIcmpv6Match());
+
+        AceIp ip;
+        AceIpv6 ipv6;
+        Matches matches;
+
+        ipv6  = new AceIpv6Builder()
+                    .setSourceIpv6Network(null)
+                    .setDestinationIpv6Network(new Ipv6Prefix("0000:0000:0000:0000:0000:0000:0000:0001/128"))
+                    .build();
+
+        ip = new AceIpBuilder()
+                    .setAceIpVersion(ipv6)
+                    .build();
+
+        matches = new MatchesBuilder()
+                    .setAceType(ip)
+                    .build();
+
+        match = new SfcScfMatch()
+                    .setAclMatch(matches)
+                    .build();
+
+        Assert.assertNull(((Ipv6Match)match.getLayer3Match()).getIpv6Source());
+        Assert.assertEquals(((Ipv6Match)match.getLayer3Match()).getIpv6Destination(), new Ipv6Prefix("0000:0000:0000:0000:0000:0000:0000:0001/128"));
+
+        ipv6  = new AceIpv6Builder()
+                    .setSourceIpv6Network(new Ipv6Prefix("0000:0000:0000:0000:0000:0000:0000:0001/128"))
+                    .setDestinationIpv6Network(null)
+                    .build();
+
+        ip = new AceIpBuilder()
+                    .setAceIpVersion(ipv6)
+                    .build();
+
+        matches = new MatchesBuilder()
+                    .setAceType(ip)
+                    .build();
+
+        match = new SfcScfMatch()
+                    .setAclMatch(matches)
+                    .build();
+        Assert.assertEquals(((Ipv6Match)match.getLayer3Match()).getIpv6Source(), new Ipv6Prefix("0000:0000:0000:0000:0000:0000:0000:0001/128"));
+        Assert.assertNull(((Ipv6Match)match.getLayer3Match()).getIpv6Destination());
+
+        ipv6  = new AceIpv6Builder()
+                    .setSourceIpv6Network(new Ipv6Prefix("0000:0000:0000:0000:0000:0000:0000:0001/128"))
+                    .setDestinationIpv6Network(new Ipv6Prefix("0000:0000:0000:0000:0000:0000:0000:0002/128"))
+                    .build();
+
+        ip = new AceIpBuilder()
+                    .setAceIpVersion(ipv6)
+                    .build();
+
+        matches = new MatchesBuilder()
+                    .setAceType(ip)
+                    .build();
+
+        match = new SfcScfMatch()
+                    .setAclMatch(matches)
+                    .build();
+        Assert.assertEquals(((Ipv6Match)match.getLayer3Match()).getIpv6Source(), new Ipv6Prefix("0000:0000:0000:0000:0000:0000:0000:0001/128"));
+        Assert.assertEquals(((Ipv6Match)match.getLayer3Match()).getIpv6Destination(), new Ipv6Prefix("0000:0000:0000:0000:0000:0000:0000:0002/128"));
+    }
+}

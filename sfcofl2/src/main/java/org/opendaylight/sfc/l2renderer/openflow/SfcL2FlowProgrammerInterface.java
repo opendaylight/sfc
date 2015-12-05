4b1014f8e6f42c7131c38b2c9b6f388ca3bb69c4
Brady Johnson <brady.allen.johnson@ericsson.com>
2015-12-05 21:54:36 +0100

    Refactoring RspProcessor
    - Moved convoluted logic from configureIngress() and
      configureEgress() to individual methods for each table.
    - Transport specific (mpls, nsh, vlan) logic has been
      moved to a newly created class hierarchy of SfcRspProcessors.
    - RspProcessorTest updated to reflect changes. Previously
      there were several duplicate calls to the same Flow
      Programmer methods being made, this no longer occurs.
    
    Change-Id: I1ef2b3d5b20b3374640849ca093bc838916629df
    Signed-off-by: Brady Johnson <brady.allen.johnson@ericsson.com>

--- sfcofl2/src/main/java/org/opendaylight/sfc/l2renderer/openflow/SfcL2FlowProgrammerInterface.java
+++ sfcofl2/src/main/java/org/opendaylight/sfc/l2renderer/openflow/SfcL2FlowProgrammerInterface.java
@@ -6,12 +6,11 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.opendaylight.sfc.l2renderer;
+package org.opendaylight.sfc.l2renderer.openflow;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-import org.opendaylight.sfc.l2renderer.openflow.SfcL2FlowWriterInterface;
 import org.opendaylight.sfc.l2renderer.sfg.GroupBucketInfo;
 
 /**
@@ -83,14 +82,19 @@ public interface SfcL2FlowProgrammerInterface {
     //
     // Table 10, Transport Egress
     //
+    public void configureVlanSfTransportEgressFlow(final String sffNodeName, final String srcMac, final String dstMac,
+            final int dstVlan, final String port, final long pathId, final boolean doPktin);
     public void configureVlanTransportEgressFlow(final String sffNodeName, final String srcMac, final String dstMac,
-            final int dstVlan, final String port, final long pathId, final boolean setDscp, final boolean doPktIn);
-
-    public void configureVxlanGpeTransportEgressFlow(final String sffNodeName, final long nshNsp, final short nshNsi,
-            final String port, final boolean isLastHop);
+            final int dstVlan, final String port, final long pathId);
 
     public void configureMplsTransportEgressFlow(final String sffNodeName, final String srcMac, final String dstMac,
-            final long mplsLabel, final String port, final long pathId, boolean setDscp, final boolean doPktIn);
+            final long mplsLabel, final String port, final long pathId);
+
+    public void configureVxlanGpeTransportEgressFlow(final String sffNodeName, final long nshNsp, final short nshNsi,
+            final String port);
+
+    public void configureVxlanGpeLastHopTransportEgressFlow(final String sffNodeName, final long nshNsp, final short nshNsi,
+            final String port);
 
     public void configureNshNscTransportEgressFlow(String sffNodeName, final long nshNsp, final short nshNsi,
             String switchPort);

6cafe394a053b7b37597f70e8b0c76df09b64f4e
Reinaldo Penno <rapenno@gmail.com>
2015-12-25 16:41:51 -0800

    SFC Demo Rest Regression Fixes
    - The idea of this commit is to allow a multi-SFf, multi-SFF demo without
      the need for VMs. All in software under a single interface. This allows
      for much quicker demos and experimentation.
    - In order to have multi-SFFs the Agent now constructs data paths on a per SFF basis. Each
      thread looks up the net hops on its own data path.
    - More fixes related to Yang model changes
    - NSH packet type should come from NSH header and not global arg
    - Implemented requirements.txt python file
    - Fixed find_sf_locator function. Too many scope problems.
    - Fixed all JSON messages related to demo regression
    - Slightly improved Agent start script by allowing passing ODL IP:port
    - Fixed initial agent info message
    - Pesky PEP8 issue
    - Still need to work on SB Rest side. It will be another commit
    
    Change-Id: I274bed3b67f70a1a394b5063edd4bed56dd95ee1
    Signed-off-by: Reinaldo Penno <rapenno@gmail.com>

--- sfc-py/start_agent.sh
+++ sfc-py/start_agent.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-# Need to instsall paramiko: sudo pip3.4 install paramiko
 
 # auto-sff-name means agent will try to discover its SFF name dynamically during
 # start-up and later when it receives a RSP request
-python3.4 sfc/sfc_agent.py --rest --odl-ip-port 192.168.1.5:8181 --auto-sff-name
+python3.5 sfc/sfc_agent.py --rest --odl-ip-port $1 --auto-sff-name

3cc93b22af41ce43afc150caaec793452c824eac
Vladimir Lavor <vlavor@cisco.com>
2016-05-10 15:30:30 +0200

    Introduce IOS-XE (Netconf) renderer for SFC
    Signed-off-by: Vladimir Lavor <vlavor@cisco.com>
    Change-Id: I3ee145fa0a4e5e9d4da7035f7d47d1add5ee011c

---
+++ sfc-renderers/sfc-ios-xe-renderer/src/test/java/org/opendaylight/sfc/sfc_ios_xe/provider/api/SfcIosXeUtilsTest.java
@@ -0,0 +1,198 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.sfc.sfc_ios_xe.provider.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opendaylight.sfc.provider.api.SfcProviderServiceForwarderAPI;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.common.rev151017.SffName;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.function.base.SfDataPlaneLocator;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.service.function.forwarder.base.SffDataPlaneLocator;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.service.function.forwarder.base.SffDataPlaneLocatorBuilder;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.service.function.forwarder.base.sff.data.plane.locator.DataPlaneLocatorBuilder;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.service.function.forwarders.ServiceFunctionForwarderBuilder;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sl.rev140701.data.plane.locator.locator.type.IpBuilder;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sl.rev140701.data.plane.locator.locator.type.MacBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Address;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.PortNumber;
+import org.opendaylight.yang.gen.v1.urn.ios.rev160308.Native;
+import org.opendaylight.yang.gen.v1.urn.ios.rev160308._native.ServiceChain;
+import org.opendaylight.yang.gen.v1.urn.ios.rev160308._native.service.chain.ServiceFunction;
+import org.opendaylight.yang.gen.v1.urn.ios.rev160308._native.service.chain.ServiceFunctionForwarder;
+import org.opendaylight.yang.gen.v1.urn.ios.rev160308._native.service.chain.ServiceFunctionKey;
+import org.opendaylight.yang.gen.v1.urn.ios.rev160308._native.service.chain.ServicePath;
+import org.opendaylight.yang.gen.v1.urn.ios.rev160308._native.service.chain.ServicePathKey;
+import org.opendaylight.yang.gen.v1.urn.ios.rev160308._native.service.chain.service.function.forwarder.Local;
+import org.opendaylight.yang.gen.v1.urn.ios.rev160308._native.service.chain.service.function.forwarder.ServiceFfName;
+import org.opendaylight.yang.gen.v1.urn.ios.rev160308._native.service.chain.service.function.forwarder.ServiceFfNameBuilder;
+import org.opendaylight.yang.gen.v1.urn.ios.rev160308._native.service.chain.service.function.forwarder.ServiceFfNameKey;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertNull;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({SfcProviderServiceForwarderAPI.class})
+public class SfcIosXeUtilsTest {
+
+    private final String ipv4Address = "10.0.0.1";
+    private final String forwarderName = "forwarder";
+
+    @Test
+    public void createLocalForwarder_nullIp() {
+        assertNull(SfcIosXeUtils.createLocalForwarder(null));
+    }
+
+    @Test
+    public void createLocalForwarder() {
+        IpAddress ipAddress = new IpAddress(new Ipv4Address(ipv4Address));
+        ServiceFunctionForwarder result = SfcIosXeUtils.createLocalForwarder(ipAddress);
+        assertNotNull(result);
+        assertEquals(result.getLocal().getIp().getAddress().getValue(), ipv4Address);
+    }
+
+    @Test
+    public void createRemoteForwarder_noForwarderFound() {
+        SffName forwarderSff = new SffName(forwarderName);
+
+        PowerMockito.stub(PowerMockito.method(SfcProviderServiceForwarderAPI.class, "readServiceFunctionForwarder"))
+                .toReturn(null);
+
+        ServiceFfName result = SfcIosXeUtils.createRemoteForwarder(forwarderSff);
+        assertNull(result);
+    }
+
+    @Test
+    public void createRemoteForwarder_noIpLocatorType() {
+        ServiceFunctionForwarderBuilder serviceFunctionForwarderBuilder = new ServiceFunctionForwarderBuilder();
+        List<SffDataPlaneLocator> sffDataPlaneLocators = new ArrayList<>();
+        SffDataPlaneLocatorBuilder sffDataPlaneLocatorBuilder = new SffDataPlaneLocatorBuilder();
+        DataPlaneLocatorBuilder dataPlaneLocatorBuilder = new DataPlaneLocatorBuilder();
+        MacBuilder macBuilder = new MacBuilder();
+        dataPlaneLocatorBuilder.setLocatorType(macBuilder.build());
+        sffDataPlaneLocatorBuilder.setDataPlaneLocator(dataPlaneLocatorBuilder.build());
+        sffDataPlaneLocators.add(sffDataPlaneLocatorBuilder.build());
+        serviceFunctionForwarderBuilder.setSffDataPlaneLocator(sffDataPlaneLocators);
+
+        SffName forwarderSff = new SffName(forwarderName);
+
+        PowerMockito.stub(PowerMockito.method(SfcProviderServiceForwarderAPI.class, "readServiceFunctionForwarder"))
+                .toReturn(serviceFunctionForwarderBuilder.build());
+
+        ServiceFfName result = SfcIosXeUtils.createRemoteForwarder(forwarderSff);
+        assertNull(result);
+    }
+
+    @Test
+    public void createRemoteForwarder() {
+        ServiceFunctionForwarderBuilder serviceFunctionForwarderBuilder = new ServiceFunctionForwarderBuilder();
+        List<SffDataPlaneLocator> sffDataPlaneLocators = new ArrayList<>();
+        SffDataPlaneLocatorBuilder sffDataPlaneLocatorBuilder = new SffDataPlaneLocatorBuilder();
+        DataPlaneLocatorBuilder dataPlaneLocatorBuilder = new DataPlaneLocatorBuilder();
+        IpBuilder ipBuilder = new IpBuilder();
+        ipBuilder.setIp(new IpAddress(new Ipv4Address(ipv4Address)))
+                .setPort(new PortNumber(100));
+        dataPlaneLocatorBuilder.setLocatorType(ipBuilder.build());
+        sffDataPlaneLocatorBuilder.setDataPlaneLocator(dataPlaneLocatorBuilder.build());
+        sffDataPlaneLocators.add(sffDataPlaneLocatorBuilder.build());
+        serviceFunctionForwarderBuilder.setSffDataPlaneLocator(sffDataPlaneLocators);
+
+        SffName forwarderSff = new SffName(forwarderName);
+
+        PowerMockito.stub(PowerMockito.method(SfcProviderServiceForwarderAPI.class, "readServiceFunctionForwarder"))
+                .toReturn(serviceFunctionForwarderBuilder.build());
+
+        ServiceFfName result = SfcIosXeUtils.createRemoteForwarder(forwarderSff);
+        assertNotNull(result);
+        assertEquals(result.getIp().getAddress().getValue(), ipv4Address);
+    }
+
+    @Test
+    public void getDplWithIpLocatorType_noIpLocator() {
+        List<SfDataPlaneLocator> dataPlaneLocators = new ArrayList<>();
+        org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.function.base.SfDataPlaneLocatorBuilder macLocatorTypeBuilder =
+                new org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.function.base.SfDataPlaneLocatorBuilder();
+        macLocatorTypeBuilder.setLocatorType(new MacBuilder().build());
+        dataPlaneLocators.add(macLocatorTypeBuilder.build());
+        SfDataPlaneLocator result = SfcIosXeUtils.getDplWithIpLocatorType(dataPlaneLocators);
+        assertNull(result);
+    }
+
+    @Test
+    public void getDplWithIpLocatorType() {
+        List<SfDataPlaneLocator> dataPlaneLocators = new ArrayList<>();
+        org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.function.base.SfDataPlaneLocatorBuilder macLocatorTypeBuilder =
+                new org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.function.base.SfDataPlaneLocatorBuilder();
+        macLocatorTypeBuilder.setLocatorType(new MacBuilder().build());
+        SfDataPlaneLocator macLocatorDpl = macLocatorTypeBuilder.build();
+        org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.function.base.SfDataPlaneLocatorBuilder ipLocatorTypeBuilder =
+                new org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.function.base.SfDataPlaneLocatorBuilder();
+        ipLocatorTypeBuilder.setLocatorType(new IpBuilder().build());
+        SfDataPlaneLocator ipLocatorDpl = ipLocatorTypeBuilder.build();
+        dataPlaneLocators.add(macLocatorDpl);
+        dataPlaneLocators.add(ipLocatorDpl);
+        SfDataPlaneLocator result = SfcIosXeUtils.getDplWithIpLocatorType(dataPlaneLocators);
+        assertNotNull(result);
+    }
+
+    @Test
+    public void createLocalSffIid() {
+        InstanceIdentifier<Local> result = SfcIosXeUtils.createLocalSffIid();
+        // Test IID
+        InstanceIdentifier<Local> testIid = InstanceIdentifier.builder(Native.class).child(ServiceChain.class)
+                .child(org.opendaylight.yang.gen.v1.urn.ios.rev160308._native.service.chain.ServiceFunctionForwarder.class)
+                .child(Local.class).build();
+        assertEquals(testIid, result);
+    }
+
+    @Test
+    public void createRemoteSffIid() {
+        ServiceFfNameBuilder serviceFfNameBuilder = new ServiceFfNameBuilder();
+        serviceFfNameBuilder.setName(forwarderName);
+        InstanceIdentifier<ServiceFfName> firstResult = SfcIosXeUtils.createRemoteSffIid(serviceFfNameBuilder.build());
+        InstanceIdentifier<ServiceFfName> secondResult = SfcIosXeUtils.createRemoteSffIid(new SffName(forwarderName));
+        // Test IID
+        InstanceIdentifier<ServiceFfName> testIid = InstanceIdentifier.builder(Native.class).child(ServiceChain.class)
+                .child(org.opendaylight.yang.gen.v1.urn.ios.rev160308._native.service.chain.ServiceFunctionForwarder.class)
+                .child(ServiceFfName.class, new ServiceFfNameKey(forwarderName)).build();
+        assertEquals(firstResult, testIid);
+        assertEquals(secondResult, testIid);
+        assertEquals(firstResult, secondResult);
+    }
+
+    @Test
+    public void createSfIid() {
+        String functionName = "function";
+        InstanceIdentifier<ServiceFunction> result = SfcIosXeUtils.createSfIid(new ServiceFunctionKey(functionName));
+        // Test IID
+        InstanceIdentifier<ServiceFunction> testIid = InstanceIdentifier.builder(Native.class)
+                .child(ServiceChain.class)
+                .child(ServiceFunction.class, new ServiceFunctionKey(functionName))
+                .build();
+        assertEquals(testIid, result);
+    }
+
+    @Test
+    public void createServicePathIid() {
+        Long servicePathKey = 10L;
+        InstanceIdentifier<ServicePath> result = SfcIosXeUtils.createServicePathIid(new ServicePathKey(servicePathKey));
+        // Test IID
+        InstanceIdentifier<ServicePath> testIid = InstanceIdentifier.builder(Native.class).child(ServiceChain.class)
+                .child(ServicePath.class, new ServicePathKey(servicePathKey)).build();
+        assertEquals(testIid, result);
+    }
+}
---
+++ sfc-renderers/sfc-ios-xe-renderer/src/test/java/org/opendaylight/sfc/sfc_ios_xe/provider/renderer/IosXeServiceFunctionMapperTest.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.sfc.sfc_ios_xe.provider.renderer;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.common.rev151017.SfName;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.function.base.SfDataPlaneLocator;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.function.base.SfDataPlaneLocatorBuilder;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.functions.ServiceFunction;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.functions.ServiceFunctionBuilder;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.functions.ServiceFunctionKey;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sl.rev140701.Gre;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sl.rev140701.data.plane.locator.locator.type.IpBuilder;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sl.rev140701.data.plane.locator.locator.type.MacBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Host;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Address;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.NetconfNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.NetconfNodeBuilder;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeBuilder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class IosXeServiceFunctionMapperTest {
+
+    private final String nodeIdString = "nodeId";
+    private final String ipAddress = "10.0.0.1";
+    private DataBroker dataBroker;
+    private NodeManager nodeManager;
+    private IosXeServiceFunctionMapper sfMapper;
+
+    @Before
+    public void init() {
+        dataBroker = mock(DataBroker.class);
+        nodeManager = mock(NodeManager.class);
+    }
+
+    @Test
+    public void syncFunctions_update() {
+        String sfName1 = "function1";
+        String sfName2 = "function2";
+        // Prepare DPL
+        SfDataPlaneLocatorBuilder macLocatorType = new SfDataPlaneLocatorBuilder();
+        macLocatorType.setLocatorType(new MacBuilder().build());
+        SfDataPlaneLocatorBuilder ipLocatorType = new SfDataPlaneLocatorBuilder();
+        ipLocatorType.setLocatorType(new IpBuilder().setIp(new IpAddress(new Ipv4Address(ipAddress))).build())
+                .setTransport(Gre.class);
+        SfDataPlaneLocator macDpl = macLocatorType.build();
+        SfDataPlaneLocator ipDpl = ipLocatorType.build();
+        List<SfDataPlaneLocator> dataPlaneLocatorList;
+        // Service functions
+        List<ServiceFunction> serviceFunctions = new ArrayList<>();
+        // SF without management IP
+        ServiceFunctionBuilder emptySfBuilder = new ServiceFunctionBuilder();
+        emptySfBuilder.setName(new SfName(sfName1))
+                .setKey(new ServiceFunctionKey(new SfName(sfName1)));
+        // SF without data plane locator
+        ServiceFunctionBuilder noDplSfBuilder = new ServiceFunctionBuilder();
+        noDplSfBuilder.setName(new SfName(sfName2))
+                .setKey(new ServiceFunctionKey(new SfName(sfName2)))
+                .setIpMgmtAddress(new IpAddress(new Ipv4Address(ipAddress)));
+        // SF without ip data plane locator
+        ServiceFunctionBuilder noIpDplSfBuilder = new ServiceFunctionBuilder();
+        dataPlaneLocatorList = new ArrayList<>();
+        dataPlaneLocatorList.add(macDpl);
+        noIpDplSfBuilder.setName(new SfName(sfName2))
+                .setKey(new ServiceFunctionKey(new SfName(sfName2)))
+                .setIpMgmtAddress(new IpAddress(new Ipv4Address(ipAddress)))
+                .setSfDataPlaneLocator(dataPlaneLocatorList);
+        // Test SF
+        ServiceFunctionBuilder testSfBuilder = new ServiceFunctionBuilder();
+        dataPlaneLocatorList = new ArrayList<>();
+        dataPlaneLocatorList.add(ipDpl);
+        testSfBuilder.setName(new SfName(sfName2))
+                .setKey(new ServiceFunctionKey(new SfName(sfName2)))
+                .setIpMgmtAddress(new IpAddress(new Ipv4Address(ipAddress)))
+                .setSfDataPlaneLocator(dataPlaneLocatorList);
+        serviceFunctions.add(emptySfBuilder.build());
+        serviceFunctions.add(noDplSfBuilder.build());
+        serviceFunctions.add(noIpDplSfBuilder.build());
+        serviceFunctions.add(testSfBuilder.build());
+        // Node list
+        Map<NodeId, Node> nodeMap = new HashMap<>();
+        NodeId nodeId = new NodeId(nodeIdString);
+        NodeBuilder nodeBuilder = new NodeBuilder();
+        NetconfNodeBuilder netconfNodeBuilder = new NetconfNodeBuilder();
+        netconfNodeBuilder.setHost(new Host(new IpAddress(new Ipv4Address(ipAddress))));
+        nodeBuilder.setNodeId(nodeId)
+                .addAugmentation(NetconfNode.class, netconfNodeBuilder.build());
+        Node node = nodeBuilder.build();
+        nodeMap.put(nodeId, node);
+
+        Map<NodeId, DataBroker> nodeWithDataBrokerMap = new HashMap<>();
+        nodeWithDataBrokerMap.put(nodeId, dataBroker);
+
+        when(nodeManager.getConnectedNodes()).thenReturn(nodeMap);
+        when(nodeManager.getNetconfNodeIp(node)).thenReturn(new IpAddress(new Ipv4Address(ipAddress)));
+        when(nodeManager.getActiveMountPoints()).thenReturn(nodeWithDataBrokerMap);
+
+        sfMapper = new IosXeServiceFunctionMapper(dataBroker, nodeManager);
+        sfMapper.syncFunctions(serviceFunctions, false);
+
+        verify(nodeManager, times(3)).getConnectedNodes();
+        verify(nodeManager, times(3)).getNetconfNodeIp(any(Node.class));
+        verify(nodeManager, times(3)).getActiveMountPoints();
+        verify(dataBroker, times(1)).newWriteOnlyTransaction();
+    }
+
+    @Test
+    public void syncFunctions_delete() {
+        String sfName2 = "function2";
+        sfMapper = new IosXeServiceFunctionMapper(dataBroker, nodeManager);
+        // Prepare DPL
+        SfDataPlaneLocatorBuilder macLocatorType = new SfDataPlaneLocatorBuilder();
+        macLocatorType.setLocatorType(new MacBuilder().build());
+        SfDataPlaneLocatorBuilder ipLocatorType = new SfDataPlaneLocatorBuilder();
+        ipLocatorType.setLocatorType(new IpBuilder().setIp(new IpAddress(new Ipv4Address(ipAddress))).build())
+                .setTransport(Gre.class);
+        SfDataPlaneLocator ipDpl = ipLocatorType.build();
+        List<SfDataPlaneLocator> dataPlaneLocatorList;
+        // Service function
+        List<ServiceFunction> serviceFunctions = new ArrayList<>();
+        ServiceFunctionBuilder testSfBuilder = new ServiceFunctionBuilder();
+        dataPlaneLocatorList = new ArrayList<>();
+        dataPlaneLocatorList.add(ipDpl);
+        testSfBuilder.setName(new SfName(sfName2))
+                .setKey(new ServiceFunctionKey(new SfName(sfName2)))
+                .setIpMgmtAddress(new IpAddress(new Ipv4Address(ipAddress)))
+                .setSfDataPlaneLocator(dataPlaneLocatorList);
+        serviceFunctions.add(testSfBuilder.build());
+        // Node list
+        Map<NodeId, Node> nodeMap = new HashMap<>();
+        NodeId nodeId = new NodeId(nodeIdString);
+        NodeBuilder nodeBuilder = new NodeBuilder();
+        NetconfNodeBuilder netconfNodeBuilder = new NetconfNodeBuilder();
+        netconfNodeBuilder.setHost(new Host(new IpAddress(new Ipv4Address(ipAddress))));
+        nodeBuilder.setNodeId(nodeId)
+                .addAugmentation(NetconfNode.class, netconfNodeBuilder.build());
+        Node node = nodeBuilder.build();
+        nodeMap.put(nodeId, node);
+
+        Map<NodeId, DataBroker> nodeWithDataBrokerMap = new HashMap<>();
+        nodeWithDataBrokerMap.put(nodeId, dataBroker);
+
+        when(nodeManager.getConnectedNodes()).thenReturn(nodeMap);
+        when(nodeManager.getNetconfNodeIp(node)).thenReturn(new IpAddress(new Ipv4Address(ipAddress)));
+        when(nodeManager.getActiveMountPoints()).thenReturn(nodeWithDataBrokerMap);
+
+        sfMapper.syncFunctions(serviceFunctions, true);
+
+        verify(nodeManager, times(1)).getConnectedNodes();
+        verify(nodeManager, times(1)).getNetconfNodeIp(any(Node.class));
+        verify(nodeManager, times(1)).getActiveMountPoints();
+        verify(dataBroker, times(1)).newWriteOnlyTransaction();
+    }
+}

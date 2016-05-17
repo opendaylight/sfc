fd39bcc381e7e6950c74f09267917c1aa8907598
Jose-Santos Pulido Garcia <jose.santos.pulido.garcia@ericsson.com>
2016-05-17 20:31:39 +0200

    Rename sfcofl2 to sfc-openflow-renderer
    Directory sfcofl2 is renamed to sfc-renderers/sfc-openflow-renderer.
    Everything in sfcofl2 is renamed accordingly.
    
    Feature odl-sfcofl2 is marked as deprecated. It depends on the new
    odl-sfc-openflow-renderer feature.
    
    = PATCH INFO = (to be removed in last patch)
    
     - Patch 1: Rename package o.o.sfc.l2renderer to ofrenderer.
    
     - Patch 2: rename sfcofl2-provider-impl.yang to
       sfc-ofrenderer-provider-impl.yang
    
     - Patch 3: rename classes SfcL2RspProcessor, SfcL2Renderer, and
       SfcL2ProviderUtils.
    
     - Patch 4: rename data listeners.
    
     - Patch 5: rename flow writer and flow programmer.
    
    Change-Id: I1fd74a04629dd31df1348b794479413d11d5f80f
    Signed-off-by: Jose-Santos Pulido Garcia <jose.santos.pulido.garcia@ericsson.com>

---
+++ sfcofl2/src/main/java/org/opendaylight/sfc/ofrenderer/SfcOfAbstractDataListener.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2014 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.sfc.ofrenderer;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public abstract class SfcOfAbstractDataListener implements DataChangeListener {
+
+    private DataBroker dataBroker;
+    private InstanceIdentifier<?> iID;
+    private ListenerRegistration<DataChangeListener> dataChangeListenerRegistration;
+
+    public void setDataBroker(DataBroker dataBroker) {
+        this.dataBroker = dataBroker;
+    }
+
+    public void setIID(InstanceIdentifier<?> iID) {
+        this.iID = iID;
+    }
+
+    public DataBroker getDataBroker() {
+        return dataBroker;
+    }
+
+    public void registerAsDataChangeListener(LogicalDatastoreType datastoreType) {
+        dataChangeListenerRegistration = dataBroker.registerDataChangeListener(datastoreType, iID,
+                this, DataBroker.DataChangeScope.SUBTREE);
+    }
+
+    public void registerAsDataChangeListener() {
+        registerAsDataChangeListener(LogicalDatastoreType.CONFIGURATION);
+    }
+
+    public void closeDataChangeListener() {
+        dataChangeListenerRegistration.close();
+    }
+
+}

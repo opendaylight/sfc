554b04c74a1e38d3f32cb9b2269952447b6f4006
Vladimir Lavor <vlavor@cisco.com>
2016-06-20 08:38:30 +0200

    sfc ios-xe renderer is now using the same ned.yang file as gbp
    Change-Id: Icda2e90aa729665e307ee59e1bdbac251f6e64d3
    Signed-off-by: Vladimir Lavor <vlavor@cisco.com>

---
+++ sfc-renderers/sfc-ios-xe-renderer/src/main/java/org/opendaylight/yang/gen/v1/urn/ios/rev160308/_native/mls/qos/MapPolicedDscpBuilder.java
@@ -0,0 +1,20 @@
+package org.opendaylight.yang.gen.v1.urn.ios.rev160308._native.mls.qos;
+import org.opendaylight.yang.gen.v1.urn.ios.rev160308._native.mls.qos.Map.PolicedDscp;
+
+
+/**
+ * The purpose of generated class in src/main/java for Union types is to create new instances of unions from a string representation.
+ * In some cases it is very difficult to automate it since there can be unions such as (uint32 - uint16), or (string - uint32).
+ *
+ * The reason behind putting it under src/main/java is:
+ * This class is generated in form of a stub and needs to be finished by the user. This class is generated only once to prevent
+ * loss of user code.
+ *
+ */
+public class MapPolicedDscpBuilder {
+
+    public static PolicedDscp getDefaultInstance(java.lang.String defaultValue) {
+        throw new java.lang.UnsupportedOperationException("Not yet implemented");
+    }
+
+}

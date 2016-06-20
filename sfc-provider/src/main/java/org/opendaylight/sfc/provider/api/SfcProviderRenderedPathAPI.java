11c1d2ef6768c2061cc1580eebf0cb7b698ecb69
Vladimir Lavor <vlavor@cisco.com>
2016-06-20 09:08:44 +0200

    Create reversed rsp name in separate method
    Change-Id: I1570d10a0761d030a800dda719208a985d6e4aff
    Signed-off-by: Vladimir Lavor <vlavor@cisco.com>

--- sfc-provider/src/main/java/org/opendaylight/sfc/provider/api/SfcProviderRenderedPathAPI.java
+++ sfc-provider/src/main/java/org/opendaylight/sfc/provider/api/SfcProviderRenderedPathAPI.java
@@ -76,6 +76,7 @@ public class SfcProviderRenderedPathAPI {
     private static final Logger LOG = LoggerFactory.getLogger(SfcProviderRenderedPathAPI.class);
     private static final int MAX_STARTING_INDEX = 255;
     private static SfcServiceFunctionSchedulerAPI defaultScheduler;
+    private static final String REVERSED_PATH_SUFFIX = "-Reverse";
 
     private static SfcServiceFunctionSchedulerAPI getServiceFunctionScheduler(
             Class<? extends ServiceFunctionSchedulerTypeIdentity> serviceFunctionSchedulerType) {
@@ -531,8 +532,7 @@ public class SfcProviderRenderedPathAPI {
 
         RenderedServicePathBuilder revRenderedServicePathBuilder = new RenderedServicePathBuilder(renderedServicePath);
         revRenderedServicePathBuilder.setPathId(pathId);
-        // TODO Move Reverse RSP Name generation to method, put String "-Reverse" to constant.
-        revPathName = new RspName(renderedServicePath.getName().getValue() + "-Reverse");
+        revPathName = generateReversedPathName(renderedServicePath.getName());
         revRenderedServicePathBuilder.setName(revPathName);
         RenderedServicePathKey revRenderedServicePathKey = new RenderedServicePathKey(revPathName);
         revRenderedServicePathBuilder.setKey(revRenderedServicePathKey);
@@ -596,6 +596,10 @@ public class SfcProviderRenderedPathAPI {
         printTraceStop(LOG);
         return ret;
 
+    }
+
+    public static RspName generateReversedPathName(RspName directRspName) {
+        return new RspName(directRspName.getValue() + REVERSED_PATH_SUFFIX);
     }
 
     /**

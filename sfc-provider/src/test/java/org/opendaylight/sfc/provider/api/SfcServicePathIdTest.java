0f136416ee00110144886b176de278aec5f9d34b
Vladimir Lavor <vlavor@cisco.com>
2016-06-28 11:36:34 +0200

    Fix unit tests in sfc-provider - SfcServicePathId
    Change-Id: Ibd5cd91a2d0e03282ce7a3c9ba2171b2f425ac11
    Signed-off-by: Vladimir Lavor <vlavor@cisco.com>

--- sfc-provider/src/test/java/org/opendaylight/sfc/provider/api/SfcServicePathIdTest.java
+++ sfc-provider/src/test/java/org/opendaylight/sfc/provider/api/SfcServicePathIdTest.java
@@ -9,6 +9,12 @@
 
 package org.opendaylight.sfc.provider.api;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.service.path.id.rev150804.GenerationAlgorithmEnum;
 
@@ -16,32 +22,31 @@ import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 
-import static org.junit.Assert.*;
-
 /**
  * SfcServicePathId Tester.
  *
  * @author Reinaldo Penno (rapenno@gmail.com)
- * @since       2015-09-01
  * @version 1.0
+ * @since 2015-09-01
  */
 
-public class SfcServicePathIdTest  extends AbstractSfcRendererServicePathAPITest {
+public class SfcServicePathIdTest extends AbstractSfcRendererServicePathAPITest {
 
-
-    LinkedHashSet<Integer> pathIdSet = new LinkedHashSet<Integer>();
-    LinkedList<Integer> pathIdList = new LinkedList<Integer>(Arrays.asList(0, 1, 63, 64, 65, 1023, 1024, 1025));
-    LinkedList<Integer> FlippedPathIdList = new LinkedList<Integer>(
+    private final LinkedHashSet<Integer> pathIdSet = new LinkedHashSet<>();
+    private final LinkedList<Integer> pathIdList = new LinkedList<>(Arrays.asList(0, 1, 63, 64, 65, 1023, 1024, 1025));
+    private final LinkedList<Integer> FlippedPathIdList = new LinkedList<>(
             Arrays.asList(8388608, 8388608, 8388671, 8388672, 8388673, 8389631, 8389632, 8389633));
 
+    @Before
+    public void init() {
+        super.init();
+    }
+
     /**
-     *
      * Method: generatePathIdRandomIncrements()
-     *
      */
     @Test
     public void testGeneratePathIdRandomIncrements() throws Exception {
-
         int i;
         for (i = 0; i < 20; i++) {
             assertTrue(pathIdSet.add(SfcServicePathId.generatePathIdRandomIncrements()));
@@ -50,12 +55,10 @@ public class SfcServicePathIdTest  extends AbstractSfcRendererServicePathAPITest
     }
 
     /**
-     *
-     * Method: check_and_allocate_pathid(long pathid)
-     *
+     * Method: check_and_allocate_pathId(long pathId)
      */
     @Test
-    public void testCheck_and_allocate_pathidPathid() throws Exception {
+    public void testCheck_and_allocate_pathIdPathId() throws Exception {
         for (Integer pathId : pathIdList) {
             assertNotEquals(-1, SfcServicePathId.check_and_allocate_pathid(pathId));
         }
@@ -65,12 +68,10 @@ public class SfcServicePathIdTest  extends AbstractSfcRendererServicePathAPITest
     }
 
     /**
-     *
-     * Method: check_and_allocate_symmetric_pathid(long pathid)
-     *
+     * Method: check_and_allocate_symmetric_pathId(long pathId)
      */
     @Test
-    public void testCheck_and_allocate_symmetric_pathid() throws Exception {
+    public void testCheck_and_allocate_symmetric_pathId() throws Exception {
         for (Integer pathId : pathIdList) {
             assertNotEquals(-1, SfcServicePathId.check_and_allocate_symmetric_pathid(pathId));
         }
@@ -83,12 +84,10 @@ public class SfcServicePathIdTest  extends AbstractSfcRendererServicePathAPITest
     }
 
     /**
-     *
-     * Method: check_and_allocate_pathid()
-     *
+     * Method: check_and_allocate_pathId()
      */
     @Test
-    public void testCheck_and_allocate_pathid() throws Exception {
+    public void testCheck_and_allocate_pathId() throws Exception {
         int i;
         for (i = 0; i < 20; i++) {
             int pathId;
@@ -107,12 +106,10 @@ public class SfcServicePathIdTest  extends AbstractSfcRendererServicePathAPITest
     }
 
     /**
-     *
-     * Method: allocate_pathid(long pathid)
-     *
+     * Method: allocate_pathId(long pathId)
      */
     @Test
-    public void testAllocate_pathid() throws Exception {
+    public void testAllocate_pathId() throws Exception {
         for (Integer pathId : pathIdList) {
             assertTrue(SfcServicePathId.allocate_pathid(pathId));
         }
@@ -125,12 +122,10 @@ public class SfcServicePathIdTest  extends AbstractSfcRendererServicePathAPITest
     }
 
     /**
-     *
-     * Method: check_suitable_pathid(long pathid)
-     *
+     * Method: check_suitable_pathId(long pathId)
      */
     @Test
-    public void testCheck_suitable_pathid() throws Exception {
+    public void testCheck_suitable_pathId() throws Exception {
         for (Integer pathId : pathIdList) {
             assertTrue(SfcServicePathId.check_suitable_pathid(pathId));
         }
@@ -149,12 +144,10 @@ public class SfcServicePathIdTest  extends AbstractSfcRendererServicePathAPITest
     }
 
     /**
-     *
-     * Method: free_pathid(long pathid)
-     *
+     * Method: free_pathId(long pathId)
      */
     @Test
-    public void testFree_pathid() throws Exception {
+    public void testFree_pathId() throws Exception {
         for (Integer pathId : pathIdList) {
             assertTrue(SfcServicePathId.allocate_pathid(pathId));
         }
@@ -167,47 +160,43 @@ public class SfcServicePathIdTest  extends AbstractSfcRendererServicePathAPITest
     }
 
     /**
-    *
-    * Method: setGenerationAlgorithm(), check_and_allocate_pathid()
-    *
-    */
-   @Test
-   public void testSequentialGenerationAlgorithm() throws Exception {
-       SfcServicePathId.setGenerationAlgorithm(GenerationAlgorithmEnum.Sequential);
-       long firstPathId = SfcServicePathId.check_and_allocate_pathid();
-       int numPathIds = 20;
+     * Method: setGenerationAlgorithm(), check_and_allocate_pathId()
+     */
+    @Test
+    public void testSequentialGenerationAlgorithm() throws Exception {
+        SfcServicePathId.setGenerationAlgorithm(GenerationAlgorithmEnum.Sequential);
+        long firstPathId = SfcServicePathId.check_and_allocate_pathid();
+        int numPathIds = 20;
 
-       // Verify the pathIds are created sequentially
-       for (long pathId = firstPathId+1; pathId < firstPathId+numPathIds; ++pathId) {
-           assertEquals(SfcServicePathId.check_and_allocate_pathid(), pathId);
-       }
-       // Free the previously allocated pathIds
-       for (long pathId = firstPathId; pathId < firstPathId+numPathIds; ++pathId) {
-           assertTrue(SfcServicePathId.free_pathid(pathId));
-       }
-   }
+        // Verify the pathIds are created sequentially
+        for (long pathId = firstPathId + 1; pathId < firstPathId + numPathIds; ++pathId) {
+            assertEquals(SfcServicePathId.check_and_allocate_pathid(), pathId);
+        }
+        // Free the previously allocated pathIds
+        for (long pathId = firstPathId; pathId < firstPathId + numPathIds; ++pathId) {
+            assertTrue(SfcServicePathId.free_pathid(pathId));
+        }
+    }
 
-   /**
-   *
-   * Method: setGenerationAlgorithm(), check_and_allocate_pathid(), check_and_allocate_symmetric_pathid()
-   *
-   */
-  @Test
-  public void testSequentialGenerationAlgorithmSymmetric() throws Exception {
-      SfcServicePathId.setGenerationAlgorithm(GenerationAlgorithmEnum.Sequential);
-      long firstPathId = SfcServicePathId.check_and_allocate_pathid();
-      int numPathIds = 40;
+    /**
+     * Method: setGenerationAlgorithm(), check_and_allocate_pathId(), check_and_allocate_symmetric_pathId()
+     */
+    @Test
+    public void testSequentialGenerationAlgorithmSymmetric() throws Exception {
+        SfcServicePathId.setGenerationAlgorithm(GenerationAlgorithmEnum.Sequential);
+        long firstPathId = SfcServicePathId.check_and_allocate_pathid();
+        int numPathIds = 40;
 
-      // Verify the pathIds are created sequentially both for the normal and the symmetric cases
-      for (long pathId = firstPathId+1; pathId < firstPathId+numPathIds; pathId+=2) {
-          long id = SfcServicePathId.check_and_allocate_pathid();
-          assertEquals(id, pathId);
-          assertEquals(SfcServicePathId.check_and_allocate_symmetric_pathid(id), pathId+1);
-      }
-      // Free the previously allocated pathIds
-      for (long pathId = firstPathId; pathId < firstPathId+numPathIds+1; ++pathId) {
-          assertTrue(SfcServicePathId.free_pathid(pathId));
-      }
-  }
+        // Verify the pathIds are created sequentially both for the normal and the symmetric cases
+        for (long pathId = firstPathId + 1; pathId < firstPathId + numPathIds; pathId += 2) {
+            long id = SfcServicePathId.check_and_allocate_pathid();
+            assertEquals(id, pathId);
+            assertEquals(SfcServicePathId.check_and_allocate_symmetric_pathid(id), pathId + 1);
+        }
+        // Free the previously allocated pathIds
+        for (long pathId = firstPathId; pathId < firstPathId + numPathIds + 1; ++pathId) {
+            assertTrue(SfcServicePathId.free_pathid(pathId));
+        }
+    }
 
 }

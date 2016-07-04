34ff5c0cef5d13340df93800507fe28e6d9f1225
Guo Ruijing <ruijing.guo@intel.com>
2016-07-04 16:33:54 +0800

    fix sfc103 demo
    1. refacotor sfc103
    2. work around sff openflow table 0 missing (will be removed)
    
    Change-Id: I9b281c1afb989d8f5dc8fc96993027faa0bfd8ce
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- sfc-demo/sfc103/setup_classifier.sh
+++ sfc-demo/sfc103/setup_classifier.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-apt-get update -y
-apt-get install autoconf -y
-apt-get install libtool -y
-apt-get install git -y
-apt-get install python3-flask requests netifaces -y
-apt-get install libssl-dev openssl -y
-apt-get install libnetfilter-queue-dev -y
-apt-get install python3-pip -y
-pip3 install sfc
-apt-get install git curl -y
-apt-get install mini-httpd -y
+set -eux
 
-rmmod openvswitch
-find /lib/modules | grep openvswitch.ko | xargs rm -rf
+apt-get update -y
+apt-get install git -y
 curl https://raw.githubusercontent.com/priteshk/ovs/nsh-v8/third-party/start-ovs-deb.sh | bash
 
 ovs-vsctl set-manager tcp:192.168.1.5:6640
@@ -39,6 +29,6 @@ else
     ip netns exec app ip link set dev veth-app up
     ip netns exec app ip link set dev lo up
     ip netns exec app ifconfig veth-app mtu 1400
-    ip netns exec app python -m SimpleHTTPServer 80
+    nohup ip netns exec app python -m SimpleHTTPServer 80 > /home/vagrant/http_server.log 2>&1  &
 fi
 ovs-vsctl show
--- sfc-demo/sfc103/setup_odl.sh
+++ sfc-demo/sfc103/setup_odl.sh
@@ -1,34 +1,64 @@
 #!/bin/bash
 
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-sudo add-apt-repository ppa:webupd8team/java -y
-sudo apt-get update -y
-sudo apt-get install oracle-java8-installer -y
-sudo update-java-alternatives -s java-8-oracle
-sudo apt-get install oracle-java8-set-default -y
-sudo apt-get install  mininet -y
-sudo mkdir -p /usr/local/apache-maven
-sudo wget http://ftp.wayne.edu/apache/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz
-sudo mv apache-maven-3.3.3-bin.tar.gz /usr/local/apache-maven
-sudo tar -xzvf /usr/local/apache-maven/apache-maven-3.3.3-bin.tar.gz -C /usr/local/apache-maven/
-sudo update-alternatives --install /usr/bin/mvn mvn /usr/local/apache-maven/apache-maven-3.3.3/bin/mvn 1
-sudo update-alternatives --config mvn
-sudo apt-get install npm vim git git-review diffstat -y
+# setup sfc from pre-build. If DIST_URL is null, build sfc from scratch
+#DIST_URL=https://nexus.opendaylight.org/content/repositories/opendaylight.snapshot/org/opendaylight/integration/distribution-karaf/0.5.0-SNAPSHOT/
 
 
-cat << EOF >> /home/vagrant/.bashrc
-export M2_HOME=/usr/local/apache-maven/apache-maven-3.3.3
+function install_packages {
+    #install java8
+    echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+    sudo add-apt-repository ppa:webupd8team/java -y
+    sudo apt-get update -y
+    sudo apt-get install oracle-java8-installer -y
+    sudo update-java-alternatives -s java-8-oracle
+    sudo apt-get install oracle-java8-set-default -y
+
+    #install maven
+    sudo mkdir -p /usr/local/apache-maven; cd /usr/local/apache-maven
+    curl https://www.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz | sudo tar -xzv
+    sudo update-alternatives --install /usr/bin/mvn mvn /usr/local/apache-maven/apache-maven-3.3.9/bin/mvn 1
+    sudo update-alternatives --config mvn
+
+    cat << EOF > $HOME/maven.env
+export M2_HOME=/usr/local/apache-maven/apache-maven-3.3.9
 export MAVEN_OPTS="-Xms256m -Xmx512m" # Very important to put the "m" on the end
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle # This matches sudo update-alternatives --config java
 EOF
+    sudo apt-get install npm vim git git-review diffstat -y
+}
 
-source /home/vagrant/.bashrc
-mkdir /home/vagrant/.m2
-wget -O  - https://raw.githubusercontent.com/opendaylight/odlparent/master/settings.xml > /home/vagrant/.m2/settings.xml
+function install_sfc {
+    if [[ -n $DIST_URL ]]; then
+        curl $DIST_URL/maven-metadata.xml | grep -A2 tar.gz | grep value | cut -f2 -d'>' | cut -f1 -d'<' | \
+            xargs -I {} curl $DIST_URL/distribution-karaf-{}.tar.gz | tar xvz-
+        rm -rf $HOME/sfc; mkdir -p $HOME/sfc/sfc-karaf/target
+        mv distribution-karaf* $HOME/sfc/sfc-karaf/target/assembly
+    else
+        source $HOME/maven.env
+        mkdir $HOME/.m2
+        wget -O  - https://raw.githubusercontent.com/opendaylight/odlparent/master/settings.xml > $HOME/.m2/settings.xml
+        rm -rf $HOME/sfc; cp -r /sfc $HOME;
+        cd $HOME/sfc;
+        #fix me: remove the workaroud
+        patch -p1 < /vagrant/sfc_openflow_workaround.diff
+        mvn clean install -nsu -DskipTests
+        #try again to work around build failure due to network issue
+        mvn clean install -nsu -DskipTests
+    fi
+}
 
-rm -rf /home/vagrant/sfc; cp -r /sfc /home/vagrant
-cd /home/vagrant/sfc; mvn clean install -nsu -DskipTests;
-cd /home/vagrant/sfc/sfc-karaf/target/assembly;
-sed -i "/^featuresBoot =/ s/$/,odl-sfc-provider,odl-sfc-core,odl-sfc-ui,odl-sfc-openflow-renderer,odl-sfc-scf-openflow,odl-sfc-sb-rest,odl-sfc-ovs,odl-sfc-netconf/" etc/org.apache.karaf.features.cfg;
-echo "log4j.logger.org.opendaylight.sfc = DEBUG,stdout" >> etc/org.ops4j.pax.logging.cfg;
-rm -rf journal snapshots; bin/karaf clean
+function start_sfc {
+    cd $HOME/sfc/sfc-karaf/target/assembly/
+    sed -i "/^featuresBoot[ ]*=/ s/$/,odl-sfc-provider,odl-sfc-core,odl-sfc-ui,odl-sfc-openflow-renderer,odl-sfc-scf-openflow,odl-sfc-sb-rest,odl-sfc-ovs,odl-sfc-netconf/" etc/org.apache.karaf.features.cfg;
+    echo "log4j.logger.org.opendaylight.sfc.ofrenderer.openflow = DEBUG,stdout" >> etc/org.ops4j.pax.logging.cfg;
+    rm -rf journal snapshots; bin/karaf clean
+}
+
+echo "SFC DEMO: Packages installation" > $HOME/sfc.prog
+install_packages
+
+echo "SFC DEMO: SFC installation" > $HOME/sfc.prog
+install_sfc
+
+echo "SFC DEMO: Launch SFC" > $HOME/sfc.prog
+start_sfc

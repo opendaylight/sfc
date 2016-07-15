d61cf409e02f6d278d78f964bb5ba9dfba064a87
Jaime Caamaño Ruiz <jaime.caamano.ruiz@ericsson.com>
2016-07-15 13:02:28 +0200

    Add new module sfc-genius
    This commit adds the skeleton of new module sfc-genius.
    Sfc will start using odl genius project as a generic
    network service provider and this module will contain
    several specific items related to this handling.
    
    Among other things, it will contain:
    * Listeners for SFC configuration events that require
      any Genius related action.
    * Listeners for Genius operational events that require
      any SFC related action.
    * Genius related utilities
    
    Change-Id: Ia26269d7765f17a5836c3054eef9e6912e9ebc0c
    Signed-off-by: Jaime Caamaño Ruiz <jaime.caamano.ruiz@ericsson.com>

---
+++ sfc-genius/src/main/java/sfc/sfc_genius/provider/listeners/SfcGeniusSfStateListener.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2016 Ericsson Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package sfc.sfc_genius.provider.listeners;
+
+import org.opendaylight.genius.datastoreutils.AsyncDataTreeChangeListenerBase;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.ServiceFunctionsState;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.functions.state.ServiceFunctionState;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sfc.sfc_genius.provider.SfcGeniusSfInterfaceManager;
+
+
+public class SfcGeniusSfStateListener extends AsyncDataTreeChangeListenerBase<ServiceFunctionState,
+        SfcGeniusSfStateListener> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SfcGeniusSfStateListener.class);
+    private final SfcGeniusSfInterfaceManager interfaceManager;
+
+    public SfcGeniusSfStateListener(SfcGeniusSfInterfaceManager interfaceManager) {
+        super(ServiceFunctionState.class, SfcGeniusSfStateListener.class);
+        this.interfaceManager = interfaceManager;
+    }
+
+    @Override
+    protected InstanceIdentifier<ServiceFunctionState> getWildCardPath() {
+        return InstanceIdentifier.create(ServiceFunctionsState.class).child(ServiceFunctionState.class);
+    }
+
+    @Override
+    protected void remove(InstanceIdentifier<ServiceFunctionState> instanceIdentifier,
+                          ServiceFunctionState serviceFunction) {
+        // TODO implement
+        // If this SF stops participating in RSPs, we need to unbind from its interfaces
+    }
+
+    @Override
+    protected void update(InstanceIdentifier<ServiceFunctionState> instanceIdentifier,
+                          ServiceFunctionState serviceFunction, ServiceFunctionState t1) {
+        // TODO implement
+        // If this SF stops participating in RSPs, we need to unbind from its interfaces
+    }
+
+    @Override
+    protected void add(InstanceIdentifier<ServiceFunctionState> instanceIdentifier,
+                       ServiceFunctionState serviceFunction) {
+        // TODO implement
+        // If this SF starts participating in RSPs, we need to bind to its interfaces
+    }
+
+    @Override
+    protected SfcGeniusSfStateListener getDataTreeChangeListener() {
+        return SfcGeniusSfStateListener.this;
+    }
+}

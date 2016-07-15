3e37335ece3a48dea0aa73e49801b322a87de483
Jaime Caamaño Ruiz <jaime.caamano.ruiz@ericsson.com>
2016-07-15 13:02:28 +0200

    Add new module sfc-genius
    This commit adds the skeleton of new module sfc-genius.
    Sfc will start using odl genius project as a generic
    network service provider and this module will contain
    several specific items related to this handling.
    
    Among other things, it will contain:
    * Listeners for SFC configuration events that require
      any Genius related action.
    * Listeners for Genius operational events that require
      any SFC related action.
    * Genius related utilities
    
    Change-Id: Ia26269d7765f17a5836c3054eef9e6912e9ebc0c
    Signed-off-by: Jaime Caamaño Ruiz <jaime.caamano.ruiz@ericsson.com>

---
+++ sfc-genius/src/main/java/sfc/sfc_genius/provider/listeners/SfcGeniusInterfaceStateListener.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2016 Ericsson Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package sfc.sfc_genius.provider.listeners;
+
+import org.opendaylight.genius.datastoreutils.AsyncDataTreeChangeListenerBase;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.InterfacesState;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.state.Interface;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sfc.sfc_genius.provider.SfcGeniusSfInterfaceManager;
+
+
+public class SfcGeniusInterfaceStateListener extends AsyncDataTreeChangeListenerBase<Interface,
+        SfcGeniusInterfaceStateListener> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SfcGeniusSfStateListener.class);
+    private final SfcGeniusSfInterfaceManager interfaceManager;
+
+    public SfcGeniusInterfaceStateListener(SfcGeniusSfInterfaceManager interfaceManager) {
+        super(Interface.class, SfcGeniusInterfaceStateListener.class);
+        this.interfaceManager = interfaceManager;
+    }
+
+    @Override
+    protected InstanceIdentifier<Interface> getWildCardPath() {
+        return InstanceIdentifier.create(InterfacesState.class).child(Interface.class);
+    }
+
+    @Override
+    protected void remove(InstanceIdentifier<Interface> instanceIdentifier, Interface interfaceState) {
+        // TODO implement
+        // See {@link org.opendaylight.genius.interfacemanager.listeners.InterfaceInventoryStateListener#remove}
+        // VM migration: interface state is removed before being added again
+        // RSPs will be re-rendered when the interface state is added again
+        // As of now, we cannot delete RSPs at this time because we would not know how to recreate them at a later time
+        // Terminating service action might have to be removed from the associated node
+    }
+
+    @Override
+    protected void update(InstanceIdentifier<Interface> instanceIdentifier, Interface interfaceState, Interface t1) {
+        // TODO implement
+        // See {@link org.opendaylight.genius.interfacemanager.listeners.InterfaceInventoryStateListener#update}
+        // NOT VM migration: VM unavailable for any other reason
+        // Do nothing, should be handled by failover
+    }
+
+    @Override
+    protected void add(InstanceIdentifier<Interface> instanceIdentifier, Interface interfaceState) {
+        // TODO implement
+        // See {@link org.opendaylight.genius.interfacemanager.listeners.InterfaceInventoryStateListener#remove}
+        // VM migration: interface state is added once the VM has migrated
+        // RSPs associated with this interface should be re-rendered
+        // Terminating service action might have to be added to the associated node
+    }
+
+    @Override
+    protected SfcGeniusInterfaceStateListener getDataTreeChangeListener() {
+        return SfcGeniusInterfaceStateListener.this;
+    }
+}

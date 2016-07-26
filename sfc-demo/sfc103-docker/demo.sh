fdac8c30cb456b04b202cf38393c42281ac789cf
ericdia <ricardo.diaz.campos@ericsson.com>
2016-07-26 17:03:40 +0200

    Demo 103 Vagrant + Docker
    Change-Id: I46870c35af3afbd760a40a21c166eb7cc96c8c96
    Signed-off-by: ericdia <ricardo.diaz.campos@ericsson.com>

---
+++ sfc-demo/sfc103-docker/demo.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+inprog=1
+
+cd ${HOME}
+while [ $inprog -ne 0 ]
+do
+    inprog=`true &> /dev/null | sfc/sfc-demo/sfc103/sfc_ready.sh`
+    if [ $inprog -ne 0 ]; then
+        sleep 60
+    fi
+done
+
+cd /vagrant
+docker-compose up -d
+
+# wait for docker deployment just in case
+sleep 10
+
+cd ${HOME}
+sfc/sfc-demo/sfc103/setup_sfc.py
+
+#wait for openflow effective
+sleep 60
+
+docker exec -it classifier1 ovs-ofctl dump-flows -OOpenflow13 br-sfc
+docker exec -it classifier2 ovs-ofctl dump-flows -OOpenflow13 br-sfc
+docker exec -it sff1 ovs-ofctl dump-flows -OOpenflow13 br-sfc
+docker exec -it sff2 ovs-ofctl dump-flows -OOpenflow13 br-sfc
+docker exec -it classifier1 ip netns exec app wget http://192.168.2.2
+
+#wait for openflow effective
+sleep 60
+
+sfc/sfc-demo/sfc103/update_sfc.py
+docker exec -it classifier1 ip netns exec app wget http://192.168.2.2

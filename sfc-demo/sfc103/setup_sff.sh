02b029cc738aef9c4f62060eb88a7d647fe74929
Guo Ruijing <ruijing.guo@intel.com>
2016-08-07 20:45:53 -0400

    Update sfc103 to docker based version
    - Additionally the demo can be executed several times without vagrant vm destruction
    
    Change-Id: I13b5561c263b5fc76511adff42010bf51696f572
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>
    Signed-off-by: Ricardo Diaz Campos <ricardo.diaz.campos@ericsson.com>

--- sfc-demo/sfc103/setup_sff.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eux
-
-apt-get update
-apt-get install git -y
-curl https://raw.githubusercontent.com/priteshk/ovs/nsh-v8/third-party/start-ovs-deb.sh | bash
-ovs-vsctl set-manager tcp:192.168.1.5:6640
-ovs-vsctl add-br br-sfc
-ovs-vsctl show

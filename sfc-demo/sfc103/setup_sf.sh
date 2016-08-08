ea66dbe44b9a08e485aad1c8d8f7d0cd8c066b16
Guo Ruijing <ruijing.guo@intel.com>
2016-08-07 20:45:53 -0400

    DONOT MERGE: Update sfc103 to docker based version
    Change-Id: I13b5561c263b5fc76511adff42010bf51696f572
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- sfc-demo/sfc103/setup_sf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eux
-
-apt-get update
-apt-get install -y python3-pip
-rm -rf /home/vagrant/sfc; cp -r /sfc /home/vagrant
-cd /home/vagrant/sfc/sfc-py;
-pip3 install -r requirements.txt
-nohup python3.4 sfc/sfc_agent.py --rest --odl-ip-port 192.168.1.5:8181 &

9c209de2f845acb5e51dd92f06eaf612a14107b4
Rashmi Pujar <rpujar@inocybe.com>
2016-09-09 17:02:06 -0400

    Add BP wiring for sfc-sb-rest
    Change-Id: I5d55db2e18842e7f915b383a2c9976fca76e980e
    Signed-off-by: Rashmi Pujar <rpujar@inocybe.com>

--- sfc-sb-rest/src/main/java/org/opendaylight/controller/config/yang/config/sfc_sb_rest_provider/impl/SfcSbRestProviderModule.java
+++ sfc-sb-rest/src/main/java/org/opendaylight/controller/config/yang/config/sfc_sb_rest_provider/impl/SfcSbRestProviderModule.java
@@ -8,78 +8,29 @@
 package org.opendaylight.controller.config.yang.config.sfc_sb_rest_provider.impl;
 
 import java.util.concurrent.ExecutionException;
+
 import org.opendaylight.sfc.provider.OpendaylightSfc;
 import org.opendaylight.sfc.sbrest.provider.keepalive.SbRestKeepAliveSocket;
-import org.opendaylight.sfc.sbrest.provider.listener.SbRestAclEntryDataListener;
-import org.opendaylight.sfc.sbrest.provider.listener.SbRestRspEntryDataListener;
-import org.opendaylight.sfc.sbrest.provider.listener.SbRestScfEntryDataListener;
-import org.opendaylight.sfc.sbrest.provider.listener.SbRestSfEntryDataListener;
-import org.opendaylight.sfc.sbrest.provider.listener.SbRestSffEntryDataListener;
-import org.opendaylight.sfc.sbrest.provider.listener.SbRestSfgEntryDataListener;
-import org.opendaylight.sfc.sbrest.provider.listener.SbRestSfstEntryDataListener;
-import org.opendaylight.sfc.sbrest.provider.listener.SbRestSfstateEntryDataListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-public class SfcSbRestProviderModule extends AbstractSfcSbRestProviderModule {
+public class SfcSbRestProviderModule implements AutoCloseable{
 
     private static final Logger LOG = LoggerFactory.getLogger(SfcSbRestProviderModule.class);
+    private static OpendaylightSfc opendaylightSfc;
 
-    public SfcSbRestProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
-        super(identifier, dependencyResolver);
-    }
-
-    public SfcSbRestProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.controller.config.yang.config.sfc_sb_rest_provider.impl.SfcSbRestProviderModule oldModule, java.lang.AutoCloseable oldInstance) {
-        super(identifier, dependencyResolver, oldModule, oldInstance);
-    }
-
-    @Override
-    public void customValidation() {
-        // add custom validation form module attributes here.
+    public void startSbRestKeepAliveSocket() {
+        opendaylightSfc = OpendaylightSfc.getOpendaylightSfcObj();
+        opendaylightSfc.getExecutor().execute(new SbRestKeepAliveSocket());
     }
 
     @Override
-    public java.lang.AutoCloseable createInstance() {
-
-        final OpendaylightSfc opendaylightSfc = OpendaylightSfc.getOpendaylightSfcObj();
-
-        final SbRestSfEntryDataListener sbRestSfEntryDataListener = new SbRestSfEntryDataListener(opendaylightSfc);
-        final SbRestSfgEntryDataListener sbRestSfgEntryDataListener = new SbRestSfgEntryDataListener(opendaylightSfc);
-        final SbRestSffEntryDataListener sbRestSffEntryDataListener = new SbRestSffEntryDataListener(opendaylightSfc);
-        final SbRestRspEntryDataListener sbRestRspEntryDataListener = new SbRestRspEntryDataListener(opendaylightSfc);
-        final SbRestAclEntryDataListener sbRestAclEntryDataListener = new SbRestAclEntryDataListener(opendaylightSfc);
-        final SbRestScfEntryDataListener sbRestScfEntryDataListener = new SbRestScfEntryDataListener(opendaylightSfc);
-        final SbRestSfstEntryDataListener sbRestSfstEntryDataListener = new SbRestSfstEntryDataListener(opendaylightSfc);
-        final SbRestSfstateEntryDataListener sbRestSfstateEntryDataListener = new SbRestSfstateEntryDataListener(opendaylightSfc);
-        final SbRestKeepAliveSocket sbRestKeepAliveSocket = new SbRestKeepAliveSocket();
-        opendaylightSfc.getExecutor().execute(sbRestKeepAliveSocket);
-
-
-        // close()
-        final class AutoCloseableSfcSbRest implements AutoCloseable {
-
-            @Override
-            public void close() {
-                sbRestSfEntryDataListener.getDataChangeListenerRegistration().close();
-                sbRestSfgEntryDataListener.getDataChangeListenerRegistration().close();
-                sbRestSffEntryDataListener.getDataChangeListenerRegistration().close();
-                sbRestRspEntryDataListener.getDataChangeListenerRegistration().close();
-                sbRestAclEntryDataListener.getDataChangeListenerRegistration().close();
-                sbRestScfEntryDataListener.getDataChangeListenerRegistration().close();
-                sbRestSfstEntryDataListener.getDataChangeListenerRegistration().close();
-                sbRestSfstateEntryDataListener.getDataChangeListenerRegistration().close();
-
-                try {
-                    opendaylightSfc.close();
-                } catch (ExecutionException | InterruptedException e) {
-                    LOG.error("\nFailed to close SfcSbRestProvider instance {} cleanly", this);
-                }
-                LOG.info("SfcSbRestProvider (instance {}) torn down", this);
-            }
+    public void close() throws Exception {
+        try {
+            opendaylightSfc.close();
+        } catch (ExecutionException | InterruptedException e) {
+            LOG.error("Failed to close OpendaylightSfc instance {} cleanly", this);
         }
-
-        return new AutoCloseableSfcSbRest();
     }
-
 }

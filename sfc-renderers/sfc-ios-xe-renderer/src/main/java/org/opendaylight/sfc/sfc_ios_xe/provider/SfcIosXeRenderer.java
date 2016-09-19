8fa67068e403fed5a0a10614b2a3ac5fe53399ef
Vladimir Lavor <vlavor@cisco.com>
2016-09-19 10:46:14 +0200

    Bug 6657: Added init-method into ios-xe-renderer blueprint xml
    Change-Id: I1c1c92592d3654c598740aad46723afcd95b711d
    Signed-off-by: Vladimir Lavor <vlavor@cisco.com>

--- sfc-renderers/sfc-ios-xe-renderer/src/main/java/org/opendaylight/sfc/sfc_ios_xe/provider/SfcIosXeRenderer.java
+++ sfc-renderers/sfc-ios-xe-renderer/src/main/java/org/opendaylight/sfc/sfc_ios_xe/provider/SfcIosXeRenderer.java
@@ -8,28 +8,41 @@
 
 package org.opendaylight.sfc.sfc_ios_xe.provider;
 
+import com.google.common.base.Preconditions;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
 import org.opendaylight.sfc.sfc_ios_xe.provider.renderer.IosXeRspProcessor;
 import org.opendaylight.sfc.sfc_ios_xe.provider.renderer.IosXeServiceForwarderMapper;
 import org.opendaylight.sfc.sfc_ios_xe.provider.renderer.IosXeServiceFunctionMapper;
 import org.opendaylight.sfc.sfc_ios_xe.provider.renderer.NodeManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Initialize all necessary IOS-XE renderer components
  */
 public class SfcIosXeRenderer {
 
-    private final NodeManager nodeManager;
-    private final IosXeServiceForwarderMapper forwarderMapper;
-    private final IosXeServiceFunctionMapper functionMapper;
-    private final IosXeRspProcessor rspProcessor;
+    private static final Logger LOG = LoggerFactory.getLogger(SfcIosXeRenderer.class);
 
-    public SfcIosXeRenderer(DataBroker dataBroker, BindingAwareBroker bindingAwareBroker) {
+    private NodeManager nodeManager;
+    private IosXeServiceForwarderMapper forwarderMapper;
+    private IosXeServiceFunctionMapper functionMapper;
+    private IosXeRspProcessor rspProcessor;
+    private final DataBroker dataBroker;
+    private final BindingAwareBroker bindingAwareBroker;
+
+    public SfcIosXeRenderer(final DataBroker dataBroker, final BindingAwareBroker bindingAwareBroker) {
+        this.dataBroker = Preconditions.checkNotNull(dataBroker);
+        this.bindingAwareBroker = Preconditions.checkNotNull(bindingAwareBroker);
+    }
+
+    public void initialize() {
         nodeManager = new NodeManager(dataBroker, bindingAwareBroker);
         forwarderMapper = new IosXeServiceForwarderMapper(dataBroker, nodeManager);
         functionMapper = new IosXeServiceFunctionMapper(dataBroker, nodeManager);
         rspProcessor = new IosXeRspProcessor(dataBroker, nodeManager);
+        LOG.info("sfc-ios-xe-renderer started");
     }
 
     public void unregisterListeners() {
@@ -37,5 +50,6 @@ public class SfcIosXeRenderer {
         forwarderMapper.unregisterSffListener();
         functionMapper.unregisterSfListener();
         rspProcessor.unregisterRspListener();
+        LOG.info("sfc-ios-xe-renderer closed");
     }
 }

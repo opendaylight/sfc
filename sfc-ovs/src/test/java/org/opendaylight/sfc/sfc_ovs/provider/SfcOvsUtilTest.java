e285444ba312e7380ba6a1e5e07e2dc69ca71ef4
Jaime Caamaño Ruiz <jaime.caamano.ruiz@ericsson.com>
2016-09-23 18:51:41 +0200

    Use SFF OVS node augmentation to get port number
    When rendering a RSP, getPortNumberFromName would use
    lookupTopologyNode to fetch the OVS node. With this patch,
    it will attempt first to use the SFF OVS node augmentation
    which already has a reference to the OVS node. If not found,
    then it will do lookupTopologyNode as before.
    
    Change-Id: Id838555afa497026330199a6bbe68cd07bbfe021
    Signed-off-by: Jaime Caamaño Ruiz <jaime.caamano.ruiz@ericsson.com>

--- sfc-ovs/src/test/java/org/opendaylight/sfc/sfc_ovs/provider/SfcOvsUtilTest.java
+++ sfc-ovs/src/test/java/org/opendaylight/sfc/sfc_ovs/provider/SfcOvsUtilTest.java
@@ -13,15 +13,23 @@ import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertNotNull;
 import static junit.framework.TestCase.assertNull;
 import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.isNull;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import org.hamcrest.core.Is;
+import org.hamcrest.core.IsNull;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Matchers;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.test.AbstractDataBrokerTest;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
@@ -35,6 +43,8 @@ import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.ovs.rev
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.ovs.rev140701.SffOvsBridgeAugmentationBuilder;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.ovs.rev140701.SffOvsLocatorOptionsAugmentation;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.ovs.rev140701.SffOvsLocatorOptionsAugmentationBuilder;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.ovs.rev140701.SffOvsNodeAugmentation;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.ovs.rev140701.SffOvsNodeAugmentationBuilder;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.ovs.rev140701.bridge.OvsBridgeBuilder;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.ovs.rev140701.node.OvsNodeBuilder;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.ovs.rev140701.options.OvsOptionsBuilder;
@@ -405,6 +415,42 @@ public class SfcOvsUtilTest extends AbstractDataBrokerTest {
         // ip address assigned to data plane locator, we can recover it from node
         assertNotNull("Must not be null", node);
         assertEquals("Must be equal", node.getNodeId().getValue(), ipv4Address);
+    }
+
+    @Test
+    public void getOvsNodeNull() throws Exception {
+        ServiceFunctionForwarder sff = new ServiceFunctionForwarderBuilder().build();
+
+        Node node = SfcOvsUtil.getOvsNode(sff, executorService);
+
+        assertThat(node, nullValue());
+    }
+
+    @Test
+    public void getOvsNodeByTopology() throws Exception {
+        IpBuilder ipBuilder = new IpBuilder();
+        ipBuilder.setIp(new IpAddress(new Ipv4Address(ipv4Address)));
+        ServiceFunctionForwarder sff = new ServiceFunctionForwarderBuilder()
+                .setSffDataPlaneLocator(createSffDataPlaneLocatorList(null, ipBuilder.build()))
+                .build();
+
+        Node node = SfcOvsUtil.getOvsNode(sff, executorService);
+
+        assertThat(node.getNodeId().getValue(), is(testIpAddress));
+    }
+
+    @Test
+    public void getOvsNodeBySffNodeAugmentation() throws Exception {
+        ServiceFunctionForwarder sff = new ServiceFunctionForwarderBuilder()
+                .addAugmentation(SffOvsNodeAugmentation.class,
+                        new SffOvsNodeAugmentationBuilder()
+                                .setOvsNode(new OvsNodeBuilder().setNodeId(new OvsdbNodeRef(nodeIID)).build())
+                                .build())
+                .build();
+
+        Node node = SfcOvsUtil.getOvsNode(sff, executorService);
+
+        assertThat(node.getNodeId().getValue(), is(testIpAddress));
     }
 
     @Test

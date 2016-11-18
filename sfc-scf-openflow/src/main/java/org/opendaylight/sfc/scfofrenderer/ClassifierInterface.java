2531a5467b3860ee090915ffde9c5387d26dd672
Miguel Duarte <miguel.duarte.de.mora.barroso@ericsson.com>
2016-11-18 12:26:32 +0100

    Integrate classifier w/ genius - commit #2
    Major refactor of the SfcScfOfProcessor class, which had too many
    responsabilities. These were split among the following classes:
    
    SfcScfOfProcessor:
    Processes adding classifier flows into a number of OVS switches.
    Client of the OpenflowClassifierProcessor class, from which it gets
    a list of all the flows to install among the OVSs, writing them in a
    single transaction object.
    
    OpenflowClassifierProcessor:
    Encapsulates a transaction object, and processes the creation of
    ACLs (along w/ respective ACEs) into the OVS switches.
    Client of the LogicallyAttachedClassifier class.
    
    ClassifierInterface:
    Responsible for building the classifier flows required for the
    logical SFF feature. WIP, and will feature in the next commit.
    There are 2 types of ClassifierInterface:
      - LogicallyAttachedClassifier - empty implementation; will be uploaded
        in the next commit
      - BareClassifier - 'Legacy' scenario, moved into a wrapper class
    
    LogicalClassifierDataGetter:
    Utility class used to fetch data relevant to the OpenflowClassifier
    Processor and the LogicallyAttached classifier.
    Encapsulates the genius RPC, and fetches data from genius, SFC, and
    IETF data models. WIP, and will feature in the next commit.
    
    The SfcScfOfUtils are also refactored, building the required flows, so
    that the SfcScfOfProcessor can install them afterwards, in a single
    transaction object.
    
    Blueprint injection of the RpcProvider (required for the logical SFF
    feature).
    
    Add unit testing of the legacy scenarios, and well as w/ DPDK flow
    generation.
    
    In this commit, while the way the classifier works is drastically
    changed, no new functionallity is added. It finished paving the way
    for integrating the new feature, making it easier to be understood,
    and easier to unit test.
    
    In the next - and last - commit, I will complete the APIs of the
    forementioned classes, thus enabling the classifier to be attached
    to a logical SFF.
    
    Change-Id: I8c59078b8b04b4e8cb49e85a779f2d4940c1e546
    Signed-off-by: Miguel Duarte <miguel.duarte.de.mora.barroso@ericsson.com>

---
+++ sfc-scf-openflow/src/main/java/org/opendaylight/sfc/scfofrenderer/ClassifierInterface.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2016 Ericsson Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.sfc.scfofrenderer;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.FlowBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.flow.Match;
+
+import java.util.Optional;
+
+public interface ClassifierInterface {
+    FlowBuilder initClassifierTable();
+
+    FlowBuilder createClassifierOutFlow(String flowKey,
+                                        Match match,
+                                        SfcNshHeader sfcNshHeader,
+                                        String classifierNodeName);
+
+    Optional<String> getNodeName(String theInterfaceName);
+
+    Optional<Long> getInPort(String ifName, String nodeName);
+}

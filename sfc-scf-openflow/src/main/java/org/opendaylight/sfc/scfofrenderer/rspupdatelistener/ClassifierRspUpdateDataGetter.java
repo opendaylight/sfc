987685b0a130f522dad7b697a200d9e0b86f7748
Miguel Duarte <miguel.duarte.de.mora.barroso@ericsson.com>
2016-11-30 12:42:39 +0100

    Classifier reacts to first SF location updates
    Whenever the first SF in an RSP migrates to another compute node, the
    classifier rules are updated accordingly - whether sending the packet
    through a different tunnel connected to the new compute node, or, in
    scenarios when the SF moved to the compute node where the classifier is
    installed, by jumping directly to the SFC transport ingress table.
    
    The helper methods on the OpenflowClassifierProcessor were moved into a
    new class - ClassifierHandler.
    
    The ClassifierRspUpdateDataGetter was finished, providing 2 methods:
      - filter all ACLs whose ACEs impact the supplied RSP
      - filter all ScfClassifier objects that implement those ACLs
    
    The ClassifierRspsUpdateListener was added logic on how to identify the
    movement of the first SF in any RSP, and also triggers the classifier
    flows update through the ClassifierRspUpdateProcessor.
    
    The ClassifierRspUpdateProcessor follows the same principle of the
    OpenflowClassifierProcessor - build all the flows to be installed. A new
    class was created to separate the concerns (classifier updates have the
    RSP as its single argument, whereas classifier add/delete have the ACL
    and the ScfClassifier objects as arguments).
    
    Everything else are tests (+- 600 lines)
    
    Change-Id: I487d98d282b28aefd79d0658fcc50e20897beffa
    Signed-off-by: Miguel Duarte <miguel.duarte.de.mora.barroso@ericsson.com>

--- sfc-scf-openflow/src/main/java/org/opendaylight/sfc/scfofrenderer/rspupdatelistener/ClassifierRspUpdateDataGetter.java
+++ sfc-scf-openflow/src/main/java/org/opendaylight/sfc/scfofrenderer/rspupdatelistener/ClassifierRspUpdateDataGetter.java
@@ -8,12 +8,23 @@
 
 package org.opendaylight.sfc.scfofrenderer.rspupdatelistener;
 
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.sfc.provider.api.SfcDataStoreAPI;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.acl.rev151001.Actions1;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.acl.rev151001.access.lists.acl.access.list.entries.ace.actions.sfc.action.AclRenderedServicePath;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.common.rev151017.RspName;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.scf.rev140701.ServiceFunctionClassifiers;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.scf.rev140701.service.function.classifiers.ServiceFunctionClassifier;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.scf.rev140701.service.function.classifiers.service.function.classifier.SclServiceFunctionForwarder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.AccessLists;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.Acl;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.Ace;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class ClassifierRspUpdateDataGetter {
     /**
@@ -21,8 +32,21 @@ public class ClassifierRspUpdateDataGetter {
      * @return  a list of all the ACLs that apply to the given RSP name
      */
     public List<Acl> filterAclsByRspName(RspName theRspName) {
-        // TODO
-        return Collections.emptyList();
+        InstanceIdentifier<AccessLists>
+                ifConfigIID = InstanceIdentifier.builder(AccessLists.class).build();
+
+        return Optional.ofNullable(SfcDataStoreAPI.readTransactionAPI(ifConfigIID, LogicalDatastoreType.CONFIGURATION))
+                .map(AccessLists::getAcl)
+                .orElse(Collections.emptyList())
+                .stream()
+                .filter(acl -> acl.getAccessListEntries() != null)
+                .filter(acl -> acl.getAccessListEntries().getAce()
+                        .stream()
+                        .map(Ace::getActions)
+                        .map(actions -> actions.getAugmentation(Actions1.class))
+                        .map(Actions1 -> (AclRenderedServicePath) Actions1.getSfcAction())
+                        .anyMatch(sfcAction -> sfcAction.getRenderedServicePath().equals(theRspName.getValue())))
+                .collect(Collectors.toList());
     }
 
     /**
@@ -30,7 +54,16 @@ public class ClassifierRspUpdateDataGetter {
      * @return  a list of all the {@link SclServiceFunctionForwarder} enforcing the given ACL
      */
     public List<SclServiceFunctionForwarder> filterClassifierNodesByAclName(String theAclName) {
-        // TODO
-        return Collections.emptyList();
+        InstanceIdentifier<ServiceFunctionClassifiers>
+                ifConfigIID = InstanceIdentifier.builder(ServiceFunctionClassifiers.class).build();
+
+        return Optional.ofNullable(SfcDataStoreAPI.readTransactionAPI(ifConfigIID, LogicalDatastoreType.CONFIGURATION))
+                .map(ServiceFunctionClassifiers::getServiceFunctionClassifier)
+                .orElse(Collections.emptyList())
+                .stream()
+                .filter(classifier -> classifier.getAcl().getName().equals(theAclName))
+                .map(ServiceFunctionClassifier::getSclServiceFunctionForwarder)
+                .findAny()
+                .orElse(Collections.emptyList());
     }
 }
---
+++ sfc-scf-openflow/src/test/java/org/opendaylight/sfc/scfofrenderer/ClassifierHandlerTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2016 Ericsson Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.sfc.scfofrenderer;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.scf.rev140701.attachment.point.attachment.point.type.BridgeBuilder;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.scf.rev140701.attachment.point.attachment.point.type.InterfaceBuilder;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.scf.rev140701.service.function.classifiers.ServiceFunctionClassifier;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.scf.rev140701.service.function.classifiers.service.function.classifier.SclServiceFunctionForwarder;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.service.function.forwarder.base.SffDataPlaneLocator;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.service.function.forwarders.ServiceFunctionForwarder;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class ClassifierHandlerTest {
+    @Mock
+    private SclServiceFunctionForwarder sffClassifier;
+
+    @Mock
+    private ServiceFunctionClassifier scf;
+
+    @Mock
+    private ServiceFunctionForwarder sff;
+
+    private ClassifierHandler handler;
+
+    private String INTERFACE_TO_CLASSIFY = "750135c0-67a9-4fc1-aac0-1359ae7944d4";
+
+    public ClassifierHandlerTest() {
+        initMocks(this);
+        handler = new ClassifierHandler();
+    }
+
+    @Before
+    public void setUp() {
+        when(sffClassifier.getName()).thenReturn("sffName");
+        when(sffClassifier.getAttachmentPointType())
+            .thenReturn(new InterfaceBuilder()
+                        .setInterface(INTERFACE_TO_CLASSIFY)
+                        .build());
+        when(scf.getSclServiceFunctionForwarder())
+            .thenReturn(new ArrayList<SclServiceFunctionForwarder>(){{ add(sffClassifier); }});
+    }
+
+    @Test
+    public void buildFlowKeyNameTest() {
+        Assert.assertEquals("abcballoons", handler.buildFlowKeyName("a", "b", "c", "balloons"));
+    }
+
+    @Test
+    public void getInterfaceNameFromClassifierOK() {
+        Optional<String> res = handler.getInterfaceNameFromClassifier(sffClassifier);
+        Assert.assertTrue(res.isPresent());
+        Assert.assertEquals(INTERFACE_TO_CLASSIFY, res.get());
+    }
+
+    @Test
+    public void getInterfaceNameFromClassifierWrongAttachmentPoint() {
+        when(sffClassifier.getAttachmentPointType()).thenReturn(new BridgeBuilder().build());
+        Optional<String> res = handler.getInterfaceNameFromClassifier(sffClassifier);
+        Assert.assertFalse(res.isPresent());
+    }
+
+    @Test
+    public void getInterfaceNameFromClassifierNullClassifier() {
+        Optional<String> res = handler.getInterfaceNameFromClassifier(null);
+        Assert.assertFalse(res.isPresent());
+    }
+
+    @Test
+    public void usesLogicalInterfacesLogicalSff() {
+        when(sff.getSffDataPlaneLocator()).thenReturn(null);
+        Assert.assertTrue(handler.usesLogicalInterfaces(sff));
+    }
+
+    @Test
+    public void usesLogicalInterfacesLegacySff() {
+        when(sff.getSffDataPlaneLocator()).thenReturn(new ArrayList<SffDataPlaneLocator>());
+        Assert.assertFalse(handler.usesLogicalInterfaces(sff));
+    }
+}

5cafe8c81d062924ef4edb882728e33c6199d932
Miguel Duarte <miguel.duarte.de.mora.barroso@ericsson.com>
2016-11-30 12:42:39 +0100

    Classifier reacts to first SF location updates
    Whenever the first SF in an RSP migrates to another compute node, the
    classifier rules are updated accordingly - whether sending the packet
    through a different tunnel connected to the new compute node, or, in
    scenarios when the SF moved to the compute node where the classifier is
    installed, by jumping directly to the SFC transport ingress table.
    
    The helper methods on the OpenflowClassifierProcessor were moved into a
    new class - ClassifierHandler.
    
    The ClassifierRspUpdateDataGetter was finished, providing 2 methods:
      - filter all ACLs whose ACEs impact the supplied RSP
      - filter all ScfClassifier objects that implement those ACLs
    
    The ClassifierRspsUpdateListener was added logic on how to identify the
    movement of the first SF in any RSP, and also triggers the classifier
    flows update through the ClassifierRspUpdateProcessor.
    
    The ClassifierRspUpdateProcessor follows the same principle of the
    OpenflowClassifierProcessor - build all the flows to be installed. A new
    class was created to separate the concerns (classifier updates have the
    RSP as its single argument, whereas classifier add/delete have the ACL
    and the ScfClassifier objects as arguments).
    
    Everything else are tests (+- 600 lines)
    
    Change-Id: I487d98d282b28aefd79d0658fcc50e20897beffa
    Signed-off-by: Miguel Duarte <miguel.duarte.de.mora.barroso@ericsson.com>

---
+++ sfc-scf-openflow/src/main/java/org/opendaylight/sfc/scfofrenderer/rspupdatelistener/ClassifierRspUpdateProcessor.java
@@ -0,0 +1,173 @@
+/*
+ * Copyright (c) 2016 Ericsson Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.sfc.scfofrenderer.rspupdatelistener;
+
+import org.opendaylight.sfc.provider.api.SfcProviderServiceForwarderAPI;
+import org.opendaylight.sfc.scfofrenderer.ClassifierHandler;
+import org.opendaylight.sfc.scfofrenderer.ClassifierInterface;
+import org.opendaylight.sfc.scfofrenderer.SfcNshHeader;
+import org.opendaylight.sfc.scfofrenderer.SfcScfMatch;
+import org.opendaylight.sfc.scfofrenderer.logicalclassifier.LogicallyAttachedClassifier;
+import org.opendaylight.sfc.util.openflow.transactional_writer.FlowDetails;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.common.rev151017.SffName;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.rsp.rev140701.rendered.service.paths.RenderedServicePath;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.scf.rev140701.service.function.classifiers.service.function.classifier.SclServiceFunctionForwarder;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.service.function.forwarders.ServiceFunctionForwarder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.Acl;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.Ace;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.FlowBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.flow.Match;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeConnectorId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ClassifierRspUpdateProcessor {
+
+    private ClassifierInterface classifierInterface = null;
+
+    private static final Logger LOG = LoggerFactory.getLogger(ClassifierRspUpdateProcessor.class);
+
+    private ClassifierHandler classifierHandler;
+
+    // hide the default constructor
+    private ClassifierRspUpdateProcessor() {classifierHandler = new ClassifierHandler();}
+
+    public ClassifierRspUpdateProcessor(LogicallyAttachedClassifier theClassifierInterface) {
+        this();
+        classifierInterface = theClassifierInterface;
+    }
+
+    /**
+     * Process adding classifier flows upon a RenderedServicePath update.
+     * The old classifier flows were already deleted, so this method only cares about writing the new flows.
+     * The RSP is passed as parameter, since within genius the interface state is currently cached, which makes its
+     * RPC return the old data-plane ID for the first SF in the RSP.
+     * Also, passing the RSP as a parameter enables us to skip that data-store read operation.
+     *
+     * @param theClassifier the classifier node to be added
+     * @param theAcl        the ACL we want to install in the classifier
+     * @param theRsp        the {@link RenderedServicePath} object for which we will re-install the classifier flows
+     * @return              a List of {@link FlowDetails} having all the generated flows, which will be later installed
+     */
+    public List<FlowDetails> processClassifier(
+            final SclServiceFunctionForwarder theClassifier,
+            final Acl theAcl,
+            final RenderedServicePath theRsp) {
+        LOG.info("processClassifier - Processing the upadate of RSP {}", theRsp.getPathId());
+
+        SffName sffName = new SffName(theClassifier.getName());
+
+        Optional<String> itfName = classifierHandler.getInterfaceNameFromClassifier(theClassifier);
+
+        ServiceFunctionForwarder sff = SfcProviderServiceForwarderAPI.readServiceFunctionForwarder(sffName);
+
+        // RSP updates only supported when attached to a logical SFF
+        if (!classifierHandler.usesLogicalInterfaces(sff)) {
+            LOG.error("processClassifier - Not attached to a logical SFF; Bailing out");
+            return Collections.emptyList();
+        }
+
+        if (!itfName.isPresent()) {
+            LOG.error("processClassifier - Could not extract a LogicalInterface from the classifier's attachment point");
+            return Collections.emptyList();
+        }
+
+        Optional<String> nodeName = classifierInterface.getNodeName(itfName.get());
+        // No need to perform service binding again; we're only concerned w/ the movement of the first SF
+
+        if(!nodeName.isPresent()) {
+            LOG.error("processClassifier - Could not extract the node name from the OVS interface");
+            return Collections.emptyList();
+        }
+
+        Optional<Long> inPort = classifierInterface.getInPort(itfName.get(), nodeName.get());
+        if (!inPort.isPresent()) {
+            LOG.error("processClassifier - port is null; returning empty list");
+            return Collections.emptyList();
+        }
+
+        return theAcl
+                .getAccessListEntries()
+                .getAce()
+                .stream()
+                .map(theAce -> processAce(theRsp,
+                        nodeName.get(),
+                        theClassifier.getName(),
+                        theAcl.getAclName(),
+                        inPort.get(),
+                        theAce))
+                .reduce(new ArrayList<>(),
+                        (dstList, theList) ->
+                                Stream.concat(dstList.stream(), theList.stream()).collect(Collectors.toList()));
+    }
+
+    /**
+     * Install an ACE entry, belonging to the given ACL, on the SFF having the supplied node name.
+     * This method is called on RSP updates.
+     *
+     * @param theRsp        the {@link RenderedServicePath} for which this ACE applies, having the updated DpnIdType.
+     * @param nodeName      the node name where the classifier flows will be installed
+     * @param theScfName    the provisioned name of the classifier - used to generate the flow key name
+     * @param aclName       the name of the provisioned ACL object
+     * @param inPort        the in port of the classifier
+     * @param theAce        the {@link Ace} object for which we want to install flows
+     * @return              a list having all the {@link FlowDetails} that will be installed in the classifier
+     */
+    private List<FlowDetails> processAce(RenderedServicePath theRsp,
+                                         String nodeName,
+                                         String theScfName,
+                                         String aclName,
+                                         long inPort,
+                                         Ace theAce) {
+        List<FlowDetails> theFlows = new ArrayList<>();
+
+        String ruleName = theAce.getRuleName();
+        if (ruleName == null) {
+            LOG.error("processAce - ruleName is null; returning empty list");
+            return Collections.emptyList();
+        }
+
+        NodeConnectorId port = new NodeConnectorId(String.format("%s:%s", nodeName, inPort));
+
+        LOG.debug("processAce: in port: {}", inPort);
+        // Match
+        Match match = new SfcScfMatch()
+                .setPortMatch(port)
+                .setAclMatch(theAce.getMatches())
+                .build();
+
+        SfcNshHeader nsh = SfcNshHeader.getSfcNshHeader(theRsp);
+
+        if (nsh == null) {
+            LOG.error("processAce - nsh is null; returning empty list");
+            return Collections.emptyList();
+        }
+
+        String flowKey = classifierHandler.buildFlowKeyName(theScfName, aclName, ruleName, ".out");
+
+        // write the flows into the classifier
+        LOG.info("processAce - About to create flows");
+        FlowBuilder initFlow, theOutFlow;
+        initFlow = classifierInterface.initClassifierTable();
+        theOutFlow  = classifierInterface.createClassifierOutFlow(flowKey, match, nsh, nodeName);
+
+        theFlows.add(classifierHandler.addRspRelatedFlowIntoNode(nodeName, initFlow, nsh.getNshNsp()));
+        theFlows.add(classifierHandler.addRspRelatedFlowIntoNode(nodeName, theOutFlow, nsh.getNshNsp()));
+
+        // DPDK is not supported for logical SFF, thus, its flows are not installed here
+        return theFlows;
+    }
+}
---
+++ sfc-scf-openflow/src/test/java/org/opendaylight/sfc/scfofrenderer/rspupdatelistener/ClassifierRspUpdateDataGetterTest.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) 2016 Ericsson Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.sfc.scfofrenderer.rspupdatelistener;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+
+import org.opendaylight.sfc.provider.api.SfcDataStoreAPI;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.acl.rev151001.Actions1;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.acl.rev151001.access.lists.acl.access.list.entries.ace.actions.sfc.action.AclRenderedServicePath;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.common.rev151017.RspName;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.scf.rev140701.ServiceFunctionClassifiers;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.scf.rev140701.service.function.classifiers.ServiceFunctionClassifier;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.scf.rev140701.service.function.classifiers.service.function.classifier.SclServiceFunctionForwarder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.AccessLists;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.Acl;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.AccessListEntries;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.Ace;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.Actions;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(SfcDataStoreAPI.class)
+public class ClassifierRspUpdateDataGetterTest {
+    @Mock
+    AccessLists accessLists;
+
+    @Mock
+    ServiceFunctionClassifiers classifierContainer;
+
+    @Mock
+    ServiceFunctionClassifier theClassifier;
+
+    @Mock
+    SclServiceFunctionForwarder sclClassifier;
+
+    @Mock
+    Acl acl;
+
+    @Mock
+    AccessListEntries aces;
+
+    @Mock
+    Ace ace;
+
+    @Mock
+    Actions actions;
+
+    @Mock
+    Actions1 findAbetterNameForThis;
+
+    @Mock
+    AclRenderedServicePath aclRsp;
+
+    @Mock
+    org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.scf.rev140701.service.function.classifiers.service.function.classifier.Acl classifierAcl;
+
+    @Mock
+    SclServiceFunctionForwarder sclForwarder;
+
+    List<Acl> aclList = new ArrayList<>();
+
+    List<Ace> aceList = new ArrayList<>();
+
+    List<ServiceFunctionClassifier> classifierList = new ArrayList<>();
+
+    List<SclServiceFunctionForwarder> sclClassifierList = new ArrayList<>();
+
+    public ClassifierRspUpdateDataGetterTest() {
+        initMocks(this);
+    }
+
+    @Before
+    public void setUp() {
+        PowerMockito.mockStatic(SfcDataStoreAPI.class);
+
+        when(accessLists.getAcl()).thenReturn(aclList);
+
+        aclList.add(acl);
+
+        when(acl.getAclName()).thenReturn("acl1");
+
+        when(acl.getAccessListEntries()).thenReturn(aces);
+
+        when(aces.getAce()).thenReturn(aceList);
+
+        aceList.add(ace);
+
+        when(ace.getActions()).thenReturn(actions);
+
+        when(actions.getAugmentation(eq(Actions1.class))).thenReturn(findAbetterNameForThis);
+
+        when(findAbetterNameForThis.getSfcAction()).thenReturn(aclRsp);
+
+        when(aclRsp.getRenderedServicePath()).thenReturn("rsp1");
+
+
+        when(classifierAcl.getName()).thenReturn("acl1");
+
+        when(theClassifier.getAcl()).thenReturn(classifierAcl);
+
+        classifierList.add(theClassifier);
+
+        when(sclForwarder.getName()).thenReturn("c1");
+
+        sclClassifierList.add(sclForwarder);
+
+        when(theClassifier.getSclServiceFunctionForwarder()).thenReturn(sclClassifierList);
+    }
+
+    @Test
+    public void testAclFilteringByRspName() {
+        PowerMockito.when(SfcDataStoreAPI
+                .readTransactionAPI(any(), any())).thenReturn(accessLists);
+        RspName theRspName = new RspName("rsp1");
+
+        ClassifierRspUpdateDataGetter rspUpdateListener = new ClassifierRspUpdateDataGetter();
+        Assert.assertFalse(rspUpdateListener.filterAclsByRspName(theRspName).isEmpty());
+        Assert.assertEquals(1, rspUpdateListener.filterAclsByRspName(theRspName).size());
+        Assert.assertEquals("acl1", rspUpdateListener.filterAclsByRspName(theRspName).get(0).getAclName());
+    }
+
+    @Test
+    public void testClassifierFilteringByAclName() {
+        PowerMockito.when(SfcDataStoreAPI
+                .readTransactionAPI(any(), any())).thenReturn(classifierContainer);
+
+        when(classifierContainer.getServiceFunctionClassifier()).thenReturn(classifierList);
+
+        ClassifierRspUpdateDataGetter rspUpdateListener = new ClassifierRspUpdateDataGetter();
+        Assert.assertFalse(rspUpdateListener.filterClassifierNodesByAclName("acl1").isEmpty());
+        Assert.assertEquals(1, rspUpdateListener.filterClassifierNodesByAclName("acl1").size());
+        Assert.assertEquals("c1", rspUpdateListener.filterClassifierNodesByAclName("acl1").get(0).getName());
+    }
+}

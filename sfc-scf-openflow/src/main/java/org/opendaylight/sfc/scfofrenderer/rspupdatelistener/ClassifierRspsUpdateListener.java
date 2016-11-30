acab79043b05b6f8d3c60de6dc9832a53d22ff1a
Miguel Duarte <miguel.duarte.de.mora.barroso@ericsson.com>
2016-11-30 12:42:39 +0100

    Classifier reacts to first SF location updates
    Change-Id: I487d98d282b28aefd79d0658fcc50e20897beffa
    Signed-off-by: Miguel Duarte <miguel.duarte.de.mora.barroso@ericsson.com>

--- sfc-scf-openflow/src/main/java/org/opendaylight/sfc/scfofrenderer/rspupdatelistener/ClassifierRspsUpdateListener.java
+++ sfc-scf-openflow/src/main/java/org/opendaylight/sfc/scfofrenderer/rspupdatelistener/ClassifierRspsUpdateListener.java
@@ -13,25 +13,32 @@ import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.sfc.provider.api.SfcInstanceIdentifiers;
 import org.opendaylight.sfc.provider.listeners.AbstractDataTreeChangeListener;
-import org.opendaylight.sfc.scfofrenderer.OpenflowClassifierProcessor;
 import org.opendaylight.sfc.scfofrenderer.logicalclassifier.LogicalClassifierDataGetter;
 import org.opendaylight.sfc.util.openflow.transactional_writer.SfcOfFlowWriterInterface;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.rsp.rev140701.rendered.service.paths.RenderedServicePath;
+import org.opendaylight.sfc.util.openflow.transactional_writer.FlowDetails;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.common.rev151017.RspName;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.Acl;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.scf.rev140701.service.function.classifiers.service.function.classifier.SclServiceFunctionForwarder;
+import org.opendaylight.yang.gen.v1.urn.ericsson.params.xml.ns.yang.sfc.sff.logical.rev160620.DpnIdType;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Optional;
 
 public class ClassifierRspsUpdateListener extends AbstractDataTreeChangeListener<RenderedServicePath> {
     private static final Logger LOG = LoggerFactory.getLogger(ClassifierRspsUpdateListener.class);
     private final DataBroker dataBroker;
     private ListenerRegistration listenerRegistration;
-    private OpenflowClassifierProcessor classifierProcessor;
+    private ClassifierRspUpdateProcessor classifierProcessor;
     private SfcOfFlowWriterInterface openflowWriter;
     private ClassifierRspUpdateDataGetter updateDataGetter;
     private LogicalClassifierDataGetter dataGetter;
 
     public ClassifierRspsUpdateListener(DataBroker theDataBroker,
-                                        OpenflowClassifierProcessor theClassifierProcessor,
+                                        ClassifierRspUpdateProcessor theClassifierProcessor,
                                         SfcOfFlowWriterInterface theOpenflowWriter,
                                         ClassifierRspUpdateDataGetter theUpdateDataGetter,
                                         LogicalClassifierDataGetter theDataGetter) {
@@ -40,8 +47,7 @@ public class ClassifierRspsUpdateListener extends AbstractDataTreeChangeListener
         openflowWriter = theOpenflowWriter;
         updateDataGetter = theUpdateDataGetter;
         dataGetter = theDataGetter;
-        // TODO - uncomment line below to activate listeners
-//        registerListeners();
+        registerListeners();
     }
 
     private void registerListeners() {
@@ -59,14 +65,48 @@ public class ClassifierRspsUpdateListener extends AbstractDataTreeChangeListener
 
     @Override
     protected void remove(RenderedServicePath deletedRsp) {
-        // TODO - should we delete the classifier flows related to a deleted RSP?...
-        // I'm inclined to say no, because we don't do anything when an RSP is added
+        /*LOG.debug("remove - removing rspId: {}", deletedRsp.getPathId());
+        openflowWriter.deleteRspFlows(deletedRsp.getPathId());
+        openflowWriter.deleteFlowSet();*/
     }
 
     @Override
     protected void update(RenderedServicePath originalRsp, RenderedServicePath updatedRsp) {
-        // TODO - identify relevant updates
-        // TODO - trigger re-rendering of classifier flows
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("update - old rspId: {}; new rspId: {}", originalRsp, updatedRsp);
+        }
+
+        Optional<DpnIdType> originalDataplaneId = dataGetter.getFirstHopDataplaneId(originalRsp);
+        Optional<DpnIdType> updatedDataplaneId = dataGetter.getFirstHopDataplaneId(updatedRsp);
+
+        if (originalDataplaneId.isPresent() &&
+                updatedDataplaneId.isPresent() &&
+                !originalDataplaneId.get().equals(updatedDataplaneId.get())) {
+            LOG.info("update - First SF of RSP [{}] *moved*. Old DPNID: {}; New DPNID: {}",
+                    originalRsp.getPathId(),
+                    originalDataplaneId.get(),
+                    updatedDataplaneId.get());
+            RspName theRspName = originalRsp.getName();
+            // first SF moved elsewhere; delete old flows, then create new flows (for now, separate transactions)
+            // keep in mind that the only flows in the openflow are classifier flows - the RSP flows are in another
+            // of writer instance
+            openflowWriter.deleteRspFlows(originalRsp.getPathId());
+            openflowWriter.deleteFlowSet();
+            List<Acl> theAcls = updateDataGetter.filterAclsByRspName(theRspName);
+
+            theAcls.forEach(acl -> {
+                List<SclServiceFunctionForwarder> theClassifierObjects =
+                        updateDataGetter.filterClassifierNodesByAclName(acl.getAclName());
+                theClassifierObjects.forEach(classifier -> {
+                    List<FlowDetails> allFlows = classifierProcessor.processClassifier(classifier, acl, updatedRsp);
+                    if(!allFlows.isEmpty()) {
+                        LOG.debug("update - Flows generated; gonna write them into the DS ");
+                        openflowWriter.writeFlows(allFlows);
+                        openflowWriter.flushFlows();
+                    }
+                });
+            });
+        }
     }
 
     @Override

3ef4969e0fc0ca8bcd0f6f348498f4cebafa916e
David Suarez <david.suarez.fuentes@ericsson.com>
2017-01-10 14:53:20 +0100

    Add private constructor.
    Add private constructor to utility class to avoid instantiation.
    
    Change-Id: I194258e406cb3a1d691b7a26c0dcbc63f6de5b2f
    Signed-off-by: David Suarez <david.suarez.fuentes@ericsson.com>

--- sfc-genius/src/main/java/org/opendaylight/sfc/genius/impl/utils/SfcGeniusUtils.java
+++ sfc-genius/src/main/java/org/opendaylight/sfc/genius/impl/utils/SfcGeniusUtils.java
@@ -25,18 +25,27 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeCon
 public class SfcGeniusUtils {
 
     /**
+     * Private constructor to avoid instantiation.
+     */
+    private SfcGeniusUtils() {
+    }
+
+    /**
      * Adapts a {@link ListenableFuture} to a {@link CompletableFuture}. The
      * provided executor is used to execute the callback as per
      * {@link Futures#addCallback(ListenableFuture, FutureCallback, Executor)}
      * On such callback, completion will be performed.
      *
-     * @param listenableFuture the listenable future to adapt.
-     * @param executor the executor where the callback execution is submitted.
-     * @param <T> the type of the listenable future.
+     * @param listenableFuture
+     *            the listenable future to adapt.
+     * @param executor
+     *            the executor where the callback execution is submitted.
+     * @param <T>
+     *            the type of the listenable future.
      * @return the completable future.
      */
-     public static <T> CompletableFuture<T> toCompletableFuture(final ListenableFuture<T> listenableFuture,
-                                                                Executor executor) {
+    public static <T> CompletableFuture<T> toCompletableFuture(final ListenableFuture<T> listenableFuture,
+            Executor executor) {
 
         CompletableFuture<T> completable = new CompletableFuture<T>() {
             @Override
@@ -64,11 +73,13 @@ public class SfcGeniusUtils {
     /**
      * Extracts the data plane node Id from a lower layer interface list.
      *
-     * @param lowerLayerIfList to extract the data plane node Id from.
+     * @param lowerLayerIfList
+     *            to extract the data plane node Id from.
      * @return the data plane node Id.
-     * @throws SfcGeniusRuntimeException wrapping an
-     * {@link IllegalArgumentException} if the input list does not contain one
-     * item only, or if the format of the item is invalid.
+     * @throws SfcGeniusRuntimeException
+     *             wrapping an {@link IllegalArgumentException} if the input
+     *             list does not contain one item only, or if the format of the
+     *             item is invalid.
      */
     public static BigInteger getDpnIdFromLowerLayerIfList(List<String> lowerLayerIfList) {
         if (lowerLayerIfList == null || lowerLayerIfList.size() != 1) {

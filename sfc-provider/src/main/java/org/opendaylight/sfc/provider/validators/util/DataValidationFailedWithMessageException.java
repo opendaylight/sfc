a111b94138c27c9c0c3146a4bf3ad5ffdb9c9982
Diego Granados <diego.jesus.granados.lopez@ericsson.com>
2017-01-16 11:58:39 +0100

    Bug 3712: SFP validation of SF types at creation time
    This commit makes use of DOMDataTreeCommitCohorts (callbacks
    that can be registered in order to participate in commit
    validation) in order to make sure that newly-added datastore
    SFPs are consistent with previously defined SFCs and SFs.
    Specifically, it is checked that the types for the SFs the
    SFP references are consistent with the SF types that the
    SFC specifies.
    
    Since the datastore commit transaction is kept open during
    validation, this validation must be as fast as possible.
    Controller guidelines about not opening new datastore
    reads in order to validate a DS write have been followed
    to the maximum extent; for this reason, a datastore cache
    has been deployed to cover both SFs and SFCs. On systems
    with less than 500 SFs & SFCs deployed, caches will provide
    total coverage (i.e. validation will never need to access
    datastore)
    
    Testing: manually tested (complex interactions between
    controller-mdsal-scf are too hard to simulate). Proper CSIT
    tests will be delivered to the sfc suites in the integration
    project.
    
    Change-Id: I5886d0be9191043aa8227c242a1498e518b31a38
    Signed-off-by: Diego Granados <diego.jesus.granados.lopez@ericsson.com>

---
+++ sfc-provider/src/main/java/org/opendaylight/sfc/provider/validators/util/DataValidationFailedWithMessageException.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2017 Ericsson Spain and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.sfc.provider.validators.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opendaylight.mdsal.common.api.DataValidationFailedException;
+import org.opendaylight.yangtools.concepts.Path;
+import org.opendaylight.yangtools.yang.common.RpcError;
+import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
+
+import com.google.common.base.MoreObjects;
+
+import org.opendaylight.yangtools.yang.common.RpcError.ErrorType;
+
+@SuppressWarnings("serial")
+/**
+ * Specific data validation exception for SFP validation. Not working
+ * yet (pending custom error message support on other projects)
+ */
+public class DataValidationFailedWithMessageException extends DataValidationFailedException {
+
+    private List<RpcError> myErrorList = new ArrayList<RpcError>();
+
+    public <P extends Path<P>> DataValidationFailedWithMessageException(final Class<P> pathType,final P path,
+                                                             final String message, final String appTag) {
+        super(pathType, path, message);
+        myErrorList.add(RpcResultBuilder.newError(ErrorType.APPLICATION, "invalid-value", message, appTag,
+                                                        path != null ? path.toString() : null, null));
+    }
+
+    @Override
+    public List<RpcError> getErrorList() {
+            return myErrorList;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this).add("message", getMessage())
+                .add("errorList", this.myErrorList).toString();
+    }
+}
+
+
---
+++ sfc-provider/src/main/java/org/opendaylight/sfc/provider/validators/util/SfcDatastoreCache.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2017 Ericsson Spain and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.sfc.provider.validators.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opendaylight.sfc.provider.api.SfcProviderServiceChainAPI;
+import org.opendaylight.sfc.provider.api.SfcProviderServiceFunctionAPI;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.common.rev151017.SfName;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.common.rev151017.SfcName;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.functions.ServiceFunction;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sfc.rev140701.service.function.chain.grouping.ServiceFunctionChain;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sfc.rev140701.service.function.chain.grouping.service.function.chain.SfcServiceFunction;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+/**
+ * SFC caching layer for datastore access. Used for allowing creation-time validation
+ * of SF paths (during which the write transaction is kept open) to be performed
+ * without datastore accesses in most cases
+ * @author Diego Granados <diego.jesus.granados.lopez@ericsson.com>
+ */
+public class SfcDatastoreCache {
+
+    /**
+     * This cache stores the relationship between SFs and SF types
+     */
+    public static LoadingCache<SfName, String> sfToSfTypeCache = CacheBuilder.newBuilder()
+            .maximumSize(500)
+            .build(new CacheLoader<SfName, String>() {
+                @Override
+                public String load(SfName key) {
+                    ServiceFunction sf = SfcProviderServiceFunctionAPI.readServiceFunction(key);
+                    if (sf == null) {
+                        return null;
+                    }
+                    return sf.getType().getValue().toString();
+                }
+            });
+
+    /**
+     * This cache holds the relation between SF chains and the list of SF types for the chain
+     */
+    public static LoadingCache<SfcName, List<String>> sfChainToSfTypeList = CacheBuilder.newBuilder()
+            .maximumSize(500)
+            .build(new CacheLoader<SfcName, List<String>>() {
+                @Override
+                public List<String> load(SfcName key) {
+                    ServiceFunctionChain serviceFunctionChain = SfcProviderServiceChainAPI.readServiceFunctionChain(key);
+                    if (serviceFunctionChain == null) {
+                        return null;
+                    }
+                    List<String> serviceFunctionTypesForChain = new ArrayList<String>();
+                    //SfcServiceFunction sfcSf = null;
+                    for (SfcServiceFunction sfcSf: serviceFunctionChain.getSfcServiceFunction()) {
+                        serviceFunctionTypesForChain.add(sfcSf.getType().getValue());
+                    }
+                    return serviceFunctionTypesForChain;
+                }
+            });
+}

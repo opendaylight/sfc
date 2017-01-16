caa282a7055d47cb66504781ac12d0eb575964af
Diego Granados <diego.jesus.granados.lopez@ericsson.com>
2017-01-16 11:58:39 +0100

    Bug 3712: SFP validation of SF types at creation time
    This commit makes use of DOMDataTreeCommitCohorts (callbacks
    that can be registered in order to participate in commit
    validation) in order to make sure that newly-added datastore
    SFPs are consistent with previously defined SFCs and SFs.
    Specifically, it is checked that the types for the SFs the
    SFP references are consistent with the SF types that the
    SFC specifies.
    
    Since the datastore commit transaction is kept open during
    validation, this validation must be as fast as possible.
    Controller guidelines about not opening new datastore
    reads in order to validate a DS write have been followed
    to the maximum extent; for this reason, a datastore cache
    has been deployed to cover both SFs and SFCs. On systems
    with less than 500 SFs & SFCs deployed, caches will provide
    total coverage (i.e. validation will never need to access
    datastore)
    
    Testing: manually tested (complex interactions between
    controller-mdsal-scf are too hard to simulate). Proper CSIT
    tests will be delivered to the sfc suites in the integration
    project.
    
    Change-Id: I5886d0be9191043aa8227c242a1498e518b31a38
    Signed-off-by: Diego Granados <diego.jesus.granados.lopez@ericsson.com>

---
+++ sfc-provider/src/main/java/org/opendaylight/sfc/provider/listeners/ServiceFunctionChainListener.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2017 Ericsson Spain and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.sfc.provider.listeners;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.sfc.provider.validators.util.SfcDatastoreCache;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sfc.rev140701.ServiceFunctionChains;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sfc.rev140701.service.function.chain.grouping.ServiceFunctionChain;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sfc.rev140701.service.function.chain.grouping.service.function.chain.SfcServiceFunction;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class listens to changes (addition, update, removal) in Service
+ * Functions chains taking the appropriate actions.
+ */
+public class ServiceFunctionChainListener extends AbstractDataTreeChangeListener<ServiceFunctionChain> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ServiceFunctionChainListener.class);
+
+    private final DataBroker dataBroker;
+
+    private ListenerRegistration<ServiceFunctionChainListener> listenerRegistration;
+
+    public ServiceFunctionChainListener(final DataBroker dataBroker) {
+        this.dataBroker = dataBroker;
+    }
+
+    private void registerListeners() {
+        final DataTreeIdentifier<ServiceFunctionChain> treeId = new DataTreeIdentifier<ServiceFunctionChain>(LogicalDatastoreType.CONFIGURATION,
+                InstanceIdentifier.create(ServiceFunctionChains.class).child(ServiceFunctionChain.class));
+        listenerRegistration = dataBroker.registerDataTreeChangeListener(treeId, this);
+
+    }
+
+    public void init() {
+        LOG.debug("ServiceFunctionChainListener:Initializing...");
+        registerListeners();
+    }
+
+    @Override
+    public void close() throws Exception {
+        LOG.debug("Closing listener...");
+        if (listenerRegistration != null) {
+            listenerRegistration.close();
+        }
+    }
+
+    @Override
+    public void add(ServiceFunctionChain serviceFunctionChain) {
+        if (serviceFunctionChain != null) {
+            LOG.debug("add:starting..(new sfc name: {})", serviceFunctionChain.getName());
+            List<String> serviceFunctionTypesForChain = new ArrayList<String>();
+            for (SfcServiceFunction sfcSf: serviceFunctionChain.getSfcServiceFunction()) {
+                LOG.debug("add:new sfc sf found; name={}, type={})", sfcSf.getName(), sfcSf.getType().getValue());
+                serviceFunctionTypesForChain.add(sfcSf.getType().getValue());
+            }
+            SfcDatastoreCache.getSfChainToSfTypeList().put(serviceFunctionChain.getName(), serviceFunctionTypesForChain);
+        }
+    }
+
+    @Override
+    public void remove(ServiceFunctionChain serviceFunctionChain) {
+        if (serviceFunctionChain != null) {
+            LOG.debug("remove: Deleting Service Function chain: {}", serviceFunctionChain.getName());
+            SfcDatastoreCache.getSfChainToSfTypeList().invalidate(serviceFunctionChain.getName());
+        }
+    }
+
+    @Override
+    protected void update(ServiceFunctionChain originalServiceFunctionChain, ServiceFunctionChain updatedServiceFunctionChain) {
+        if (originalServiceFunctionChain != null && updatedServiceFunctionChain != null) {
+            LOG.debug("update:Updating Service Function chain: {}", originalServiceFunctionChain.getName());
+            SfcDatastoreCache.getSfChainToSfTypeList().invalidate(originalServiceFunctionChain.getName());
+            add(updatedServiceFunctionChain);
+        }
+    }
+}
---
+++ sfc-provider/src/main/java/org/opendaylight/sfc/provider/validators/ServiceFunctionPathValidator.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) 2017 Ericsson Spain and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.sfc.provider.validators;
+
+import java.util.List;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataBroker;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeCommitCohortRegistry;
+import org.opendaylight.sfc.provider.validators.util.ValidationConstants;
+import org.opendaylight.sfc.provider.validators.util.DataValidationFailedWithMessageException;
+import org.opendaylight.sfc.provider.validators.util.SfcDatastoreCache;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.common.rev151017.SfName;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sfp.rev140701.service.function.paths.ServiceFunctionPath;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class performs pre-commit validation for new / modified SFPs in
+ * order to guarantee that the SFs it specifies are consistent with
+ * the types specified in the associated SF chain
+ * @author Diego Granados (diego.jesus.granados.lopez@ericsson.com)
+ */
+public class ServiceFunctionPathValidator {
+    private static final Logger LOG = LoggerFactory.getLogger(ServiceFunctionPathValidator.class);
+
+    private final DOMDataTreeCommitCohortRegistry registry;
+
+    public ServiceFunctionPathValidator(final DOMDataBroker domDataBroker) {
+        this.registry = (DOMDataTreeCommitCohortRegistry)domDataBroker
+                .getSupportedExtensions()
+                .get(org.opendaylight.controller.md.sal.dom.api.DOMDataTreeCommitCohortRegistry.class);
+    }
+
+    public void init() {
+        LOG.debug("ServiceFunctionPathValidator:Initializing...");
+        registerValidationCohorts();
+    }
+
+    private void registerValidationCohorts() {
+
+            ServiceFunctionPathCohort myCohort = new ServiceFunctionPathCohort(this);
+
+            LOG.debug("registerValidationCohorts: sfp cohort created");
+            registry.registerCommitCohort(ValidationConstants.SFP_ID, myCohort);
+            LOG.info("registerValidationCohorts:initialized. registered cohort: {}", myCohort);
+
+    }
+
+    public void close() throws Exception {
+        LOG.debug("close(): closing sfp validator ...");
+    }
+
+    /**
+     * Performs validation of a service function path
+     * @param serviceFunctionPath a candidate SFP that is being added / updated in a currently open transaction
+     * @return true when validation is passed (i.e. when the SFs contained in the SFP are coherent (type-wise)
+     *         with the type definitions in the associated SFC; false afterwards)
+     * @throws DataValidationFailedWithMessageException when validation cannot be performed because some of the
+     *         referenced SFs / SFCs do not exist
+     */
+    protected boolean validateServiceFunctionPath(
+            ServiceFunctionPath serviceFunctionPath) throws DataValidationFailedWithMessageException {
+        if (serviceFunctionPath != null) {
+            LOG.debug("ServiceFunctionPathListener:validateServiceFunctionPath:starting..(new sfc name: {})",
+                    serviceFunctionPath.getName());
+
+            if (serviceFunctionPath.getServicePathHop() == null) {
+                // no hops defined in SFP -> nothing to validate
+                LOG.info("ServiceFunctionPathListener:validateServiceFunctionPath:"
+                        + "SFP without explicit hop definition -> validation not required");
+                return true;
+            }
+
+            // 1. Get the size of the list of SF names in the SFP
+            int numberOfSpecifiedSFs = serviceFunctionPath.getServicePathHop().size();
+
+            // 2. Get the SF types referenced in the chain (fail if can't find the chain)
+            List<String> sfChainTypes;
+            try {
+                sfChainTypes = SfcDatastoreCache.getSfChainToSfTypeList()
+                        .get(serviceFunctionPath.getServiceChainName());
+            } catch (Exception e) {
+                throw ValidationConstants.SFP_FAILED_CAN_COMMIT_EXCEPTION_SFC_MISSING;
+            }
+
+            // 3. Referential integrity (SFC)
+            if (sfChainTypes == null || sfChainTypes.isEmpty()) {
+                LOG.error("validateServiceFunctionPath:: ERROR! (no sf chains defined!)");
+                throw ValidationConstants.SFP_FAILED_CAN_COMMIT_EXCEPTION_SFC_MISSING;
+            }
+
+            // 4. Correct number of values in SFC types, SFs in the SFP
+            LOG.debug("validateServiceFunctionPath:retrieved SFC {} for SFP {}); they have {}, {} elements respectively",
+                    serviceFunctionPath.getServiceChainName().getValue(),
+                    serviceFunctionPath.getName().getValue(),
+                    sfChainTypes.size(), numberOfSpecifiedSFs);
+
+            // A chain can have more elements than the list of service functions, but not the other way around
+            // (A SFP can choose to set all or only some of the SFs in the path)
+            if (sfChainTypes.size() < numberOfSpecifiedSFs) {
+                LOG.error(
+                        "validateServiceFunctionPath: ERROR! (incorrect chain-path list sizes [chain={}, path={}])",
+                        sfChainTypes.size(),
+                        numberOfSpecifiedSFs);
+                return false;
+            }
+
+            // 5. SF type matching
+            boolean errorFound = false;
+            for (int i = 0; i < numberOfSpecifiedSFs; i++) {
+                SfName sfName = serviceFunctionPath.getServicePathHop().get(i).getServiceFunctionName();
+                if (sfName == null) {
+                    continue;
+                }
+                String sfChainTypeName = sfChainTypes.get(serviceFunctionPath.getServicePathHop().get(i).getHopNumber());
+
+                String sfTypeNameFromSFP;
+                try {
+                    sfTypeNameFromSFP = SfcDatastoreCache.getSfToSfTypeCache()
+                            .get(sfName);
+                } catch (Exception e) {
+                    throw ValidationConstants.SFP_FAILED_CAN_COMMIT_EXCEPTION_SF_MISSING;
+                }
+
+                if (!sfChainTypeName.equals(sfTypeNameFromSFP)) {
+                    LOG.error(
+                            "validateServiceFunctionPath: error on SFP validation! element with index {} is not of the correct type [{}/{}]",
+                            i, sfChainTypeName, sfTypeNameFromSFP);
+                    errorFound = true;
+                    break;
+                }
+            }
+            if (!errorFound) {
+                LOG.info("validateServiceFunctionPath:SFP validation passed!");
+                return true;
+            }
+        }
+        return false;
+    }
+}

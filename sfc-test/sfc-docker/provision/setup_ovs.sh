a2b3b64f94ad925051ce93661039bbf34116af38
eceabpe <alvaro.berlanga@ericsson.com>
2017-01-17 20:08:28 +0100

    DO NOT MERGE - Docker environment + SFC e2e traffic tests
    -Add some updates to 'dovs' utility (that simulates neutron networks)
    -Add tools to test end to end traffic for Service Function Chaining
    with NSH in Logical-SFF configurations
    -Add a Simple Service Function HTTP Header Enrichment (with it own tests)
    -Add brief documentation (README.asciidoc files)
    -Add Test cases see below
    
    Example demo for testing the Logical SFF end-to-end SFC traffic
    -Start the Controller (e.g. that run on 172.28.128.3)
    -Start the 'dovs' environment:
    cd sfc-test/sfc-docker/
    vagrant destroy -f && vagrant up && vagrant ssh
    > cd /vagrant/sf_hhe/logical_sff
    
    A. SFs in a DIFFERENT subnets
    -1 Test traffic in two Service Function Chains
    > sudo ./test_case_01_traffic_different_subnet.sh -o 172.28.128.3
    -2 Test traffic in one Service Function Chain
     + Test traffic after moving one VM to other compute node
    > sudo ./test_case_02_move_vm_different_subnet.sh -o 172.28.128.3
    
    B. SFs in the SAME subnet
    -3 Test traffic in two Service Function Chains
    > sudo ./test_case_03_traffic_same_subnet.sh -o 172.28.128.3
    -4 Test traffic in one Service Function Chain
     + Test traffic after moving one VM to other compute node
    > sudo ./test_case_04_move_vm_same_subnet.sh -o 172.28.128.3
    
    - Clean the network topology in 'dovs'
    > sudo dovs clean --odl 172.28.128.3
    
    Executing tests from A and B requires restart ODL Controller and dovs
    
    -There are many logs that will be stored in ./logs
    (e.g. To see TCP packages in client of first Chain for test case 01)
     > tcpdump -qns 0 -A -r ./logs/client1_tcpdump.pcap tcp )
    
    - Check SFC configuration
    > ./GetSfcConfig.py -v -ip 172.28.128.3
    
    
    Do not merge. It depends on commit with Subject:
    "docker environment to test sfc"
    
    Change-Id: Idf02270444eb52597a4921563374e6876b5959cf
    Signed-off-by: eceabpe <alvaro.berlanga@ericsson.com>

---
+++ sfc-test/sfc-docker/provision/setup_ovs.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+OVS_VERSION="2.5.90"
+
+install_kernel_devel() {
+  if [ ! yum -q info kernel-devel-$(uname -r) &>/dev/null ]
+  then
+    CENTOS_VERSION="$(grep -Po '\b\d+\.[\d.]+\b' /etc/centos-release)/x86_64"
+    PCKG_URL="http://mirror.centos.org/centos/${CENTOS_VERSION}"
+    yum localinstall ${PCKG_URL}/{os,updates}/Packages/kernel-devel-$(uname -r).rpm
+  else
+    yum install kernel-devel-$(uname -r) -y
+  fi
+}
+
+install_build_requirements() {
+  install_kernel_devel
+  yum install -y git automake libtool rpm-build openssl-devel \
+      python-zope-interface python-twisted-core               \
+      PyQt4 python-six desktop-file-utils groff graphviz      \
+      selinux-policy-devel python
+}
+
+install_requirements() {
+  yum install -y python yum-plugin-versionlock
+  yum versionlock kernel-$(uname -r) kernel-devel-$(uname -r)
+}
+
+build_ovs() {
+  [ -e ovs_packages/openvswitch-${OVS_VERSION}-1.el7.centos.x86_64.rpm ]           \
+    && [ -e ovs_packages/openvswitch-kmod-${OVS_VERSION}-1.el7.centos.x86_64.rpm ] \
+    && return 0
+
+  install_build_requirements
+
+  git clone https://github.com/openvswitch/ovs.git
+  git clone https://github.com/yyang13/ovs_nsh_patches.git
+
+  pushd ovs_nsh_patches && git reset --hard HEAD && popd
+  pushd ovs
+  git reset --hard 7d433ae57ebb90cd68e8fa948a096f619ac4e2d8
+  cp ../ovs_nsh_patches/*.patch ./
+  git apply *.patch
+
+  ./boot.sh
+  ./configure --disable-shared
+  make
+  make dist
+  make DESTDIR="${PWD}/../install/openvswitch-${OVS_VERSION}" install
+
+  popd && tar xvzf ovs/openvswitch-${OVS_VERSION}.tar.gz
+  mkdir -p ~/rpmbuild/SOURCES/
+  cp ovs/openvswitch-${OVS_VERSION}.tar.gz cp ~/rpmbuild/SOURCES/
+  pushd openvswitch-${OVS_VERSION}
+  rpmbuild -bb --without check --without libcapng rhel/openvswitch-fedora.spec
+  rpmbuild -bb --without check -D "kversion $(uname -r)" rhel/openvswitch-kmod-fedora.spec
+
+  popd && mkdir -p ovs_packages
+  cp ~/rpmbuild/RPMS/x86_64/openvswitch*.rpm ovs_packages/
+  tar cvzf ovs_packages/openvswitch-${OVS_VERSION}.tar.gz -C install .
+}
+
+install_ovs() {
+  install_requirements
+  yum localinstall ovs_packages/openvswitch-kmod-${OVS_VERSION}-1.el7.centos.x86_64.rpm -y
+  yum localinstall ovs_packages/openvswitch-${OVS_VERSION}-1.el7.centos.x86_64.rpm -y
+  systemctl enable openvswitch
+  systemctl start openvswitch
+}
+
+mkdir -p build && pushd build
+build_ovs
+install_ovs
+

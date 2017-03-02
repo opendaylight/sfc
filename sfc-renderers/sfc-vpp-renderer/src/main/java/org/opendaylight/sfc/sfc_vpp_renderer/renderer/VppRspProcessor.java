f2e7a8b15b1b9387fffb9e2baeb844075dc3938e
Faseela K <faseela.k@ericsson.com>
2017-03-02 13:32:16 +0530

    sfc-vpp-renderer - enforce checkstyle
    Change-Id: I39c179bc22651087f3dd526adbbecece293d84c7
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

--- sfc-renderers/sfc-vpp-renderer/src/main/java/org/opendaylight/sfc/sfc_vpp_renderer/renderer/VppRspProcessor.java
+++ sfc-renderers/sfc-vpp-renderer/src/main/java/org/opendaylight/sfc/sfc_vpp_renderer/renderer/VppRspProcessor.java
@@ -18,9 +18,9 @@ import org.opendaylight.sfc.provider.api.SfcProviderServiceFunctionAPI;
 import org.opendaylight.sfc.util.vpp.SfcVppUtils;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.common.rev151017.SfName;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.common.rev151017.SffName;
-import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.functions.ServiceFunction;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.rsp.rev140701.rendered.service.paths.RenderedServicePath;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.rsp.rev140701.rendered.service.paths.rendered.service.path.RenderedServicePathHop;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.functions.ServiceFunction;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
 
 import org.slf4j.Logger;
@@ -31,8 +31,8 @@ public class VppRspProcessor {
     private static final Logger LOG = LoggerFactory.getLogger(VppRspProcessor.class);
 
     private final VppNodeManager nodeManager;
-    private static final String SFC_BD_NAME = new String("SFCVPP");
-    private static final String DUMMY_BD_NAME = new String("SFCDUMMY");
+    private static final String SFC_BD_NAME = "SFCVPP";
+    private static final String DUMMY_BD_NAME = "SFCDUMMY";
     private final Map<String, String> bridgeDomainCreated = new HashMap<>();
 
     public VppRspProcessor(VppNodeManager nodeManager) {
@@ -54,8 +54,8 @@ public class VppRspProcessor {
         IpAddress preLocalIp = null;
         boolean ret = false;
 
-        if (renderedServicePath.getRenderedServicePathHop() == null ||
-                renderedServicePath.getRenderedServicePathHop().isEmpty()) {
+        if (renderedServicePath.getRenderedServicePathHop() == null
+            || renderedServicePath.getRenderedServicePathHop().isEmpty()) {
             LOG.warn("Rendered path {} does not contain any hop", renderedServicePath.getName().getValue());
             return;
         }
@@ -63,15 +63,15 @@ public class VppRspProcessor {
                 .iterator();
 
         while (rspHopIterator.hasNext()) {
-            RenderedServicePathHop hop = rspHopIterator.next();
             previousSffName = currentSffName;
             previousMountPoint = currentMountpoint;
             preLocalIp = localIp;
+            RenderedServicePathHop hop = rspHopIterator.next();
             currentSffName = hop.getServiceFunctionForwarder();
             currentMountpoint = SfcVppUtils.getSffMountpoint(this.nodeManager.getMountPointService(), currentSffName);
             if (currentMountpoint == null) {
-                LOG.error("Resolving of RSP {} failed in updateRsp, mountpoint for SFF {} is null", renderedServicePath.getName()
-                    .getValue(), currentSffName.getValue());
+                LOG.error("Resolving of RSP {} failed in updateRsp, mountpoint for SFF {} is null",
+                    renderedServicePath.getName().getValue(), currentSffName.getValue());
                 return;
             }
 
@@ -84,7 +84,8 @@ public class VppRspProcessor {
             }
             ipList = SfcVppUtils.getSffSfIps(currentSffName, sfName);
             if (ipList == null || ipList.isEmpty()) {
-                LOG.error("failed to get IP for DPL for SFF {} in RSP {}", currentSffName.getValue(), renderedServicePath.getName().getValue());
+                LOG.error("failed to get IP for DPL for SFF {} in RSP {}", currentSffName.getValue(),
+                    renderedServicePath.getName().getValue());
                 return;
             }
             localIp = ipList.get(0);
@@ -94,22 +95,27 @@ public class VppRspProcessor {
             if (!bridgeDomainCreated.containsKey(currentSffName.getValue())) {
                 SfcVppUtils.addDummyBridgeDomain(currentMountpoint, DUMMY_BD_NAME, currentSffName.getValue());
                 SfcVppUtils.addDummyNshEntry(currentMountpoint, 0L, (short)1, currentSffName.getValue());
-                SfcVppUtils.addDummyNshMap(currentMountpoint, 0L, (short)1, 0L, (short)1, new String("local0"), currentSffName.getValue());
+                SfcVppUtils.addDummyNshMap(currentMountpoint, 0L, (short)1, 0L, (short)1, "local0",
+                    currentSffName.getValue());
                 SfcVppUtils.addBridgeDomain(currentMountpoint, SFC_BD_NAME, currentSffName.getValue());
                 bridgeDomainCreated.put(currentSffName.getValue(), SFC_BD_NAME);
             }
 
-            ret = SfcVppUtils.configureVxlanGpeNsh(currentMountpoint, currentSffName, SFC_BD_NAME, localIp, remoteIp, pathId, serviceIndex);
+            ret = SfcVppUtils.configureVxlanGpeNsh(currentMountpoint, currentSffName, SFC_BD_NAME, localIp, remoteIp,
+                pathId, serviceIndex);
             if (!ret) {
-                LOG.error("failed to configure VxLAN-gpe and NSH for RSP {} in SFF {} for SF hop", renderedServicePath.getName().getValue(), currentSffName.getValue());
+                LOG.error("failed to configure VxLAN-gpe and NSH for RSP {} in SFF {} for SF hop",
+                    renderedServicePath.getName().getValue(), currentSffName.getValue());
                 return;
             }
 
             //previous SFF <-> current SFF
             if (previousSffName != null && !previousSffName.equals(currentSffName)) {
-                ret = SfcVppUtils.configureVxlanGpeNsh(previousMountPoint, previousSffName, SFC_BD_NAME, preLocalIp, localIp, pathId, serviceIndex);
+                ret = SfcVppUtils.configureVxlanGpeNsh(previousMountPoint, previousSffName, SFC_BD_NAME, preLocalIp,
+                    localIp, pathId, serviceIndex);
                 if (!ret) {
-                    LOG.error("failed to configure VxLAN-gpe and NSH for RSP {} in SFF {} for SFF hop", renderedServicePath.getName().getValue(), previousSffName.getValue());
+                    LOG.error("failed to configure VxLAN-gpe and NSH for RSP {} in SFF {} for SFF hop",
+                        renderedServicePath.getName().getValue(), previousSffName.getValue());
                     return;
                 }
             }
@@ -133,8 +139,8 @@ public class VppRspProcessor {
         IpAddress remoteIp;
         IpAddress preLocalIp = null;
 
-        if (renderedServicePath.getRenderedServicePathHop() == null ||
-                renderedServicePath.getRenderedServicePathHop().isEmpty()) {
+        if (renderedServicePath.getRenderedServicePathHop() == null
+            || renderedServicePath.getRenderedServicePathHop().isEmpty()) {
             LOG.warn("Rendered path {} does not contain any hop", renderedServicePath.getName().getValue());
             return;
         }
@@ -142,15 +148,15 @@ public class VppRspProcessor {
                 .iterator();
 
         while (rspHopIterator.hasNext()) {
-            RenderedServicePathHop hop = rspHopIterator.next();
             previousSffName = currentSffName;
             previousMountPoint = currentMountpoint;
             preLocalIp = localIp;
+            RenderedServicePathHop hop = rspHopIterator.next();
             currentSffName = hop.getServiceFunctionForwarder();
             currentMountpoint = SfcVppUtils.getSffMountpoint(this.nodeManager.getMountPointService(), currentSffName);
             if (currentMountpoint == null) {
-                LOG.error("Resolving of RSP {} failed in deleteRsp, mountpoint for SFF {} is null", renderedServicePath.getName()
-                    .getValue(), currentSffName.getValue());
+                LOG.error("Resolving of RSP {} failed in deleteRsp, mountpoint for SFF {} is null",
+                    renderedServicePath.getName().getValue(), currentSffName.getValue());
                 return;
             }
 
@@ -163,23 +169,28 @@ public class VppRspProcessor {
             }
             ipList = SfcVppUtils.getSffSfIps(currentSffName, sfName);
             if (ipList == null || ipList.isEmpty()) {
-                LOG.error("failed to get IP for DPL for SFF {} in RSP {}", currentSffName.getValue(), renderedServicePath.getName().getValue());
+                LOG.error("failed to get IP for DPL for SFF {} in RSP {}", currentSffName.getValue(),
+                    renderedServicePath.getName().getValue());
                 return;
             }
             localIp = ipList.get(0);
             remoteIp = ipList.get(1);
 
-            ret = SfcVppUtils.removeVxlanGpeNsh(currentMountpoint, currentSffName, localIp, remoteIp, pathId, serviceIndex);
+            ret = SfcVppUtils.removeVxlanGpeNsh(currentMountpoint, currentSffName, localIp, remoteIp, pathId,
+                serviceIndex);
             if (!ret) {
-                LOG.error("failed to remove VxLAN-gpe and NSH for RSP {} in SFF {}", renderedServicePath.getName().getValue(), currentSffName.getValue());
+                LOG.error("failed to remove VxLAN-gpe and NSH for RSP {} in SFF {}", renderedServicePath.getName()
+                    .getValue(), currentSffName.getValue());
                 return;
             }
 
             //previous SFF <-> current SFF
             if (previousSffName != null && !previousSffName.equals(currentSffName)) {
-                ret = SfcVppUtils.removeVxlanGpeNsh(previousMountPoint, previousSffName, preLocalIp, localIp, pathId, serviceIndex);
+                ret = SfcVppUtils.removeVxlanGpeNsh(previousMountPoint, previousSffName, preLocalIp, localIp, pathId,
+                    serviceIndex);
                 if (!ret) {
-                    LOG.error("failed to remove VxLAN-gpe and NSH for RSP {} in SFF {}", renderedServicePath.getName().getValue(), previousSffName.getValue());
+                    LOG.error("failed to remove VxLAN-gpe and NSH for RSP {} in SFF {}",
+                        renderedServicePath.getName().getValue(), previousSffName.getValue());
                     return;
                 }
             }

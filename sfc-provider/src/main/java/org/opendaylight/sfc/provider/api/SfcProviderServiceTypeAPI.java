909423563dd4a5738d4ce7e01e6a1c6e4066cb93
David Suarez <david.suarez.fuentes@gmail.com>
2017-08-03 14:45:42 +0200

    Add a readAll method to get all the Service Function Types.
    Add a readAll method to get all the Service Function Types.
    
    Change-Id: I0028800cc3a87b5eec2102604420da0941124404
    Signed-off-by: David Suarez <david.suarez.fuentes@gmail.com>

--- sfc-provider/src/main/java/org/opendaylight/sfc/provider/api/SfcProviderServiceTypeAPI.java
+++ sfc-provider/src/main/java/org/opendaylight/sfc/provider/api/SfcProviderServiceTypeAPI.java
@@ -125,6 +125,12 @@ public final class SfcProviderServiceTypeAPI {
         return sft;
     }
 
+    public static ServiceFunctionTypes readAllServiceFunctionTypes() {
+        InstanceIdentifier<ServiceFunctionTypes> ids = InstanceIdentifier.builder(ServiceFunctionTypes.class).build();
+        return SfcDataStoreAPI.readTransactionAPI(ids, LogicalDatastoreType.CONFIGURATION);
+    }
+
+
     /**
      * This method is used to delete a Service Function entry from the Service
      * Function Type list.

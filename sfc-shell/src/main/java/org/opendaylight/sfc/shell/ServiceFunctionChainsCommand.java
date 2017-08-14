6b088c4ea3d663fec6650501c5862a18c083afa1
David Suarez <david.suarez.fuentes@gmail.com>
2017-08-14 08:53:17 +0200

    Add SFC shell's command to show Service Function Chains
    Add SFC shell's command to show Service Function Chains, as described in
    [1],
    
    [1] https://git.opendaylight.org/gerrit/#/c/61059/
    
    Change-Id: Iceadac2c1a39b4c93e950503d5e23924e07e3f39
    Signed-off-by: David Suarez <david.suarez.fuentes@gmail.com>

---
+++ sfc-shell/src/main/java/org/opendaylight/sfc/shell/ServiceFunctionChainsCommand.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2017 Ericsson S.A. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.sfc.shell;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.Option;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.apache.karaf.shell.support.table.Col;
+import org.apache.karaf.shell.support.table.ShellTable;
+import org.opendaylight.sfc.provider.api.SfcProviderServiceChainAPI;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.common.rev151017.SfcName;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sfc.rev140701.ServiceFunctionChains;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sfc.rev140701.service.function.chain.grouping.ServiceFunctionChain;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Karaf command to show the Service Function provisioned.
+ *
+ * @author David Suárez (david.suarez.fuentes@gmail.com)
+ *
+ */
+@Service
+@Command(scope = "sfc", name = "sc-list", description = "Show the provisioned Service Function Chains")
+public class ServiceFunctionChainsCommand extends AbstractCommand {
+
+    private static final String SEPARATOR = ", ";
+
+    @Option(name = "-name", aliases = {
+            "--name" }, description = "Name of the Service Function Chain", required = false, multiValued = false)
+    private String name;
+
+    private final ShellTable table;
+
+    private static final Logger LOG = LoggerFactory.getLogger(ServiceFunctionChainsCommand.class);
+
+    public ServiceFunctionChainsCommand() {
+        table = new ShellTable();
+        table.column(new Col("Name"));
+        table.column(new Col("SF Types"));
+    }
+
+    @Override
+    public Object execute() throws Exception {
+        LOG.debug("Service Function Chain name: {}", name);
+
+        if (name != null) {
+            renderContent(SfcProviderServiceChainAPI.readServiceFunctionChain(new SfcName(name)));
+        } else {
+            LOG.debug("Getting the list of Service Function Chains");
+            ServiceFunctionChains sfcs = SfcProviderServiceChainAPI.readAllServiceFunctionChains();
+            if (sfcs != null) {
+                List<ServiceFunctionChain> serviceFunctionChains = sfcs.getServiceFunctionChain();
+                serviceFunctionChains.forEach(this::renderContent);
+            }
+        }
+        table.print(getConsole());
+        return null;
+    }
+
+    private void renderContent(ServiceFunctionChain serviceFunctionChain) {
+        if (serviceFunctionChain != null) {
+            LOG.debug("Service Function Chain data: {}", serviceFunctionChain);
+            table.addRow().addContent(serviceFunctionChain.getName().getValue(),
+                    serviceFunctionChain.getSfcServiceFunction() == null ? NO_VALUE
+                            : serviceFunctionChain.getSfcServiceFunction().stream().map(sf -> sf.getType().getValue())
+                                    .collect(Collectors.joining(SEPARATOR)));
+        }
+    }
+}

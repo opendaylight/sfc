2db2da7700557b01238ba2bc17f1e7c52bbe9f1f
David Suarez <david.suarez.fuentes@gmail.com>
2017-10-25 19:36:25 +0200

    Migrate ServiceFunctionForwarderListener to Genius' Listeners
    Change-Id: Ie09ee0efe4fde576b366a95a85ff41cd55f87488
    Signed-off-by: David Suarez <david.suarez.fuentes@gmail.com>

--- sfc-provider/src/main/java/org/opendaylight/sfc/provider/listeners/ServiceFunctionForwarderListener.java
+++ sfc-provider/src/main/java/org/opendaylight/sfc/provider/listeners/ServiceFunctionForwarderListener.java
@@ -11,11 +11,12 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
-import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.datastoreutils.listeners.AbstractSyncDataTreeChangeListener;
 import org.opendaylight.sfc.provider.api.SfcProviderRenderedPathAPI;
 import org.opendaylight.sfc.provider.api.SfcProviderServiceForwarderAPI;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.common.rev151017.RspName;
@@ -28,7 +29,6 @@ import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev1407
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.service.function.forwarders.ServiceFunctionForwarder;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.service.function.forwarders.service.function.forwarder.ServiceFunctionDictionary;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
-import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,43 +38,25 @@ import org.slf4j.LoggerFactory;
  * Forwarders taking the appropriate actions.
  *
  * @author David Suárez (david.suarez.fuentes@gmail.com)
- *
  */
 @Singleton
-public class ServiceFunctionForwarderListener extends AbstractDataTreeChangeListener<ServiceFunctionForwarder> {
+public class ServiceFunctionForwarderListener extends AbstractSyncDataTreeChangeListener<ServiceFunctionForwarder> {
+
     private static final Logger LOG = LoggerFactory.getLogger(ServiceFunctionForwarderListener.class);
 
-    private final DataBroker dataBroker;
-    private ListenerRegistration<ServiceFunctionForwarderListener> listenerRegistration;
-
     @Inject
-    public ServiceFunctionForwarderListener(final DataBroker dataBroker) {
-        this.dataBroker = dataBroker;
-        registerListeners();
-    }
-
-    private void registerListeners() {
-        final DataTreeIdentifier<ServiceFunctionForwarder> treeId = new DataTreeIdentifier<>(
-                LogicalDatastoreType.CONFIGURATION,
-                InstanceIdentifier.create(ServiceFunctionForwarders.class).child(ServiceFunctionForwarder.class));
-        listenerRegistration = dataBroker.registerDataTreeChangeListener(treeId, this);
+    public ServiceFunctionForwarderListener(DataBroker dataBroker) {
+        super(dataBroker, LogicalDatastoreType.CONFIGURATION,
+              InstanceIdentifier.create(ServiceFunctionForwarders.class).child(ServiceFunctionForwarder.class));
     }
 
     @Override
-    public void close() throws Exception {
-        LOG.debug("Closing listener...");
-        if (listenerRegistration != null) {
-            listenerRegistration.close();
-        }
-    }
-
-    @Override
-    protected void add(ServiceFunctionForwarder serviceFunctionForwarder) {
+    public void add(@Nonnull ServiceFunctionForwarder serviceFunctionForwarder) {
         LOG.info("Adding Service Function Forwarder: {}", serviceFunctionForwarder.getName());
     }
 
     @Override
-    protected void remove(ServiceFunctionForwarder serviceFunctionForwarder) {
+    public void remove(@Nonnull ServiceFunctionForwarder serviceFunctionForwarder) {
         SffName sffName = serviceFunctionForwarder.getName();
         // Get RSPs of SFF
         LOG.debug("Deleting Service Function Forwarder {}", sffName);
@@ -84,8 +66,8 @@ public class ServiceFunctionForwarderListener extends AbstractDataTreeChangeList
     }
 
     @Override
-    protected void update(ServiceFunctionForwarder originalServiceFunctionForwarder,
-            ServiceFunctionForwarder updatedServiceFunctionForwarder) {
+    public void update(@Nonnull ServiceFunctionForwarder originalServiceFunctionForwarder,
+                       ServiceFunctionForwarder updatedServiceFunctionForwarder) {
         LOG.debug("Updating Service Function Forwarder: {}", originalServiceFunctionForwarder.getName());
         List<RspName> rspNames = findAffectedRsp(originalServiceFunctionForwarder, updatedServiceFunctionForwarder);
         LOG.debug("Deleting Rendered Service Paths {}", rspNames);
@@ -95,14 +77,11 @@ public class ServiceFunctionForwarderListener extends AbstractDataTreeChangeList
     /**
      * Obtains the list of RSP affected by a change on the SFF.
      *
-     * @param originalSff
-     *            the original SFF.
-     * @param updatedSff
-     *            the updated SFF.
+     * @param originalSff the original SFF.
+     * @param updatedSff  the updated SFF.
      * @return a list of {@link RspName} of the affected RSP.
      */
     private List<RspName> findAffectedRsp(ServiceFunctionForwarder originalSff, ServiceFunctionForwarder updatedSff) {
-
         SffName sffName = originalSff.getName();
         List<RspName> rspNames = SfcProviderServiceForwarderAPI.readRspNamesFromSffState(sffName);
 
@@ -126,31 +105,28 @@ public class ServiceFunctionForwarderListener extends AbstractDataTreeChangeList
         // TODO Current data model does not allow to know which DPL is used on a RSP
         List<SffDataPlaneLocator> originalLocators = originalSff.getSffDataPlaneLocator();
         List<SffDataPlaneLocator> updatedLocators = updatedSff.getSffDataPlaneLocator();
-        boolean isAnyLocatorChanged = originalLocators != null
-                && !originalLocators.isEmpty()
-                && (updatedLocators == null || !updatedLocators.containsAll(originalLocators));
+        boolean isAnyLocatorChanged = originalLocators != null && !originalLocators.isEmpty() && (
+                updatedLocators == null || !updatedLocators.containsAll(originalLocators));
         if (isAnyLocatorChanged) {
             LOG.debug("SFF locators changed: original {} updated {}", originalLocators, updatedLocators);
             return rspNames;
         }
 
         // If a dictionary changed, any RSP making use of it is affected
-        List<ServiceFunctionDictionary> originalDictList = originalSff.getServiceFunctionDictionary() != null
-                ? originalSff.getServiceFunctionDictionary()
-                : Collections.emptyList();
-        List<ServiceFunctionDictionary> updatedDictList = updatedSff.getServiceFunctionDictionary() != null
-                ? updatedSff.getServiceFunctionDictionary()
-                : Collections.emptyList();
-        List<ServiceFunctionDictionary> removedDictList = updatedDictList.isEmpty()
-                ? originalDictList
-                : originalDictList.stream().filter(d -> !updatedDictList.contains(d)).collect(Collectors.toList());
+        List<ServiceFunctionDictionary> originalDictList =
+                originalSff.getServiceFunctionDictionary() != null ? originalSff
+                        .getServiceFunctionDictionary() : Collections.emptyList();
+        List<ServiceFunctionDictionary> updatedDictList = updatedSff.getServiceFunctionDictionary() != null ? updatedSff
+                .getServiceFunctionDictionary() : Collections.emptyList();
+        List<ServiceFunctionDictionary> removedDictList = updatedDictList
+                .isEmpty() ? originalDictList : originalDictList.stream().filter(d -> !updatedDictList.contains(d))
+                .collect(Collectors.toList());
         if (!removedDictList.isEmpty()) {
             LOG.debug("SFF dictionaries removed {}", removedDictList);
             return rspNames.stream().map(SfcProviderRenderedPathAPI::readRenderedServicePath)
                     .filter(rsp -> isAnyDictionaryUsedInRsp(sffName, rsp, removedDictList))
                     .map(RenderedServicePath::getName).collect(Collectors.toList());
         }
-
         return Collections.emptyList();
     }
 
@@ -158,16 +134,13 @@ public class ServiceFunctionForwarderListener extends AbstractDataTreeChangeList
      * Whether any hop of a RSP makes use of any SF dictionary of a given list for
      * the given SFF.
      *
-     * @param sffName
-     *            the SFF name.
-     * @param renderedServicePath
-     *            the RSP.
-     * @param dictionaries
-     *            the list of SF dictionaries.
+     * @param sffName             the SFF name.
+     * @param renderedServicePath the RSP.
+     * @param dictionaries        the list of SF dictionaries.
      * @return true if the RSP makes use of the dictionary.
      */
     private boolean isAnyDictionaryUsedInRsp(final SffName sffName, final RenderedServicePath renderedServicePath,
-            final List<ServiceFunctionDictionary> dictionaries) {
+                                             final List<ServiceFunctionDictionary> dictionaries) {
         List<SfName> dictionarySfNames = dictionaries.stream().map(ServiceFunctionDictionary::getName)
                 .collect(Collectors.toList());
         return renderedServicePath.getRenderedServicePathHop().stream()

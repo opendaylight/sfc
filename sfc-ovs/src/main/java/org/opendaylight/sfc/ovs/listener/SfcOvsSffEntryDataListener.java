b293e6b9c1398e4b68dacd28b577178b663e16cc
Faseela K <faseela.k@ericsson.com>
2018-04-11 10:36:30 +0530

    Migrate from genius mdsalutil-api/utils to tools-api/utils
    Change-Id: I9293be18ee02bbe0ab2562c6d2249097d729c78f
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

--- sfc-ovs/src/main/java/org/opendaylight/sfc/ovs/listener/SfcOvsSffEntryDataListener.java
+++ sfc-ovs/src/main/java/org/opendaylight/sfc/ovs/listener/SfcOvsSffEntryDataListener.java
@@ -21,7 +21,7 @@ import javax.annotation.Nonnull;
 import javax.inject.Singleton;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
-import org.opendaylight.genius.datastoreutils.listeners.AbstractSyncDataTreeChangeListener;
+import org.opendaylight.genius.tools.mdsal.listener.AbstractSyncDataTreeChangeListener;
 import org.opendaylight.sfc.ovs.api.SfcSffToOvsMappingAPI;
 import org.opendaylight.sfc.ovs.provider.SfcOvsUtil;
 import org.opendaylight.sfc.provider.api.SfcDataStoreAPI;

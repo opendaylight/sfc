33ae8e2dbcd143912b389bd1d72b9475dd17c706
Tom Pantelis <tompantelis@gmail.com>
2018-07-25 10:41:55 -0400

    Adjust to DOMDataTreeCommitCohort API change
    Implement the new canCommit method in ServiceFunctionPathCohort
    that returns FluentFuture instead of the deprecated CheckedFuture.
    
    Change-Id: I18dd564542ad367212a89c9652b731ac64bbbfe6
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- sfc-provider/src/main/java/org/opendaylight/sfc/provider/validators/ServiceFunctionPathCohort.java
+++ sfc-provider/src/main/java/org/opendaylight/sfc/provider/validators/ServiceFunctionPathCohort.java
@@ -8,8 +8,7 @@
 package org.opendaylight.sfc.provider.validators;
 
 import com.google.common.collect.ImmutableSet;
-import com.google.common.util.concurrent.CheckedFuture;
-import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.FluentFuture;
 import java.util.Collection;
 import java.util.Iterator;
 import javassist.ClassPool;
@@ -25,6 +24,7 @@ import org.opendaylight.mdsal.dom.api.DOMDataTreeCandidate;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeCommitCohort;
 import org.opendaylight.sfc.provider.validators.util.ValidationConstants;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sfp.rev140701.service.function.paths.ServiceFunctionPath;
+import org.opendaylight.yangtools.util.concurrent.FluentFutures;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.YangModuleInfo;
 import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
@@ -57,53 +57,55 @@ public class ServiceFunctionPathCohort implements DOMDataTreeCommitCohort {
     }
 
     @Override
-    @Deprecated
     @SuppressWarnings({ "rawtypes", "unchecked" })
-    public CheckedFuture<PostCanCommitStep, DataValidationFailedException> canCommit(Object txId,
-            DOMDataTreeCandidate candidate, SchemaContext ctx) {
+    public FluentFuture<PostCanCommitStep> canCommit(Object txId, SchemaContext ctx,
+            Collection<DOMDataTreeCandidate> candidates) {
 
-        LOG.debug("canCommit:called! txId={}, candidate={}, context={} ", txId, candidate, ctx);
+        for (DOMDataTreeCandidate candidate : candidates) {
+            LOG.debug("canCommit:called! txId={}, candidate={}, context={} ", txId, candidate, ctx);
 
-        DataTreeCandidateNode candidateRoot = candidate.getRootNode();
-        NormalizedNode nn = candidateRoot.getDataAfter().orElse(null);
-        if (nn == null) {
-            LOG.debug("canCommit:no sfp after the change");
-            return ValidationConstants.SUCCESS_CAN_COMMIT_FUTURE;
-        }
+            DataTreeCandidateNode candidateRoot = candidate.getRootNode();
+            NormalizedNode nn = candidateRoot.getDataAfter().orElse(null);
+            if (nn == null) {
+                LOG.debug("canCommit:no sfp after the change");
+                continue;
+            }
 
-        LOG.debug("canCommit:updating codec contexts");
-        moduleContext.addModuleInfos(infos);
-        bindingContext = BindingRuntimeContext.create(moduleContext, ctx);
-        codecRegistry.onBindingRuntimeContextUpdated(bindingContext);
+            LOG.debug("canCommit:updating codec contexts");
+            moduleContext.addModuleInfos(infos);
+            bindingContext = BindingRuntimeContext.create(moduleContext, ctx);
+            codecRegistry.onBindingRuntimeContextUpdated(bindingContext);
 
-        LOG.debug("canCommit:Mapping service ready");
+            LOG.debug("canCommit:Mapping service ready");
 
-        LOG.debug("canCommit:before deserializing:  {}", nn);
-        // (nn is an immutableMapNode). Contains an unmodifiable collection
-        // reference of all this thing
-        // https://wiki.opendaylight.org/view/OpenDaylight_Controller:MD-SAL:Design:Normalized_DOM_Model
-        Collection collection = (Collection<MapEntryNode>) nn.getValue();
-        LOG.debug("canCommit:collection containing the sfs:  {}", collection);
-        Iterator<MapEntryNode> menIter = collection.iterator();
-        while (menIter.hasNext()) {
-            MapEntryNode meNode = menIter.next();
-            LOG.debug("canCommit:sfp to process: {}", meNode);
-            LOG.debug("canCommit:the first SF (as nn):  {}", meNode);
-            DataObject dobj = codecRegistry.fromNormalizedNode(ValidationConstants.SFP_PATH_YII, meNode)
-                    .getValue();
-            LOG.debug("canCommit:registerValidationCohorts:the first SFP (as dataobject):  {}", dobj);
-            ServiceFunctionPath sfp = (ServiceFunctionPath) dobj;
-            LOG.debug("canCommit:registerValidationCohorts:the implemented interface: {}",
-                    dobj.getImplementedInterface());
-            LOG.debug("canCommit:the first SF (as binding representation):  {}", sfp);
-            try {
-                if (!sfpv.validateServiceFunctionPath(sfp)) {
-                    return ValidationConstants.FAILED_CAN_COMMIT_SFP_FUTURE;
+            LOG.debug("canCommit:before deserializing:  {}", nn);
+            // (nn is an immutableMapNode). Contains an unmodifiable collection
+            // reference of all this thing
+            // https://wiki.opendaylight.org/view/OpenDaylight_Controller:MD-SAL:Design:Normalized_DOM_Model
+            Collection collection = (Collection<MapEntryNode>) nn.getValue();
+            LOG.debug("canCommit:collection containing the sfs:  {}", collection);
+            Iterator<MapEntryNode> menIter = collection.iterator();
+            while (menIter.hasNext()) {
+                MapEntryNode meNode = menIter.next();
+                LOG.debug("canCommit:sfp to process: {}", meNode);
+                LOG.debug("canCommit:the first SF (as nn):  {}", meNode);
+                DataObject dobj = codecRegistry.fromNormalizedNode(ValidationConstants.SFP_PATH_YII, meNode)
+                        .getValue();
+                LOG.debug("canCommit:registerValidationCohorts:the first SFP (as dataobject):  {}", dobj);
+                ServiceFunctionPath sfp = (ServiceFunctionPath) dobj;
+                LOG.debug("canCommit:registerValidationCohorts:the implemented interface: {}",
+                        dobj.getImplementedInterface());
+                LOG.debug("canCommit:the first SF (as binding representation):  {}", sfp);
+                try {
+                    if (!sfpv.validateServiceFunctionPath(sfp)) {
+                        return ValidationConstants.FAILED_CAN_COMMIT_SFP_FUTURE;
+                    }
+                } catch (DataValidationFailedException dvfe) {
+                    return FluentFutures.immediateFailedFluentFuture(dvfe);
                 }
-            } catch (DataValidationFailedException dvfe) {
-                return Futures.immediateFailedCheckedFuture(dvfe);
             }
         }
-        return ValidationConstants.SUCCESS_CAN_COMMIT_FUTURE;
+
+        return PostCanCommitStep.NOOP_SUCCESSFUL_FUTURE;
     }
 }
